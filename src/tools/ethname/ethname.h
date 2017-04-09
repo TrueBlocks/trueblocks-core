f8bc259a1d3021819b41611dad6acb84f2161936
Thomas Jay Rush <jrush@greathill.com>
2017-04-08 21:51:13 -0400

    Inclusion of first source code files for ethname

---
+++ src/tools/ethname/ethname.h
@@ -0,0 +1,37 @@
+#pragma once
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 1999, 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *------------------------------------------------------------------------*/
+#include "utillib.h"
+#include "options.h"
+
+class CAccountName //: public CAccount
+{
+public:
+	SFString addr;
+	SFString source;
+	SFString name;
+	CAccountName() { }
+	CAccountName(SFString& nameIn)
+	{
+		source = nameIn;
+		addr = toLower(nextTokenClear(source,'\t'));
+		name = nextTokenClear(source,'\t');
+	}
+	SFString Format12(void)
+	{
+#define F(a) (SFString("`") + (SFString(#a) + "`: `" + a + "` ")).Substitute("`","\"")
+		return ("{" + F(addr) + ", " + F(name) + ", " + F(source) + "},").Substitute(" ,",",");
+	}
+        bool Match(const SFString& s1, const SFString& s2, const SFString& s3, bool matchCase, bool all);
+};
+
+extern CAccountName *accounts;
+extern uint32_t nNames;
+extern CFileExportContext out;
+extern void loadData(void);
+extern uint32_t countOf(const SFString& addr);
+extern SFString showName(const SFString& s1, const SFString& s2, const SFString& s3, bool match, bool all);
+extern SFString setName(const SFString& addr, const SFString& name);
