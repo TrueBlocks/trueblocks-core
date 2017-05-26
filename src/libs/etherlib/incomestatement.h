673b1afdd4035c4fb4176f3299b0f8a3236d2c67
Thomas Jay Rush <jrush@greathill.com>
2017-05-25 20:23:29 -0400

    Loading in ether lib

---
+++ src/libs/etherlib/incomestatement.h
@@ -0,0 +1,165 @@
+#pragma once
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+/*
+ * This file was generated with makeClass. Edit only those parts of the code inside
+ * of 'EXISTING_CODE' tags.
+ */
+#include "ethtypes.h"
+#include "abilib.h"
+
+namespace qblocks {
+
+//--------------------------------------------------------------------------
+class CIncomeStatement;
+typedef SFArrayBase<CIncomeStatement>         CIncomeStatementArray;
+typedef SFList<CIncomeStatement*>             CIncomeStatementList;
+typedef SFUniqueList<CIncomeStatement*>       CIncomeStatementListU;
+
+// EXISTING_CODE
+// EXISTING_CODE
+
+//--------------------------------------------------------------------------
+class CIncomeStatement : public CBaseNode
+{
+public:
+    SFIntBN begBal;
+    SFIntBN inflow;
+    SFIntBN outflow;
+    SFIntBN endBal;
+    SFUint32 blockNum;
+
+public:
+    CIncomeStatement(void);
+    CIncomeStatement(const CIncomeStatement& in);
+    ~CIncomeStatement(void);
+    CIncomeStatement& operator=(const CIncomeStatement& in);
+
+    DECLARE_NODE (CIncomeStatement);
+
+    // EXISTING_CODE
+    void operator+=(const CIncomeStatement &x);
+    // EXISTING_CODE
+
+protected:
+    void Clear(void);
+    void Init(void);
+    void Copy(const CIncomeStatement& in);
+    bool readBackLevel(SFArchive& archive);
+
+    // EXISTING_CODE
+    // EXISTING_CODE
+};
+
+//--------------------------------------------------------------------------
+inline CIncomeStatement::CIncomeStatement(void)
+{
+    Init();
+    // EXISTING_CODE
+    // EXISTING_CODE
+}
+
+//--------------------------------------------------------------------------
+inline CIncomeStatement::CIncomeStatement(const CIncomeStatement& in)
+{
+    // EXISTING_CODE
+    // EXISTING_CODE
+    Copy(in);
+}
+
+// EXISTING_CODE
+// EXISTING_CODE
+
+//--------------------------------------------------------------------------
+inline CIncomeStatement::~CIncomeStatement(void)
+{
+    Clear();
+    // EXISTING_CODE
+    // EXISTING_CODE
+}
+
+//--------------------------------------------------------------------------
+inline void CIncomeStatement::Clear(void)
+{
+    // EXISTING_CODE
+    // EXISTING_CODE
+}
+
+//--------------------------------------------------------------------------
+inline void CIncomeStatement::Init(void)
+{
+    CBaseNode::Init();
+
+    begBal = 0;
+    inflow = 0;
+    outflow = 0;
+    endBal = 0;
+    blockNum = 0;
+
+    // EXISTING_CODE
+    // EXISTING_CODE
+}
+
+//--------------------------------------------------------------------------
+inline void CIncomeStatement::Copy(const CIncomeStatement& in)
+{
+    Clear();
+    CBaseNode::Copy(in);
+
+    begBal = in.begBal;
+    inflow = in.inflow;
+    outflow = in.outflow;
+    endBal = in.endBal;
+    blockNum = in.blockNum;
+
+    // EXISTING_CODE
+    // EXISTING_CODE
+    finishParse();
+}
+
+//--------------------------------------------------------------------------
+inline CIncomeStatement& CIncomeStatement::operator=(const CIncomeStatement& in)
+{
+    Copy(in);
+    // EXISTING_CODE
+    // EXISTING_CODE
+    return *this;
+}
+
+//---------------------------------------------------------------------------
+inline SFString CIncomeStatement::getValueByName(const SFString& fieldName) const
+{
+    // EXISTING_CODE
+    // EXISTING_CODE
+    return Format("[{"+toUpper(fieldName)+"}]");
+}
+
+//---------------------------------------------------------------------------
+IMPLEMENT_ARCHIVE_ARRAY(CIncomeStatementArray);
+IMPLEMENT_ARCHIVE_ARRAY_C(CIncomeStatementArray);
+IMPLEMENT_ARCHIVE_LIST(CIncomeStatementList);
+
+//---------------------------------------------------------------------------
+extern SFArchive& operator<<(SFArchive& archive, const CIncomeStatement& inc);
+extern SFArchive& operator>>(SFArchive& archive, CIncomeStatement& inc);
+
+//---------------------------------------------------------------------------
+// EXISTING_CODE
+    extern ostream& operator<<(ostream& os, const CIncomeStatement& t);
+    //------------------------------------------------------------
+    inline void CIncomeStatement::operator+=(const CIncomeStatement &x)
+    {
+        begBal += x.begBal;
+        inflow += x.inflow;
+        outflow += x.outflow;
+        endBal += x.endBal;
+        blockNum = x.blockNum;
+    }
+// EXISTING_CODE
+}  // namespace qblocks
+
---
+++ src/libs/etherlib/rpc.cpp
@@ -0,0 +1,126 @@
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+#if 0
+#include <stdio.h>
+#include "rpc.h"
+
+namespace qblocks {
+
+//-----------------------------------------------------
+IPCSocket::IPCSocket(SFString const& _path): m_path(_path)
+{
+    if (_path.length() >= sizeof(sockaddr_un::sun_path))
+    { cerr << "Error opening IPC: socket path is too long!" << "\n"; exit(0); }
+
+    struct sockaddr_un saun;
+    memset(&saun, 0, sizeof(sockaddr_un));
+    saun.sun_family = AF_UNIX;
+    strcpy(saun.sun_path, (const char*)_path);
+    saun.sun_len = sizeof(struct sockaddr_un);
+
+    if ((m_socket = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
+    { cerr << "Error creating IPC socket object" << "\n"; exit(0); }
+
+    if (connect(m_socket, reinterpret_cast<struct sockaddr const*>(&saun), sizeof(struct sockaddr_un)) < 0)
+    { cerr << "Error connecting to IPC socket: " << "\n"; exit(0); }
+
+    m_fp = fdopen(m_socket, "r");
+}
+
+//-----------------------------------------------------
+SFString IPCSocket::sendRequest(SFString const& _req)
+{
+    send(m_socket, (const char*)_req, _req.length(), 0);
+
+    char c;
+    SFString response;
+    while ((c = fgetc(m_fp)) != EOF)
+    {
+        if (c != '\n')
+            response += c;
+        else
+            break;
+    }
+    return response;
+}
+
+//-----------------------------------------------------
+CReceipt RPCSession::eth_getTransactionReceipt(SFString const& _transactionHash)
+{
+    CReceipt receipt;
+#if 0
+    SFString const result = rpcCall("eth_getTransactionReceipt", { _transactionHash });
+    if (result.empty())
+    {
+        cerr << "Result from eth_getTransactionReceipt call is empty. Quitting...\n";
+        exit(0);
+    }
+    receipt.gasUsed = result["gasUsed"];
+    receipt.contractAddress = result["contractAddress"];
+    for (auto const& log: result["logs"])
+    {
+        LogEntry entry;
+        entry.address = log["address"];
+        entry.data = log["data"];
+        for (auto const& topic: log["topics"])
+            entry.topics.push_back(topic);
+        receipt.logEntries.push_back(entry);
+    }
+#endif
+    return receipt;
+}
+
+//-----------------------------------------------------
+SFString RPCSession::rpcCall(SFString const& _methodName, SFString const& _args)
+{
+    SFString request = "{\"jsonrpc\":\"2.0\",\"method\":\"" + _methodName + "\",\"params\":[";
+    SFString args = _args;
+    while (!args.empty())
+    {
+        request += "\"" + nextTokenClear(args, '|') + "\"";
+        if (!args.empty())
+            request += ",";
+    }
+    request += "],\"id\":" + asString(m_rpcSequence) + "}";
+    ++m_rpcSequence;
+
+    SFString result = m_ipcSocket.sendRequest(request);
+    if (result.Contains("error"))
+    {
+        cerr << "Error on JSON-RPC call: " << result << "\n";
+        exit(0);
+    }
+    return result;
+}
+
+//-----------------------------------------------------
+SFString RPCSession::eth_getCode(SFString const& _address, SFString const& _blockNumber)
+{
+    return rpcCall("eth_getCode", _address + "|" + _blockNumber);
+}
+
+//-----------------------------------------------------
+SFString RPCSession::eth_getBalanc e(SFString const& _address, SFString const& _blockNumber)
+{
+    return rpcCall("eth_getBalanc e", _address + "|" + _blockNumber);
+}
+
+//-----------------------------------------------------
+SFString RPCSession::eth_getStorageRoot(SFString const& _address, SFString const& _blockNumber)
+{
+    return rpcCall("eth_getStorageRoot", _address + "|" + _blockNumber);
+}
+
+//-----------------------------------------------------
+RPCSession::RPCSession(const SFString& _path) : m_ipcSocket(_path)
+{
+}
+
+}  // namespace qblocks
+
+#endif
