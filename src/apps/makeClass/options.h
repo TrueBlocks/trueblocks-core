74795fb5a8f80d7223964d7b582f7c3fcc468fce
Thomas Jay Rush <jrush@greathill.com>
2017-05-27 01:44:18 -0400

    Added makeClass to source tree

---
+++ src/apps/makeClass/options.h
@@ -0,0 +1,34 @@
+#pragma once
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+#include "pairFile.h"
+
+//-------------------------------------------------------------------
+class COptions : public COptionsBase {
+public:
+    bool isAll;
+    bool isRun;
+    bool isEdit;
+    bool isRemove;
+    bool isList;
+    SFString namesp;
+
+    SFString classNames;
+    SFString filter;
+    SFString dataFile;
+    CToml1 classFile;
+
+    COptions(void);
+    ~COptions(void);
+
+    bool parseArguments(SFString& command);
+    void Init(void);
+};
+
+//-------------------------------------------------------------------
+extern bool listClasses(const SFString& path, void *data);
