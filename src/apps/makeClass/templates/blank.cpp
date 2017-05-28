1b399528faa8275f685a5bffcd6ef615359ccc56
Thomas Jay Rush <jrush@greathill.com>
2017-05-28 12:10:36 -0400

    Switch makeClass to cmake build system

--- src/apps/makeClass/templates/blank.cpp
+++ src/apps/makeClass/templates/blank.cpp
@@ -20,13 +20,11 @@ IMPLEMENT_NODE([{CLASS_NAME}], [{BASE_CLASS}], curVersion);
 static SFString next[{PROPER}]Chunk_custom(const SFString& fieldIn, bool& force, const void *data);
 
 //---------------------------------------------------------------------------
-void [{CLASS_NAME}]::Format(CExportContext& ctx, const SFString& fmtIn, void *data) const
-{
+void [{CLASS_NAME}]::Format(CExportContext& ctx, const SFString& fmtIn, void *data) const {
     if (!m_showing)
         return;
 
-    if (fmtIn.empty())
-    {
+    if (fmtIn.empty()) {
         ctx << toJson();
         return;
     }
@@ -40,18 +38,15 @@ void [{CLASS_NAME}]::Format(CExportContext& ctx, const SFString& fmtIn, void *da
 }
 
 //---------------------------------------------------------------------------
-SFString next[{PROPER}]Chunk(const SFString& fieldIn, bool& force, const void *data)
-{
+SFString next[{PROPER}]Chunk(const SFString& fieldIn, bool& force, const void *data) {
     const [{CLASS_NAME}] *[{SHORT3}] = (const [{CLASS_NAME}] *)data;
-    if ([{SHORT3}])
-    {
+    if ([{SHORT3}]) {
         // Give customized code a chance to override first
         SFString ret = next[{PROPER}]Chunk_custom(fieldIn, force, data);
         if (!ret.empty())
             return ret;
 
-        switch (tolower(fieldIn[0]))
-        {
+        switch (tolower(fieldIn[0])) {
 [FIELD_CASE]		}
 
         // EXISTING_CODE
@@ -63,32 +58,28 @@ SFString next[{PROPER}]Chunk(const SFString& fieldIn, bool& force, const void *d
             return ret;
     }
 
-    return "<span class=warning>Field not found: [{" + fieldIn + "}]</span>\n";
+    return "Field not found: [{" + fieldIn + "}]\n";
 }
 
 //---------------------------------------------------------------------------------------------------
-bool [{CLASS_NAME}]::setValueByName(const SFString& fieldName, const SFString& fieldValue)
-{
+bool [{CLASS_NAME}]::setValueByName(const SFString& fieldName, const SFString& fieldValue) {
     // EXISTING_CODE
     // EXISTING_CODE
 
 [{PARENT_SET}]
-    switch (tolower(fieldName[0]))
-    {
+    switch (tolower(fieldName[0])) {
 [FIELD_SETCASE]    }
     return false;
 }
 
 //---------------------------------------------------------------------------------------------------
-void [{CLASS_NAME}]::finishParse()
-{
+void [{CLASS_NAME}]::finishParse() {
     // EXISTING_CODE
     // EXISTING_CODE
 }
 
 //---------------------------------------------------------------------------------------------------
-bool [{CLASS_NAME}]::Serialize(SFArchive& archive)
-{
+bool [{CLASS_NAME}]::Serialize(SFArchive& archive) {
     if (!archive.isReading())
         return ((const [{CLASS_NAME}]*)this)->SerializeC(archive);
 
@@ -97,20 +88,18 @@ bool [{CLASS_NAME}]::Serialize(SFArchive& archive)
 }
 
 //---------------------------------------------------------------------------------------------------
-bool [{CLASS_NAME}]::SerializeC(SFArchive& archive) const
-{
+bool [{CLASS_NAME}]::SerializeC(SFArchive& archive) const {
 [{PARENT_SER1}][ARCHIVE_WRITE]
     return true;
 }
 
 //---------------------------------------------------------------------------
-void [{CLASS_NAME}]::registerClass(void)
-{
-    static bool been_here=false;
+void [{CLASS_NAME}]::registerClass(void) {
+    static bool been_here = false;
     if (been_here) return;
-    been_here=true;
+    been_here = true;
 
-    [{PARENT_REG}]uint32_t fieldNum=1000;
+    [{PARENT_REG}]uint32_t fieldNum = 1000;
     ADD_FIELD([{CLASS_NAME}], "schema",  T_NUMBER|TS_LABEL, ++fieldNum);
     ADD_FIELD([{CLASS_NAME}], "deleted", T_BOOL|TS_LABEL,  ++fieldNum);
 [REGISTER_FIELDS]
@@ -124,19 +113,16 @@ void [{CLASS_NAME}]::registerClass(void)
 }
 
 //---------------------------------------------------------------------------
-SFString next[{PROPER}]Chunk_custom(const SFString& fieldIn, bool& force, const void *data)
-{
+SFString next[{PROPER}]Chunk_custom(const SFString& fieldIn, bool& force, const void *data) {
     const [{CLASS_NAME}] *[{SHORT3}] = (const [{CLASS_NAME}] *)data;
-    if ([{SHORT3}])
-    {
-        switch (tolower(fieldIn[0]))
-        {
+    if ([{SHORT3}]) {
+        switch (tolower(fieldIn[0])) {
             // EXISTING_CODE
             // EXISTING_CODE
             case 'p':
                 // Display only the fields of this node, not it's parent type
                 if ( fieldIn % "parsed" )
-                    return nextBasenodeChunk(fieldIn,force,[{SHORT3}]);
+                    return nextBasenodeChunk(fieldIn, force, [{SHORT3}]);
                 break;
 
             default:
@@ -148,17 +134,15 @@ SFString next[{PROPER}]Chunk_custom(const SFString& fieldIn, bool& force, const 
 }
 
 //---------------------------------------------------------------------------
-bool [{CLASS_NAME}]::handleCustomFormat(CExportContext& ctx, const SFString& fmtIn, void *data) const
-{
+bool [{CLASS_NAME}]::handleCustomFormat(CExportContext& ctx, const SFString& fmtIn, void *data) const {
     // EXISTING_CODE
     // EXISTING_CODE
     return false;
 }
 
 //---------------------------------------------------------------------------
-bool [{CLASS_NAME}]::readBackLevel(SFArchive& archive)
-{
-    bool done=false;
+bool [{CLASS_NAME}]::readBackLevel(SFArchive& archive) {
+    bool done = false;
     // EXISTING_CODE
     // EXISTING_CODE
     return done;
