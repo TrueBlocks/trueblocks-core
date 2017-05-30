2b2b03f5b9d9a7b5fe5a7f801ae6c3453e8957a0
Thomas Jay Rush <jrush@greathill.com>
2017-05-30 06:58:42 -0400

    Cleanup for de-linting

--- src/libs/utillib/sfstring.h
+++ src/libs/utillib/sfstring.h
@@ -594,4 +594,36 @@ namespace qblocks {
         return padRight(in.length()>x-3 ? in.Left(x-3) + "..." : in, x);
     }
 
+    //--------------------------------------------------------------------
+    inline SFString StripTrailing(const SFString& str, char c) {
+        SFString ret = str;
+        while (ret.endsWith(c))
+            ret = ret.Left(ret.length()-1);
+
+        return ret;
+    }
+
+    //--------------------------------------------------------------------
+    inline SFString StripLeading(const SFString& str, char c) {
+        SFString ret = str;
+        while (ret.startsWith(c))
+            ret = ret.substr(1);
+
+        return ret;
+    }
+
+    //--------------------------------------------------------------------
+    inline SFString Strip(const SFString& str, char c) {
+        return StripTrailing(StripLeading(str, c), c);
+    }
+
+    //--------------------------------------------------------------------
+    inline SFString StripAny(const SFString& str, const SFString& any) {
+        SFString ret = str;
+        while (endsWithAny(ret, any) || startsWithAny(ret, any)) {
+            for (int i = 0 ; i < any.length() ; i++)
+                ret = Strip(ret, any[i]);
+        }
+        return ret;
+    }
 }  // namespace qblocks
