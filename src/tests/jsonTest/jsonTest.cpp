db3dddc61558b02036fa4f39befec9004e3a1283
Thomas Jay Rush <jrush@greathill.com>
2017-06-01 06:55:07 -0400

    Cleaning up test cases

--- src/tests/jsonTest/jsonTest.cpp
+++ src/tests/jsonTest/jsonTest.cpp
@@ -9,12 +9,11 @@
 #include "options.h"
 
 //--------------------------------------------------------------
-int main(int argc, const char * argv[])
-{
-	CFunction::registerClass();
-	CParameter::registerClass();
-	CAccount::registerClass();
-	CTransaction::registerClass();
+int main(int argc, const char * argv[]) {
+    CFunction::registerClass();
+    CParameter::registerClass();
+    CAccount::registerClass();
+    CTransaction::registerClass();
 
     // Tell the system where the blocks are and which version to use
     setStorageRoot(BLOCK_CACHE);
@@ -29,103 +28,99 @@ int main(int argc, const char * argv[])
         SFString command = nextTokenClear(options.commandList, '\n');
         if (!options.parseArguments(command))
             return 0;
-	}
+    }
 
 #if 0
-	CAccount theAccount;
+    CAccount theAccount;
 
-	// This is how many records we currently have
-	uint32_t origCount  = theAccount.transactions.getCount();
-	uint32_t nNewBlocks = 0;
-	uint32_t nextRecord = origCount;
+    // This is how many records we currently have
+    uint32_t origCount  = theAccount.transactions.getCount();
+    uint32_t nNewBlocks = 0;
+    uint32_t nextRecord = origCount;
 
-	cerr << "\tSlurping new transactions from blockchain...\n";
-	uint32_t  nRead = 0;
+    cerr << "\tSlurping new transactions from blockchain...\n";
+    uint32_t  nRead = 0;
 
-extern char *cleanUpJson(char *s);
-	// Keep reading until we get less than a full page
-	SFString contents = asciiFileToString("test.json");
-	contents = cleanUpJson((char *)(const char*)contents);
-	SFString message = nextTokenClear(contents, '[');
-	uint32_t nRecords = countOf('}',contents)-1;
-	nRead += nRecords;
-	cerr << "\tDownloaded " << nRead << " potentially new transactions." << (isTesting?"\n":"\r");
+    extern char *cleanUpJson(char *s);
+    // Keep reading until we get less than a full page
+    SFString contents = asciiFileToString("test.json");
+    contents = cleanUpJson(contents.c_str());
+    SFString message = nextTokenClear(contents, '[');
+    uint32_t nRecords = countOf('}', contents) - 1;
+    nRead += nRecords;
+    cerr << "\tDownloaded " << nRead << " potentially new transactions." << (isTesting?"\n":"\r");
 
-	uint32_t minBlock=0,maxBlock=0;
-	findBlockRange(contents, minBlock, maxBlock);
-	cerr << "\n\tDownload contains blocks from " << minBlock << " to " << maxBlock << "\n";
+    uint32_t minBlock = 0, maxBlock = 0;
+    findBlockRange(contents, minBlock, maxBlock);
+    cerr << "\n\tDownload contains blocks from " << minBlock << " to " << maxBlock << "\n";
 
-	// Keep track of which last full page we've read
-	theAccount.lastPage = 0;
-	theAccount.pageSize = 5000;
+    // Keep track of which last full page we've read
+    theAccount.lastPage = 0;
+    theAccount.pageSize = 5000;
 
-	cerr << "\tSearching transactions";
-	if (theAccount.lastBlock>0)
-	    cerr << " after block " << theAccount.lastBlock;
-	cerr << "\n";
+    cerr << "\tSearching transactions";
+    if (theAccount.lastBlock > 0)
+        cerr << " after block " << theAccount.lastBlock;
+    cerr << "\n";
 
-	// pre allocate the array
-	theAccount.transactions.Grow(nRead);
+    // pre allocate the array
+    theAccount.transactions.Grow(nRead);
 
     CPerformanceClock bigStart = qbNow();
-	uint32_t lastBlock=0;
-	uint32_t cnt=0;
-	char *p = (char *)(const char*)contents;
-	while (p && *p)
-	{
-		CTransaction trans;uint32_t nFields=0;
-//double littleStart = qbNow();
-		p = trans.parseJson(p,nFields);
-//double littleStart2 = qbNow();
-		if (nFields)
-		{
-			uint32_t transBlock = trans.blockNumber;
-			if (transBlock > theAccount.lastBlock) // add the new transaction if it's in a new block
-			{
-				theAccount.transactions[nextRecord++] = trans;
-				lastBlock = transBlock;
-				if (!(++nNewBlocks%REP_FREQ))
-				{
-					cerr << "\tFound new transaction at block " << transBlock << ". Importing..." << (isTesting?"\n":"\r");
-					cerr.flush();
-				}
-			}
-		}
-		cnt++;
-//double littleEnd = qbNow();
-if (!(nNewBlocks%REP_FREQ))
-{
-//	cerr << "one record: " << (littleEnd-littleStart) << "\n";
-//	cerr << "one parse: " << (littleStart2-littleStart) << "\n";
-}
-	}
-	if (!isTesting && nNewBlocks) { cerr << "\tFound new transaction at block " << lastBlock << ". Importing...\n"; cerr.flush(); }
+    uint32_t lastBlock = 0;
+    uint32_t cnt = 0;
+    char *p = contents.c_str();
+    while (p && *p) {
+        CTransaction trans;
+        uint32_t nFields = 0;
+        // double littleStart = qbNow();
+        p = trans.parseJson(p, nFields);
+        // double littleStart2 = qbNow();
+        if (nFields) {
+            uint32_t transBlock = trans.blockNumber;
+            if (transBlock > theAccount.lastBlock) {  // add the new transaction if it's in a new block
+                theAccount.transactions[nextRecord++] = trans;
+                lastBlock = transBlock;
+                if (!(++nNewBlocks%REP_FREQ)) {
+                    cerr << "\tFound new transaction at block " << transBlock
+                        << ". Importing..." << (isTesting?"\n":"\r");
+                    cerr.flush();
+                }
+            }
+        }
+        cnt++;
+        // double littleEnd = qbNow();
+        if (!(nNewBlocks%REP_FREQ)) {
+            // cerr << "one record: " << (littleEnd-littleStart) << "\n";
+            // cerr << "one parse: " << (littleStart2-littleStart) << "\n";
+        }
+    }
+    if (!isTesting && nNewBlocks) {
+        cerr << "\tFound new transaction at block " << lastBlock << ". Importing...\n"; cerr.flush();
+    }
     CPerformanceClock bigEnd = qbNow();
     cerr << "all records: " << (bigEnd - bigStart) << "\n";
 #endif
 
-	return 0;
+    return 0;
 }
 
 //--------------------------------------------------------------------------------
-void findBlockRange(const SFString& json, uint32_t& minBlock, uint32_t& maxBlock)
-{
-	SFString search = "blockNumber:";
-	uint32_t len = search.length();
+void findBlockRange(const SFString& json, uint32_t& minBlock, uint32_t& maxBlock) {
+    SFString search = "blockNumber:";
+    uint32_t len = search.length();
 
-	minBlock = 0;
-	int32_t first = json.find(search);
-	if (first != (int32_t)NOPOS)
-	{
-		SFString str = json.substr(first+len);
-		minBlock = toLong(str);
-	}
+    minBlock = 0;
+    int32_t first = json.find(search);
+    if (first != (int32_t)NOPOS) {
+        SFString str = json.substr(first+len);
+        minBlock = toLong(str);
+    }
 
-	SFString end = json.substr(json.ReverseFind('{'));//pull off the last transaction
-	int32_t last = end.find(search);
-	if (last != (int32_t)NOPOS)
-	{
-		SFString str = end.substr(last+len);
-		maxBlock = toLong(str);
-	}
+    SFString end = json.substr(json.ReverseFind('{'));  // pull off the last transaction
+    int32_t last = end.find(search);
+    if (last != (int32_t)NOPOS) {
+        SFString str = end.substr(last+len);
+        maxBlock = toLong(str);
+    }
 }
