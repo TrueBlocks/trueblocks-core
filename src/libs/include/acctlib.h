25138f2f64d366dbfc7c4b1f500cbb0700d73489
Thomas Jay Rush <jrush@greathill.com>
2017-06-08 14:44:35 -0400

    Fix to acctlib and fixes to BUILD.md file

---
+++ src/libs/include/acctlib.h
@@ -0,0 +1,17 @@
+#pragma once
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+
+#include "accounttree.h"
+#include "acctlib.h"
+#include "branch.h"
+#include "infix.h"
+#include "leaf.h"
+#include "treenode.h"
+
+using namespace qblocks;
