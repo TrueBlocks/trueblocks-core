eb00348d8736050b032b9c7c4760b1cfc7845e21
Thomas Jay Rush <jrush@greathill.com>
2017-06-24 04:48:20 -0400

    Added getTokenBal tool

---
+++ src/tools/getTokenBal/options.h
@@ -0,0 +1,26 @@
+#pragma once
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation. All Rights Reserved.
+ *------------------------------------------------------------------------*/
+#include "etherlib.h"
+
+//-----------------------------------------------------------------------------
+class COptions : public COptionsBase {
+public:
+    SFString token;
+    SFString holders;
+    SFString blocks;
+    bool asEther;
+    bool asData;
+
+    COptions(void);
+    ~COptions(void);
+
+    bool parseArguments(SFString& command);
+    void Init(void);
+};
+
+//-----------------------------------------------------------------------------
+extern bool visitNonEmptyBlock(CBlock& node, void *data);
+extern bool visitEmptyBlock(CBlock& node, void *data);
