c4ed82ff8297f7bb329c89a5627f802388179ca4
Thomas Jay Rush <jrush@greathill.com>
2017-06-24 04:51:43 -0400

    Added bloomScan tool
    This is really intended only as an example program, not a full fledged
    tool

---
+++ src/tools/bloomScan/options.h
@@ -0,0 +1,37 @@
+#pragma once
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+#include "etherlib.h"
+
+//-----------------------------------------------------------------------------
+class COptions : public COptionsBase
+{
+public:
+	SFString mode;
+
+	        COptions  (void);
+	       ~COptions  (void);
+
+	bool parseArguments(SFString& command);
+	void Init (void);
+};
+
+//-----------------------------------------------------------------------------
+extern bool visitNonEmptyBlock(CBlock& node, void *data);
+extern bool displayBloom(CBlock& node, void *data);
+extern bool checkBloom(CBlock& node, void *data);
+inline SFString dateColor(SFUint32 bn)
+{
+	if (1442339 <= bn && bn <= 1439214) return bBlue;
+	return bYellow;
+}
+inline SFTime toDate(timestamp_t ts) { return dateFromTimeStamp(ts); }
+inline void etherlib_init(void) { setStorageRoot(BLOCK_CACHE); etherlib_init("binary"); }
+#define null ((void*)0)
+#define toSpaces dbgBloom
+#define forEveryBlock(a,b,c) forEveryNonEmptyBlockOnDisc(a, null, b, c)
