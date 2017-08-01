aae63465c939c1ceee2d4b8ddb38a59dc120d0a5
Thomas Jay Rush <jrush@greathill.com>
2017-08-01 11:25:26 -0400

    Adding tests for apps

---
+++ test/apps/blockScraper/bytesUsed/reporter.h
@@ -0,0 +1,23 @@
+#pragma once
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+#include "etherlib.h"
+
+class CReporter {
+public:
+    SFUintBN nFiles;
+    SFUintBN nBytes;
+    SFUintBN totFiles;
+    SFUintBN totBytes;
+    SFString largest;
+    SFString smallest;
+    CReporter() : nFiles(0), nBytes(0), totFiles(0), totBytes(0) { }
+};
+
+//-----------------------------------------------------------------------
+extern bool countBytes(const SFString& fileName, void *data);
