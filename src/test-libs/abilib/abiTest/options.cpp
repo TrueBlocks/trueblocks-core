c5ee470550a7b467369a04778d31dc4a7a1f278a
Thomas Jay Rush <jrush@greathill.com>
2017-08-06 11:41:17 -0400

    Added acctTest, abiTest, whenBlock, and makeClass to testing regime.

---
+++ src/test-libs/abilib/abiTest/options.cpp
@@ -0,0 +1,56 @@
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+#include "options.h"
+
+//---------------------------------------------------------------------------------------------------
+CParams params[] = {
+    CParams("~mode", "Mode of operation. One or more of 'encoding' or 'generation'.\n"),
+    CParams("", "Simple program to illustrate how to encode function and event signatures.\n"),
+};
+uint32_t nParams = sizeof(params) / sizeof(CParams);
+
+//---------------------------------------------------------------------------------------------------
+bool COptions::parseArguments(SFString& command) {
+
+    Init();
+    while (!command.empty()) {
+        SFString arg = nextTokenClear(command, ' ');
+        if (arg == "encoding" || arg == "generation") {
+            mode += (arg + "|");
+        } else {
+            return usage("Invalid argument " + arg + ". Please enter either 'encoding' or 'generation'. Quitting...");
+        }
+    }
+    if (mode == "")
+        return usage("You must enter either 'encoding' or 'generation'. Quitting...");
+    return true;
+}
+
+//---------------------------------------------------------------------------------------------------
+void COptions::Init(void) {
+    paramsPtr = params;
+    nParamsRef = nParams;
+
+    // mode = "";
+    
+    useVerbose = true;
+    useTesting = false;
+    minArgs = 0;
+}
+
+//---------------------------------------------------------------------------------------------------
+COptions::COptions(void) {
+    Init();
+    // header = "";
+    // footer = "";
+    // seeAlso = "";
+}
+
+//--------------------------------------------------------------------------------
+COptions::~COptions(void) {
+}
