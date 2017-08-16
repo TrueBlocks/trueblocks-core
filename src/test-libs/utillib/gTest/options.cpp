a0d5aba30e67d0e33bbccc6c8fb0e027992d84f9
Thomas Jay Rush <jrush@greathill.com>
2017-08-16 09:24:43 -0400

    Adding test shell for use of Google test libraries.

---
+++ src/test-libs/utillib/gTest/options.cpp
@@ -0,0 +1,56 @@
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+#include "options.h"
+
+//---------------------------------------------------------------------------------------------------
+CParams params[] = {
+    CParams("-option", "First option"),
+    CParams("",        "Test harness for quickBlocks.\n"),
+};
+uint32_t nParams = sizeof(params) / sizeof(CParams);
+
+//---------------------------------------------------------------------------------------------------
+bool COptions::parseArguments(SFString& command) {
+
+    Init();
+    while (!command.empty()) {
+        SFString arg = nextTokenClear(command, ' ');
+        if (arg == "-o" || arg == "--option") {
+            // whatever
+
+        } else if (arg.startsWith('-')) {  // do not collapse
+            if (!builtInCmd(arg)) {
+                return usage("Invalid option: " + arg);
+            }
+
+        } else {
+            return usage("Error. Quitting...");
+        }
+    }
+
+    return true;
+}
+
+//---------------------------------------------------------------------------------------------------
+void COptions::Init(void) {
+    paramsPtr = params;
+    nParamsRef = nParams;
+
+    useVerbose = true;
+    useTesting = false;
+    //minArgs = 0;
+}
+
+//---------------------------------------------------------------------------------------------------
+COptions::COptions(void) {
+    Init();
+}
+
+//--------------------------------------------------------------------------------
+COptions::~COptions(void) {
+}
