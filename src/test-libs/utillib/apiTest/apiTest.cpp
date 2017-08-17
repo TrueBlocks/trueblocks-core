cdef8baf96577c5afc7d252b20a24677fcb8c809
Thomas Jay Rush <jrush@greathill.com>
2017-08-17 09:35:41 -0400

    Removing google test stuff.  Renamed ./test-libs/utillib/gTest to ./test-libs/utillib/apiTest

--- src/test-libs/utillib/apiTest/apiTest.cpp
+++ src/test-libs/utillib/apiTest/apiTest.cpp
@@ -6,11 +6,10 @@
  * The LICENSE at the root of this repo details your rights (if any)
  *------------------------------------------------------------------------*/
 
-#ifdef OLD_THOMAS_CODE
-
-#include "jsonTest.h"
+#include "etherlib.h"
 #include "options.h"
 
+#if 1
 //--------------------------------------------------------------
 int main(int argc, const char * argv[]) {
 
@@ -29,39 +28,34 @@ int main(int argc, const char * argv[]) {
         if (!options.parseArguments(command))
             return 0;
 
-        while (!options.fileName.empty()) {
-            SFString file = nextTokenClear(options.fileName, '|');
-            cout << file << "\n";
-        }
+//        while (!options.fileName.empty()) {
+//            SFString file = nextTokenClear(options.fileName, '|');
+//            cout << file << "\n";
+//        }
     }
 
     return 0;
 }
 
 #else
-
 //--------------------------------------------------------------------------------------------------------
-
 // Include for GTEST library
 #include <gtest/gtest.h>
 // Code we want to test - we include the options.h header file so we can define here tests for this class
 #include "options.h"
-
 // IMPORTANT: To test a class we need to define a wrapper of the class we want to test
 // In this example we want to test COptions so we define a COptionsTest class
-
 class COptionsTest : public ::testing::Test {
     protected:
         virtual void SetUp() {
 	    // Common initialization code done always we run a test
-        }					   
+        }
 
         virtual void TearDown() {
 	    // Code here will be called immediately after each test
             // (right before the destructor).
 	}
 };
-
 // At TEST functions we need to use TEST_F, we define a namespace and then inside each namespace, the name of the test
 // These examples define the SquareRootTest and inside it the PositiveNos and NegativeNos tests
 TEST_F(COptionsTest, WhatEverYouWant1) {
@@ -69,20 +63,18 @@ TEST_F(COptionsTest, WhatEverYouWant1) {
    // do whatever you want here and then test an options method
    // code to do initializations, define variables
    // At ASSERT we can then compare the output of the member function selected with the value expected
+    cerr << "Running test WhatEverYouWant1\n";
    ASSERT_EQ(6, 6);
 }
-
 TEST_F(COptionsTest, WhatEverYouWant2) {
     // The same here, define an object, do things and test result of member function
+    cerr << "Running test WhatEverYouWant2\n";
     ASSERT_EQ(-1.0, -1.0);
 }
-
 // Initialize the library and run all the tests defined
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
 //--------------------------------------------------------------------------------------------------------
-
 #endif // OLD_THOMAS_CODE
