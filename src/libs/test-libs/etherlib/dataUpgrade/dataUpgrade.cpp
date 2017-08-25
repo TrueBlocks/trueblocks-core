b4077214ea9412c96369db89ff23ac363cc4c91c
Thomas Jay Rush <jrush@greathill.com>
2017-08-25 11:21:19 -0400

    Fixes to dataUpgrade test cases.

--- src/libs/test-libs/etherlib/dataUpgrade/dataUpgrade.cpp
+++ src/libs/test-libs/etherlib/dataUpgrade/dataUpgrade.cpp
@@ -24,6 +24,11 @@ int main(int argc, const char *argv[]) {
     if (!options.prepareArguments(argc, argv))
         return 0;
 
+    if (!fileExists("./oldFmt.cache")) {
+        CFilename fileName("./oldFmt.cache");
+        return usage("File `" + fileName.getFullPath() + "' not found. All tests will fail without that file.");
+    }
+
     while (!options.commandList.empty()) {
         SFString command = nextTokenClear(options.commandList, '\n');
         if (!options.parseArguments(command))
@@ -47,16 +52,16 @@ bool testReadWrite(COptions& options) {
 
     switch (options.testNum) {
         case 0: {
-            ASSERT(fileExists("./oldFmt.bin"));
+            ASSERT(fileExists("./oldFmt.cache"));
             cout << "Read from old binary format and do nothing...\n";
-            readOneBlock_fromBinary(block, "./oldFmt.bin");
+            readOneBlock_fromBinary(block, "./oldFmt.cache");
             cout << block.Format() << "\n";
             break;
         }
         case 1: {
-            ASSERT(fileExists("./oldFmt.bin"));
+            ASSERT(fileExists("./oldFmt.cache"));
             cout << "Read from old binary format, write to JSON...\n";
-            readOneBlock_fromBinary(block, "./oldFmt.bin");
+            readOneBlock_fromBinary(block, "./oldFmt.cache");
             writeToJson(block, "./newFmt.json");
             ASSERT(fileExists("./newFmt.json"));
             cout << block.Format() << "\n";
@@ -68,16 +73,16 @@ bool testReadWrite(COptions& options) {
             cout << "Read from JSON, write to new binary format...\n";
             readOneBlock_fromJson(block, "./newFmt.json");
             newBlock = CNewBlock(block);
-            writeToBinary(newBlock, "./newFmt.bin");
-            ASSERT(fileExists("./newFmt.bin"));
+            writeToBinary(newBlock, "./newFmt.cache");
+            ASSERT(fileExists("./newFmt.cache"));
             cout << block.Format() << "\n";
             cout << newBlock.Format() << "\n";
             break;
         }
         case 3: {
-            ASSERT(fileExists("./newFmt.bin"));
+            ASSERT(fileExists("./newFmt.cache"));
             cout << "Read from new binary format, write to JSON...\n";
-            readOneNewBlock_fromBinary(newBlock, "./newFmt.bin");
+            readOneNewBlock_fromBinary(newBlock, "./newFmt.cache");
             writeToJson(newBlock, "./newFmt2.json");
             ASSERT(fileExists("./newFmt2.json"));
             cout << newBlock.Format() << "\n";
@@ -92,15 +97,15 @@ bool testReadWrite(COptions& options) {
             break;
         }
         case 5: {
-            ASSERT(fileExists("./oldFmt.bin"));
+            ASSERT(fileExists("./oldFmt.cache"));
             cout << "Read from old binary format, using new binary format...\n";
-            readOneNewBlock_fromBinary(newBlock, "./oldFmt.bin");
+            readOneNewBlock_fromBinary(newBlock, "./oldFmt.cache");
             cout << newBlock.Format() << "\n";
 
             // Clean up all but the original old format and make sure we did.
-            ASSERT( fileExists("./newFmt.bin"));
-            removeFile("./newFmt.bin");
-            ASSERT(!fileExists("./newFmt.bin"));
+            ASSERT( fileExists("./newFmt.cache"));
+            removeFile("./newFmt.cache");
+            ASSERT(!fileExists("./newFmt.cache"));
 
             ASSERT( fileExists("./newFmt.json"));
             removeFile("./newFmt.json");
