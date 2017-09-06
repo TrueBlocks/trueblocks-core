f015502a2e4c28ce5afa3e8b4b00da67a54e2704
Thomas Jay Rush <jrush@greathill.com>
2017-09-06 11:16:35 -0400

    Making small changes to the cache target (renamed to qb_config)

--- src/other/install/qb_config.cmake
+++ src/other/install/qb_config.cmake
@@ -1,9 +1,9 @@
 # Deploy basic configuration and directories structure for quickblocks
 
-message(STATUS "Running here...")
-
 set(QUICKBLOCKS_HOME "$ENV{HOME}/.quickBlocks")
 
+# create the files (will silently fail if already present)
+message(STATUS "Establishing configuration folders at ${QUICKBLOCKS_HOME}")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/configs")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/slurps")
@@ -14,7 +14,8 @@ file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/chifra")
 
 # Create toml file only if it does not exist
 set(QUICKBLOCKS_TOML_FILE "${QUICKBLOCKS_HOME}/quickBlocks.toml")
-if (NOT EXISTS  "${QUICKBLOCKS_TOML_FILE}")
-    file(COPY "${CMAKE_SOURCE_DIR}/../src/other/install/quickBlocks.toml" DESTINATION "${QUICKBLOCKS_HOME}" FILE_PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ)
+if (NOT EXISTS "${QUICKBLOCKS_TOML_FILE}")
+    message(STATUS "Copying configuration file ${QUICKBLOCKS_TOML_FILE}")
+    file(COPY "${CMAKE_SOURCE_DIR}/../../../src/other/install/quickBlocks.toml" DESTINATION "${QUICKBLOCKS_HOME}" FILE_PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ)
 endif()
 
