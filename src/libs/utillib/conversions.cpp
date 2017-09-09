905afa59ccb2e534700c66f1965676e822ef0864
Thomas Jay Rush <jrush@greathill.com>
2017-09-09 14:08:00 -0400

    Updating bloom to hex string conversion.

--- src/libs/utillib/conversions.cpp
+++ src/libs/utillib/conversions.cpp
@@ -10,86 +10,45 @@
 
 namespace qblocks {
 
-#ifndef NEW_CODE
-#define NEW_CODE
-#endif
-#ifdef NEW_CODE
-    class BigUnsignedInABase2 : public SFBigNumStore<unsigned short>
-    {
+    //------------------------------------------------------------------
+    class SFBloomHex : public SFBigNumStore<unsigned char> {
     public:
-        unsigned short base;
-        BigUnsignedInABase2(const SFUintBN &x);
-        operator string() const;
+        SFBloomHex(const SFUintBN& numIn);
+        SFString str;
     };
 
-    unsigned int bitLen2(unsigned int x) {
-        unsigned int len = 0;
-        while (x > 0) {
-            x >>= 1;
-            len++;
-        }
-        return len;
-    }
-
     //------------------------------------------------------------------
-    BigUnsignedInABase2::BigUnsignedInABase2(const SFUintBN &x)
-    {
-        this->base = 16;
+    SFBloomHex::SFBloomHex(const SFUintBN& numIn) {
+        len = 1024;
+        allocate(1024);
 
-        // Get an upper bound on how much space we need
-        int maxBitLenOfX    = (int)(x.len * SFUintBN::N);
-        int minBitsPerDigit = (int)bitLen2(16) - 1;
-        int maxDigitLenOfX  = (maxBitLenOfX + minBitsPerDigit - 1) / minBitsPerDigit;
-
-        len = (unsigned int)maxDigitLenOfX; // Another change to comply with `staying in bounds'.
-        allocate(len); // Get the space
-
-        SFUintBN x2(x), buBase(16);
+        SFUintBN x2(numIn);
         unsigned int digitNum = 0;
-
-        while (x2.len!=0)
+        while (x2.len != 0)
         {
-            // Get last digit.  This is like `lastDigit = x2 % buBase, x2 /= buBase'.
             SFUintBN lastDigit(x2);
-            lastDigit.divide(buBase, x2);
-            // Save the digit.
-            blk[digitNum] = lastDigit.to_ushort();
-            // Move on.  We can't run out of room: we figured it out above.
+            lastDigit.divide(16, x2);
+            blk[digitNum] = (unsigned char)lastDigit.to_ushort();
             digitNum++;
         }
-
-        // Save the actual length.
         len = digitNum;
-    }
 
-    //------------------------------------------------------------------
-    BigUnsignedInABase2::operator string(void) const
-    {
-        if (len==0)
-            return string("0");
-
-        char *s = new char[len+1];
-        memset(s,'\0',len+1);
-        for (unsigned int p=0;p<len;p++)
-        {
+        char s[1024+1];
+		memset(s,'\0',sizeof(s));
+        for (unsigned int p=0;p<len;p++) {
             unsigned short c = blk[len-1-p];
             s[p] = ((c < 10) ? char('0'+c) : char('A'+c-10));
         }
-
-        string ret = s;
-        delete [] s;
-        return ret;
+        str = s;
     }
 
+    //------------------------------------------------------------------
     SFString fromBloom(SFBloom bl) {
-        if (bl == 0)
-            return "0x0";
-        BigUnsignedInABase2 b2(bl);
-        SFString ret = b2.operator::string().c_str();
-        return ("0x"+padLeft(toLower(ret),512,'0'));
+        if (bl != 0) {
+            SFBloomHex b2(bl);
+            return ("0x"+padLeft(toLower(b2.str),512,'0'));
+        }
+        return "0x0";
     }
-#else
-    int unusedToAvoidWarnings = 0;
-#endif
 
 }  // namespace qblocks
