5f2deb945c59cf577792a497d0d8a9e1cbfff9df
Thomas Jay Rush <jrush@greathill.com>
2017-09-16 23:00:36 -0400

    Adding serialize test case for help fixing #226.

---
+++ src/libs/test-libs/etherlib/serialize/serialize.cpp
@@ -0,0 +1,73 @@
+#include "utillib.h"
+#include "options.h"
+#include "person.h"
+
+CPerson leader("Leader", 100);
+CPerson *lastAdded = &leader;
+
+int main(int argc, const char *argv[])
+{
+    // Tell the system where the blocks are and which version to use
+    etherlib_init("fastest");
+
+    // Parse command line, allowing for command files
+    COptions options;
+    if (!options.prepareArguments(argc, argv))
+        return 0;
+
+    //while (!options.commandList.empty())
+    {
+        SFString command = nextTokenClear(options.commandList, '\n');
+        if (!options.parseArguments(command))
+            return 0;
+
+        for (uint32_t i = 0 ; i < 10 ; i++) {
+            CPerson *newPerson = new CPerson("Person " + asStringU(i), i * 2);
+            lastAdded->next = newPerson;
+            lastAdded = newPerson;
+        }
+
+        cout << "Creation\n" << SFString('-', 80) << "\n";
+        CPerson *cur = &leader;
+        while (cur) {
+            cout << cur->name << " : " << cur->age << "\n";
+            cur = cur->next;
+        }
+        cout.flush();
+
+        SFArchive out(WRITING_ARCHIVE);
+        if (out.Lock("./file.bin", binaryWriteCreate, LOCK_WAIT)) {
+            leader.Serialize(out);
+            out.Release();
+        }
+
+        cout << "\nReset\n" << SFString('-', 80) << "\n";
+        leader = CPerson();
+        cur = &leader;
+        while (cur) {
+            cout << cur->name << " : " << cur->age << "\n";
+            cur = cur->next;
+        }
+        cout.flush();
+
+        SFArchive in(READING_ARCHIVE);
+        if (in.Lock("./file.bin", binaryReadOnly, LOCK_WAIT)) {
+            leader.Serialize(in);
+            in.Release();
+        }
+
+        cout << "\nRead in data\n" << SFString('-', 80) << "\n";
+        cur = &leader;
+        while (cur) {
+            cout << cur->name << " : " << cur->age << "\n";
+            cur = cur->next;
+        }
+        cout.flush();
+
+        CPerson::registerClass();
+        cout << leader.Format() << "\n";
+
+        removeFile("./file.bin");
+    }
+    return 0;
+}
