31b101de418c3b7176eb0b47095f3de6208bb997
Thomas Jay Rush <jrush@greathill.com>
2017-09-24 22:11:38 -0400

    Cleaning up (or trying to) the fromBloom code which is really slow.

--- src/libs/utillib/conversions.cpp
+++ src/libs/utillib/conversions.cpp
@@ -17,22 +17,25 @@ namespace qblocks {
         SFString str;
     };
 
+#define OLD_CODE 1
     //------------------------------------------------------------------
     SFBloomHex::SFBloomHex(const SFUintBN& numIn) {
+
         len = 1024;
         allocate(1024);
 
         SFUintBN x2(numIn);
-        unsigned int digitNum = 0;
+        unsigned int nDigits = 0;
         while (x2.len != 0)
         {
             SFUintBN lastDigit(x2);
             lastDigit.divide(16, x2);
-            blk[digitNum] = (unsigned char)lastDigit.to_ushort();
-            digitNum++;
+            blk[nDigits] = (unsigned char)lastDigit.to_ushort();
+            nDigits++;
         }
-        len = digitNum;
+        len = nDigits;
 
+#ifdef OLD_CODE
         char s[1024+1];
 		memset(s,'\0',sizeof(s));
         for (unsigned int p=0;p<len;p++) {
@@ -40,15 +43,27 @@ namespace qblocks {
             s[p] = ((c < 10) ? char('0'+c) : char('A'+c-10));
         }
         str = s;
+#else
+        str.reserve(1025);
+        char *pStr = (char*)(const char*)str.c_str();
+        memset(pStr,'\0',1025);
+        for (unsigned int p = 0 ; p < len ; p++) {
+            unsigned short c = blk[len-1-p];
+            pStr[p] = ((c < 10) ? char('0'+c) : char('a'+c-10));
+        }
+#endif
     }
 
     //------------------------------------------------------------------
-    SFString fromBloom(SFBloom bl) {
-        if (bl != 0) {
-            SFBloomHex b2(bl);
-            return ("0x"+padLeft(toLower(b2.str),512,'0'));
-        }
-        return "0x0";
+    SFString fromBloom(const SFBloom& bl) {
+        if (bl == 0)
+            return "0x0";
+        SFBloomHex b2(bl);
+#ifdef OLD_CODE
+        return ("0x" + padLeft(toLower(b2.str),512,'0'));
+#else
+        return ("0x" + padLeft(b2.str,512,'0'));
+#endif
     }
 
     //----------------------------------------------------------------------------------------------------
