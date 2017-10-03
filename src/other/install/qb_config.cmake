d2ae98bdca38dcfb653233e2517012a27c019bb9
Thomas Jay Rush <jrush@greathill.com>
2017-10-02 23:05:23 -0400

    Fix to ethName to install the names.txt file.

--- src/other/install/qb_config.cmake
+++ src/other/install/qb_config.cmake
@@ -7,6 +7,7 @@ message(STATUS "Establishing configuration folders at ${QUICKBLOCKS_HOME}")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/cache")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/cache/abis")
+file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/cache/names")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/makeClass")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/grabABI")
 file(MAKE_DIRECTORY "${QUICKBLOCKS_HOME}/chifra")
@@ -16,6 +17,13 @@ set(QUICKBLOCKS_TOML_FILE "${QUICKBLOCKS_HOME}/quickBlocks.toml")
 if (NOT EXISTS "${QUICKBLOCKS_TOML_FILE}")
     message(STATUS "Copying configuration file ${QUICKBLOCKS_TOML_FILE}")
     file(COPY "${CMAKE_SOURCE_DIR}/../../../src/other/install/quickBlocks.toml" DESTINATION "${QUICKBLOCKS_HOME}" FILE_PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ)
+endif()
+
+# Create names fie only if it does not exist
+set(QUICKBLOCKS_NAMES_FILE "${QUICKBLOCKS_HOME}/cache/names/names.txt")
+if (NOT EXISTS "${QUICKBLOCKS_NAMES_FILE}")
+    message(STATUS "Copying names file ${QUICKBLOCKS_NAMES_FILE}")
+    file(COPY "${CMAKE_SOURCE_DIR}/../../../src/other/install/names.txt" DESTINATION "${QUICKBLOCKS_HOME}/cache/names" FILE_PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ)
 endif()
 
 # makeClass content
