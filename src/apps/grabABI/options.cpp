a702c39f15bdf7664998c46fc9df9c1cb192d715
Thomas Jay Rush <jrush@greathill.com>
2017-10-14 12:24:20 -0400

    Fixing issue #2

--- src/apps/grabABI/options.cpp
+++ src/apps/grabABI/options.cpp
@@ -15,6 +15,7 @@ CParams params[] = {
     CParams("-encode",    "generate the encodings for the functions / events in the ABI"),
     CParams("-noconst",   "generate encodings for non-constant functions and events only (always true when generating)"), // NOLINT
     CParams("-open",      "open the ABI file for editing, download if not already present"),
+    CParams("-raw",       "force retrieval of ABI from etherscan (ignoring cache)"),
     CParams("@-json",     "print the ABI to the screen as json"),
     CParams("@-silent",   "If ABI cannot be acquired, fail silently (useful for scripting)"),
     CParams("",           "Fetches the ABI for a smart contract. Optionally generates C++ source code "
@@ -49,6 +50,9 @@ bool COptions::parseArguments(SFString& command) {
 
         } else if (arg == "-n" || arg == "--noconst") {
             noconst = true;
+
+        } else if (arg == "-r" || arg == "--raw") {
+            raw = true;
 
         } else if (arg == "-o" || arg == "--open") {
             open = true;
@@ -96,6 +100,7 @@ void COptions::Init(void) {
     open = false;
     silent = false;
     asJson = false;
+    raw = false;
     for (uint32_t i = 0 ; i < MAX_ADDRS ; i++) {
         addrs[i] = "";
     }
