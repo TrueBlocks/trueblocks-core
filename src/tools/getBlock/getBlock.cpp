87ad596ef7e36dd94b99eb4853516f74da4ff838
Thomas Jay Rush <jrush@greathill.com>
2017-10-21 22:41:21 -0400

    Small cleanups

--- src/tools/getBlock/getBlock.cpp
+++ src/tools/getBlock/getBlock.cpp
@@ -124,7 +124,7 @@ SFString doOneBlock(SFUint32 num, const COptions& opt) {
 
         if (getSource().Contains("Only")) {
             // --source::cache mode doesn't include timestamp in transactions
-            for (txnum_t t = 0 ; t < gold.transactions.getCount() ; t++) {
+            for (uint32_t t = 0 ; t < gold.transactions.getCount() ; t++) {
                 gold.transactions[t].timestamp = gold.timestamp;
             }
 
