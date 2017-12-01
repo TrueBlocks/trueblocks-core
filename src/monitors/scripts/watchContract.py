ab8ca2b8079b860fc2c6eb749d24968948a8e559
Thomas Jay Rush <jrush@greathill.com>
2017-12-01 07:17:03 -0500

    Fixing watchContract.py which, it turns out, didn't need to be killing Parity after all.

--- src/monitors/scripts/watchContract.py
+++ src/monitors/scripts/watchContract.py
@@ -96,14 +96,14 @@ def get_parity_pid():
 # Guard timeout while parity is starting
 startup_timeout = 5
 # Threshold to restart parity (When space left percentage is less than this value)
-percentage_threshold = 10
+percentage_threshold = 3
 # Parity process name
 parity_proc_name = 'parity'
 # Contracts and bin paths (can be absolute or relative path, by default using relative one present at bash script)
 contracts_path = '../monitors'
 contracts_bin_subpath = 'bin'
 # Timeout (in seconds) for periodic parity restart and guard time to wait till it is completelly stopped
-parity_restart_periodicity = 7200 # (120 min * 60 sec)
+parity_restart_periodicity = 7200000 # (120 min * 60 sec = 2 hours) * 10 * 10 * 10 == 2000 hours (never...)
 parity_stop_guard_time = 20
 
 #---------------------
@@ -129,51 +129,50 @@ else:
      exit(2)
 
 while True:
-    # Get last parity restart timestamp = now
-    last_restart_time = int(time.time())
-
-    # Start parity if it is NOT running, otherwise start watching logic directly (parity is alread running)
-    is_running = is_parity_running()
-    if(is_running != True):
-        print("Starting Parity...")
-        proc = subprocess.Popen([parity_proc_name, '--tracing', 'on', '--pruning', 'archive'])
-        print("Parity running with pid {0}, waiting {1} seconds before running watcher".format(proc.pid, startup_timeout))
-        time.sleep(startup_timeout)
-    else:
-        print("Parity is already running")
-
-    while True:
-        # Get the occupation percentage at current filesystem (in GB)
-        print("Cheking filesystem occupation...")
-        s = os.statvfs(getmount('.'))
-        gigs_left = (s.f_bavail * s.f_frsize) / 1024 / 1024 / 1024
-        gigs_total_space = (s.f_blocks * s.f_frsize) / 1024 / 1024 / 1024
-        percentage_left = (gigs_left * 100/gigs_total_space)
-        # When we have less than 10% left - restart parity
-        if percentage_left < percentage_threshold:
-            break
-        else:
-            # we can run the watcher
-            # After watchers we check if the parity restart is required or we can continue
-            runwatcher()
-
-            now_time = int(time.time())
-            elapsed_time = (now_time - last_restart_time)
-
-            #print("Elapsed {0} seconds since last restart, restart scheduled every {1} seconds".format(elapsed_time, parity_restart_periodicity))
-
-            if elapsed_time >= parity_restart_periodicity:
-                last_restart_time = now_time # keep track of last restart
-                break
-
-            time.sleep(float(timeout))
-    print("Restarting Parity...")
-    # Get PID based on process name, this way we can also kill it when already running
-    pid = get_parity_pid()
-    os.kill(pid, signal.SIGINT) #Always running at this point
-    if not is_running:
-        proc.wait()
-    else:
-        # Wait enough time after kill
-        #print("Waiting {0} seconds till parity is completely stopped".format(parity_stop_guard_time))
-        time.sleep(parity_stop_guard_time)
+#    # Get last parity restart timestamp = now
+#    last_restart_time = int(time.time())
+#
+#    # Start parity if it is NOT running, otherwise start watching logic directly (parity is alread running)
+#    is_running = is_parity_running()
+#    if(is_running != True):
+#        print("Starting Parity...")
+#        proc = subprocess.Popen([parity_proc_name, '--tracing', 'on', '--pruning', 'archive'])
+#        print("Parity running with pid {0}, waiting {1} seconds before running watcher".format(proc.pid, startup_timeout))
+#        time.sleep(startup_timeout)
+#    else:
+#        print("Parity is already running")
+#
+#    while True:
+#        # Get the occupation percentage at current filesystem (in GB)
+#        print("Cheking filesystem occupation...")
+#        s = os.statvfs(getmount('.'))
+#        gigs_left = (s.f_bavail * s.f_frsize) / 1024 / 1024 / 1024
+#        gigs_total_space = (s.f_blocks * s.f_frsize) / 1024 / 1024 / 1024
+#        percentage_left = (gigs_left * 100/gigs_total_space)
+#        # When we have less than 10% left - restart parity
+#        if percentage_left < percentage_threshold:
+#            break
+#        else:
+#            # we can run the watcher
+#            # After watchers we check if the parity restart is required or we can continue
+    runwatcher()
+#            now_time = int(time.time())
+#            elapsed_time = (now_time - last_restart_time)
+#
+#            #print("Elapsed {0} seconds since last restart, restart scheduled every {1} seconds".format(elapsed_time, parity_restart_periodicity))
+#
+#            if elapsed_time >= parity_restart_periodicity:
+#                last_restart_time = now_time # keep track of last restart
+#                break
+#
+    time.sleep(float(timeout))
+#    print("Restarting Parity...")
+#    # Get PID based on process name, this way we can also kill it when already running
+#    pid = get_parity_pid()
+#    os.kill(pid, signal.SIGINT) #Always running at this point
+#    if not is_running:
+#        proc.wait()
+#    else:
+#        # Wait enough time after kill
+#        #print("Waiting {0} seconds till parity is completely stopped".format(parity_stop_guard_time))
+#        time.sleep(parity_stop_guard_time)
