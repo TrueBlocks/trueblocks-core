9030ba7f970b3efbf48ad5430d105de0133cae97
Thomas Jay Rush <jrush@greathill.com>
2017-12-27 08:00:54 -0500

    Consilodation of miniblock code

--- src/libs/etherlib/miniblock.cpp
+++ src/libs/etherlib/miniblock.cpp
@@ -139,6 +139,21 @@ namespace qblocks {
     }
 
     //--------------------------------------------------------------------------
+    inline SFString TIMER_IN(double& st) {
+        CStringExportContext ctx;
+        ctx << (qbNow()-st) << ": ";
+        st = qbNow();
+        return ctx.str;
+    }
+
+    //-------------------------------------------------------------------------
+    inline SFString TIMER_TICK(double st) {
+        CStringExportContext ctx;
+        ctx << "in " << cGreen << (qbNow()-st) << cOff << " seconds.";
+        return ctx.str;
+    }
+
+    //--------------------------------------------------------------------------
     bool CInMemoryCache::Load(blknum_t _start, blknum_t _count) {
 
         double startTime = qbNow();
