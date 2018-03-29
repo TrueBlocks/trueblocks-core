51c22a8ccf0655b3947ca45835f3ba48955f3c7e
Thomas Jay Rush <jrush@greathill.com>
2018-03-29 18:48:50 -0400

    Adding --bloom and --account to whereBlock

--- src/tools/whereBlock/whereBlock.cpp
+++ src/tools/whereBlock/whereBlock.cpp
@@ -26,41 +26,32 @@ int main(int argc, const char *argv[]) {
         if (!options.parseArguments(command))
             return 0;
 
-        if (!options.alone) {
-            SFString cachePath = blockCachePath("");
-            if (isTestMode())
-                cachePath = "--";
-            cout << cYellow << "\nReport on block locations:" << cOff << (verbose ? "" : "\n  (cache folder: " + cachePath + ")") << "\n";
-        }
+        SFString cachePath = blockCachePath("").Substitute("/blocks/", "/"+options.mode+"s/");
+        if (isTestMode())
+            cachePath = "--";
+        cout << cYellow << "\nReport on " << options.mode << " locations:" << cOff << (verbose ? "" : "\n  (cache folder: " + cachePath + ")") << "\n";
 
         SFString list = options.getBlockNumList();
         while (!list.empty()) {
             blknum_t bn = toLongU(nextTokenClear(list, '|'));
-            CFilename fileName(getBinaryFilename(bn));
+            CFilename fileName(getBinaryFilename(bn).Substitute("/blocks/", "/"+options.mode+"s/"));
             bool exists = fileExists(fileName.getFullPath());
+            cout << fileName.getFullPath() << " " << exists << "\n";
 
-            if (options.alone) {
-                // When running in 'alone' mode, only report items in the cache
-                if (exists)
-                    cout << fileName.getFullPath() << "\n";
+            SFString path = (verbose ? fileName.getFullPath() : fileName.relativePath(blockCachePath("").Substitute("/blocks/", "/"+options.mode+"s/")));
+            SFString vers = getVersionFromClient();
+            if (isTestMode() && verbose)
+                path = "--";
+            if (isTestMode())
+                vers = "--";
+            SFString fallback = getenv("FALLBACK");
+            bool running_node = isNodeRunning();
 
-            } else {
-
-                SFString path = (verbose ? fileName.getFullPath() : fileName.relativePath(blockCachePath("")));
-                SFString vers = getVersionFromClient();
-                if (isTestMode() && verbose)
-                    path = "--";
-                if (isTestMode())
-                    vers = "--";
-                SFString fallback = getenv("FALLBACK");
-                bool running_node = isNodeRunning();
-
-                cout << "\tblock " << cTeal << padLeft(asStringU(bn),9) << cOff << " ";
-                     if (exists)            cout << "found at cache:  " << cTeal << path << cOff << "\n";
-                else if (running_node)      cout << "found at node:   " << vers << "\n";
-                else if (!fallback.empty()) cout << "found at remote: " << fallback << "\n";
-                else                        cout << "was not found\n";
-            }
+            cout << "\t" << options.mode << " " << cTeal << padLeft(asStringU(bn),9) << cOff << " ";
+            if (exists)            cout << "found at cache:  " << cTeal << path << cOff << "\n";
+            else if (running_node)      cout << "found at node:   " << vers << "\n";
+            else if (!fallback.empty()) cout << "found at remote: " << fallback << "\n";
+            else                        cout << "was not found\n";
         }
     }
     return 0;
