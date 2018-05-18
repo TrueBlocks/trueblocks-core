004bb0162d9d3581f051bef1f1a3dc0bd5e4486b
Thomas Jay Rush <jrush@greathill.com>
2018-05-18 16:53:45 -0400

    Fixing getBloom tool to handle EABs properly.

--- src/tools/getBloom/getBloom.cpp
+++ src/tools/getBloom/getBloom.cpp
@@ -13,7 +13,7 @@ extern SFString doOneBloom(uint64_t num, const COptions& opt);
 //------------------------------------------------------------
 int main(int argc, const char * argv[]) {
 
-    etherlib_init();
+    etherlib_init(quickQuitHander);
 
     // Parse command line, allowing for command files
     COptions options;
@@ -25,87 +25,106 @@ int main(int argc, const char * argv[]) {
         if (!options.parseArguments(command))
             return 0;
 
-        if (!options.quiet)
-            cout << (options.isMulti() ? "[" : "");
-
-        int cnt=0;
+        cout << (options.isMulti() ? "[" : "");
         SFString list = options.getBlockNumList();
         while (!list.empty()) {
             blknum_t bn = toLongU(nextTokenClear(list, '|'));
             SFString result = doOneBloom(bn, options);
-            if (options.isCheck) {
-                // Not implemented
-            } else {
-                if (!options.quiet) {
-                    cout << result;
-                    if (!list.empty())
-                        cout << ",";
-                    cout << "\n";
-
-                } else if (!(cnt%150)) {
-                    cout << ".";
-                    cout.flush();
-
-                } else if (!(cnt%1000)) {
-                    cout << "+";
-                    cout.flush();
-                }
-                cnt++;
+            cout << result;
+            if (!options.asBars) {
+                if (!list.empty())
+                    cout << ",";
+                cout << "\n";
             }
         }
-
-        if (!options.quiet)
-            cout << (options.isMulti() ? "]" : "");
+        cout << (options.isMulti() ? "]" : "");
     }
 
     return 0;
 }
 
-//------------------------------------------------------------
+SFString toBits(const SFString& blIn) {
+    SFString ret = toLower(blIn).substr(2);
+    ret.ReplaceAll("0","0000");
+    ret.ReplaceAll("1","0001");
+    ret.ReplaceAll("2","0010");
+    ret.ReplaceAll("3","0011");
+    ret.ReplaceAll("4","0100");
+    ret.ReplaceAll("5","0101");
+    ret.ReplaceAll("6","0110");
+    ret.ReplaceAll("7","0111");
+    ret.ReplaceAll("8","1000");
+    ret.ReplaceAll("9","1001");
+    ret.ReplaceAll("a","1010");
+    ret.ReplaceAll("b","1011");
+    ret.ReplaceAll("c","1100");
+    ret.ReplaceAll("d","1101");
+    ret.ReplaceAll("e","1110");
+    ret.ReplaceAll("f","1111");
+    return "0x"+ret;
+}
+SFString asBar(const SFString& blIn) {
+    return toBits(blIn).substr(2).Substitute("0", "").Substitute("1", "-").Substitute("---", "🁡");
+}
+
+#include "bloom_blocks.h"
 SFString doOneBloom(uint64_t num, const COptions& opt) {
 
     CBlock gold;
     gold.blockNumber = num;
     SFString result;
     SFString numStr = asStringU(num);
+
     if (opt.isRaw) {
 
-        if (!queryRawBlock(result, numStr, true, false)) {
-            result = "Could not query raw block " + numStr + ". Is an Ethereum node running?";
+        if (!opt.receipt) {
+            HIDE_FIELD(CBloomBlock, "transactions");
+        } else {
+            HIDE_FIELD(CBloomBlock, "number");
+            HIDE_FIELD(CBloomBlock, "logsBloom");
         }
-        result.ReplaceAll(",",",\n");
-        SFString ret;
-        while (!result.empty()) {
-            SFString line = nextTokenClear(result,'\n');
-            if (line.Contains("logsBloom") || line.Contains("number")) {
-                ret += line + "\n";
+
+        SFString r = getRawBlock(num);
+        CBloomBlock rawBlock;
+        rawBlock.parseJson(cleanUpJson((char*)r.c_str()));
+        HIDE_FIELD(CBloomTrans,"hash");
+
+        if (opt.asBits) {
+            rawBlock.logsBloom = toBits(rawBlock.logsBloom);
+            for (uint32_t i = 0 ; i < rawBlock.transactions.getCount() ; i++)
+                rawBlock.transactions[i].receipt.logsBloom = toBits(rawBlock.transactions[i].receipt.logsBloom);
+        }
+        if (opt.asBars) {
+            ostringstream os;
+            if (opt.receipt)
+                os << "\n" << SFString('-',90) << " " << rawBlock.number << SFString('-',90) << "\n";
+            else
+                os << num << ": ";
+            os << asBar(rawBlock.logsBloom) << "\n";
+            for (uint32_t i = 0 ; i < rawBlock.transactions.getCount() ; i++) {
+                if (opt.receipt) {
+                    SFString x = asBar(rawBlock.transactions[i].receipt.logsBloom);
+                    if (!x.empty())
+                        os << x << "\n";
+                }
             }
+            return os.str().c_str();
         }
-        result = "{" + Strip(ret.Substitute(",\n",","),',') + "}";
+
+        ostringstream os;
+        rawBlock.doExport(os);
+        return os.str().c_str();
 
     } else {
 
-        // queryBlock returns false if there are no transactions, so ignore the return value
-        if (opt.isCache) {
-            readBlockFromBinary(gold, getBinaryFilename(gold.blockNumber));
-            // --source::cache mode doesn't include timestamp in transactions
-            for (uint32_t t = 0 ; t < gold.transactions.getCount() ; t++) {
-                gold.transactions[t].timestamp = gold.timestamp;
-            }
-
-        } else {
-            queryBlock(gold, numStr, true, false);
-            if (/* DISABLES CODE */ (false)) { // turn this on to force a write of the block to the disc
-                CBlock latest;
-                getBlock(latest, "latest");
-                SFString fileName = getBinaryFilename(gold.blockNumber);
-                gold.finalized = isBlockFinal(gold.timestamp, latest.timestamp);
-                writeBlockToBinary(gold, fileName);
-            }
+        SFBloomArray blooms;
+        readBloomArray(blooms, getBinaryFilename(num).Substitute("/blocks/", "/blooms/"));
+        ostringstream os;
+        os << "\n" << SFString('-',90) << " " << num << SFString('-',90) << "\n";
+        for (uint32_t i = 0 ; i < blooms.getCount(); i++) {
+            os << asBar(bloom2Bits(blooms[i])) << "\n";
         }
-
-        result = gold.Format();
+        result = os.str().c_str();
     }
-
-    return (opt.quiet ? "" : result);
+    return result;
 }
