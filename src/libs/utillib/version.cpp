4e28697f9202a769aa980de886c82fe4fbcf10de
Thomas Jay Rush <jrush@quickblocks.io>
2018-06-04 19:47:46 -0400

    Updating version

--- src/libs/utillib/version.cpp
+++ src/libs/utillib/version.cpp
@@ -17,7 +17,7 @@ namespace qblocks {
 
 #define MAJOR 0
 #define MINOR 4
-#define BUILD 5
+#define BUILD 6
 #define SUBVERS "alpha"
     //--------------------------------------------------------------------------------
     uint32_t getVersionNum(void) {
