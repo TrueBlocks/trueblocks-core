b0e937660c7c5d8b58580e926467336db916578e
Thomas Jay Rush <jrush@quickblocks.io>
2018-06-29 15:51:04 -0400

    Cleaning up a few test cases and examples

--- src/other/traceCheck/traceCheck.cpp
+++ src/other/traceCheck/traceCheck.cpp
@@ -1,40 +1,37 @@
-/*-------------------------------------------------------------------------
- * This source code is confidential proprietary information which is
- * Copyright (c) 2017 by Great Hill Corporation.
- * All Rights Reserved
+/*-------------------------------------------------------------------------------------------
+ * QuickBlocks - Decentralized, useful, and detailed data from Ethereum blockchains
+ * Copyright (c) 2018 Great Hill Corporation (http://quickblocks.io)
  *
- * The LICENSE at the root of this repo details your rights (if any)
- *------------------------------------------------------------------------*/
+ * This program is free software: you may redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version. This program is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ *-------------------------------------------------------------------------------------------*/
 #include "traceCheck.h"
 
-extern bool visitBlock(uint64_t num, void *data);
-extern bool visitTransaction(CTransaction& trans, void *data);
-extern bool visitTrace(CTrace& trace, void *data);
 //--------------------------------------------------------------
 int main(int argc, const char *argv[]) {
 
     etherlib_init(quickQuitHander);
 
-    // Parse command line, allowing for command files
     COptions options;
     if (!options.prepareArguments(argc, argv))
         return 0;
 
-    string_q checkResults;
-    while (!options.commandList.empty()) {
-        string_q command = nextTokenClear(options.commandList, '\n');
-        if (!options.parseArguments(command))
-            return 0;
+    string_q command = nextTokenClear(options.commandList, '\n');
+    if (!options.parseArguments(command))
+        return 0;
 
-        options.blocks.forEveryBlockNumber(visitBlock, NULL);
-    }
+    options.blocks.forEveryBlockNumber(visitBlock, NULL);
 
     return 0;
 }
 
 //--------------------------------------------------------------
 bool visitBlock(uint64_t num, void *data) {
-
     CBlock block;
     getBlock(block, num);
     return forEveryTransactionInBlock(visitTransaction, &block, block);
@@ -52,13 +49,10 @@ bool visitTransaction(CTransaction& trans, void *data) {
 
 //--------------------------------------------------------------
 extern bool visitTrace(CTrace& trace, void *data) {
-
     string_q str = substitute(string_q(trace.traceAddress.size()+1, '.'), ".", "  ");
     replaceReverse(str, "  ", cYellow + "--- " + cOff);
     cout << str;
     cout << trace.action.from << cTeal << " [" << trace.action.callType << "] " << cOff;
     cout << trace.action.to << " (" << trace.result.gasUsed << ") " << trace.action.input.length() << ".\n";
-
     return true;
 }
-
