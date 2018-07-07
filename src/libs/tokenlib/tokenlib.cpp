1190b9edb64245cfe501da2660314cff8b4b7ca7
Thomas Jay Rush <jrush@quickblocks.io>
2018-07-06 22:05:55 -0400

    Closing issue #597

--- src/libs/tokenlib/tokenlib.cpp
+++ src/libs/tokenlib/tokenlib.cpp
@@ -15,9 +15,14 @@
 //-----------------------------------------------------------------------
 void tokenlib_init(void) {
     QApprovalEvent::registerClass();
+    QApprovalForAllEvent::registerClass();
     QTransferEvent::registerClass();
     QApprove::registerClass();
     QApproveAndCall::registerClass();
+    QOwnerOf::registerClass();
+    QSafeTransferFrom::registerClass();
+    QSafeTransferFromToke::registerClass();
+    QSetApprovalForAll::registerClass();
     QTransfer::registerClass();
     QTransferFrom::registerClass();
     walletlib_init();
@@ -29,7 +34,14 @@ const string_q func_approve_qb = "0x095ea7b3";
 const string_q func_approveAndCall_qb = "0xcae9ca51";
 const string_q func_balanceOf_qb = "0x70a08231";
 const string_q func_decimals_qb = "0x313ce567";
+const string_q func_exists_qb = "0x4f558e79";
+const string_q func_getApproved_qb = "0x081812fc";
+const string_q func_isApprovedForAll_qb = "0xe985e9c5";
 const string_q func_name_qb = "0x06fdde03";
+const string_q func_ownerOf_qb = "0x6352211e";
+const string_q func_safeTransferFrom_qb = "0x42842e0e";
+const string_q func_safeTransferFromToke_qb = "0xb88d4fde";
+const string_q func_setApprovalForAll_qb = "0xa22cb465";
 const string_q func_symbol_qb = "0x95d89b41";
 const string_q func_totalSupply_qb = "0x18160ddd";
 const string_q func_transfer_qb = "0xa9059cbb";
@@ -73,6 +85,58 @@ const CTransaction *promoteToToken(const CTransaction *p) {
             a->function = toFunction("approveAndCall", params, nItems, items);
             return a;
 
+        } else if (encoding == func_ownerOf_qb) {
+            // function ownerOf(uint256 _tokenId)
+            // 0x6352211e
+            QOwnerOf *a = new QOwnerOf;
+            *(CTransaction*)a = *p;  // NOLINT
+            a->_tokenId = toWei("0x"+extract(params, 0*64, 64));
+            items[nItems++] = "uint256";
+            a->function = toFunction("ownerOf", params, nItems, items);
+            return a;
+
+        } else if (encoding == func_safeTransferFrom_qb) {
+            // function safeTransferFrom(address _from, address _to, uint256 _tokenId)
+            // 0x42842e0e
+            QSafeTransferFrom *a = new QSafeTransferFrom;
+            *(CTransaction*)a = *p;  // NOLINT
+            a->_from = toAddress(extract(params, 0*64, 64));
+            a->_to = toAddress(extract(params, 1*64, 64));
+            a->_tokenId = toWei("0x"+extract(params, 2*64, 64));
+            items[nItems++] = "address";
+            items[nItems++] = "address";
+            items[nItems++] = "uint256";
+            a->function = toFunction("safeTransferFrom", params, nItems, items);
+            return a;
+
+        } else if (encoding == func_safeTransferFromToke_qb) {
+            // function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes _data)
+            // 0xb88d4fde
+            QSafeTransferFromToke *a = new QSafeTransferFromToke;
+            *(CTransaction*)a = *p;  // NOLINT
+            a->_from = toAddress(extract(params, 0*64, 64));
+            a->_to = toAddress(extract(params, 1*64, 64));
+            a->_tokenId = toWei("0x"+extract(params, 2*64, 64));
+            a->_data = extract(params, 3*64);
+            items[nItems++] = "address";
+            items[nItems++] = "address";
+            items[nItems++] = "uint256";
+            items[nItems++] = "bytes";
+            a->function = toFunction("safeTransferFromToke", params, nItems, items);
+            return a;
+
+        } else if (encoding == func_setApprovalForAll_qb) {
+            // function setApprovalForAll(address _operator, bool _approved)
+            // 0xa22cb465
+            QSetApprovalForAll *a = new QSetApprovalForAll;
+            *(CTransaction*)a = *p;  // NOLINT
+            a->_operator = toAddress(extract(params, 0*64, 64));
+            a->_approved = toLong(extract(params, 1*64, 64));
+            items[nItems++] = "address";
+            items[nItems++] = "bool";
+            a->function = toFunction("setApprovalForAll", params, nItems, items);
+            return a;
+
         } else if (encoding == func_transfer_qb) {
             // function transfer(address _to, uint256 _value)
             // 0xa9059cbb
@@ -112,6 +176,7 @@ const CTransaction *promoteToToken(const CTransaction *p) {
 
 //-----------------------------------------------------------------------
 const string_q evt_Approval_qb = "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925";
+const string_q evt_ApprovalForAll_qb = "0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31";
 const string_q evt_Transfer_qb = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
 //-----------------------------------------------------------------------
@@ -133,6 +198,16 @@ const CLogEntry *promoteToTokenEvent(const CLogEntry *p) {
             a->_owner = toAddress(nTops > 1 ? fromTopic(p->topics[1]) : "");
             a->_spender = toAddress(nTops > 2 ? fromTopic(p->topics[2]) : "");
             a->_value = toWei("0x" + extract(data, 0*64, 64));
+            return a;
+
+        } else if (fromTopic(p->topics[0]) % evt_ApprovalForAll_qb) {
+            // event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved)
+            // 0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31
+            QApprovalForAllEvent *a = new QApprovalForAllEvent;
+            *(CLogEntry*)a = *p;  // NOLINT
+            a->_owner = toAddress(nTops > 1 ? fromTopic(p->topics[1]) : "");
+            a->_operator = toAddress(nTops > 2 ? fromTopic(p->topics[2]) : "");
+            a->_approved = toLong("0x" + extract(data, 0*64, 64));
             return a;
 
         } else if (fromTopic(p->topics[0]) % evt_Transfer_qb) {
@@ -158,6 +233,6 @@ const CLogEntry *promoteToTokenEvent(const CLogEntry *p) {
 
 /*
  ABI for addr : 0xTokenLib
-[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"_initialAmount","type":"uint256"},{"name":"_tokenName","type":"string"},{"name":"_decimalUnits","type":"uint8"},{"name":"_tokenSymbol","type":"string"}],"type":"constructor"},{"payable":false,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]
+[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"name":"_owner","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"exists","outputs":[{"name":"_exists","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"name":"_operator","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_operator","type":"address"},{"name":"_approved","type":"bool"}],"name":"setApprovalForAll","outputs":[{"name":"x","type":"y"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"name":"_ret","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"_initialAmount","type":"uint256"},{"name":"_tokenName","type":"string"},{"name":"_decimalUnits","type":"uint8"},{"name":"_tokenSymbol","type":"string"}],"type":"constructor"},{"payable":false,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_operator","type":"address"},{"indexed":false,"name":"_approved","type":"bool"}],"name":"ApprovalForAll","type":"event"}]
 
 */
