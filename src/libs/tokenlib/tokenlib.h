e46b40ea2e8e93114a370e790f2348e8266eeb7c
Thomas Jay Rush <jrush@quickblocks.io>
2018-07-06 22:05:55 -0400

    Closing issue #597

--- src/libs/tokenlib/tokenlib.h
+++ src/libs/tokenlib/tokenlib.h
@@ -17,9 +17,14 @@
 #include "walletlib.h"
 #endif
 #include "approvalevent.h"
+#include "approvalforallevent.h"
 #include "transferevent.h"
 #include "approve.h"
 #include "approveandcall.h"
+#include "ownerof.h"
+#include "safetransferfrom.h"
+#include "safetransferfromtoke.h"
+#include "setapprovalforall.h"
 #include "transfer.h"
 #include "transferfrom.h"
 
@@ -34,7 +39,14 @@ extern const string_q func_approve_qb;
 extern const string_q func_approveAndCall_qb;
 extern const string_q func_balanceOf_qb;
 extern const string_q func_decimals_qb;
+extern const string_q func_exists_qb;
+extern const string_q func_getApproved_qb;
+extern const string_q func_isApprovedForAll_qb;
 extern const string_q func_name_qb;
+extern const string_q func_ownerOf_qb;
+extern const string_q func_safeTransferFrom_qb;
+extern const string_q func_safeTransferFromToke_qb;
+extern const string_q func_setApprovalForAll_qb;
 extern const string_q func_symbol_qb;
 extern const string_q func_totalSupply_qb;
 extern const string_q func_transfer_qb;
@@ -43,6 +55,7 @@ extern const string_q func_version_qb;
 
 //------------------------------------------------------------------------
 extern const string_q evt_Approval_qb;
+extern const string_q evt_ApprovalForAll_qb;
 extern const string_q evt_Transfer_qb;
 
 // EXISTING_CODE
@@ -52,11 +65,18 @@ inline bool isTokenRelated(const CTransaction *trans) {
             startsWith(trans->input, func_transfer_qb) ||
             startsWith(trans->input, func_transferFrom_qb) ||
             startsWith(trans->input, func_approve_qb) ||
+            startsWith(trans->input, func_safeTransferFrom_qb) ||
+            startsWith(trans->input, func_safeTransferFromToke_qb) ||
+            startsWith(trans->input, func_setApprovalForAll_qb) ||
             startsWith(trans->input, func_approveAndCall_qb) ||
             startsWith(trans->input, func_allowance_qb) ||
             startsWith(trans->input, func_balanceOf_qb) ||
             startsWith(trans->input, func_decimals_qb) ||
             startsWith(trans->input, func_name_qb) ||
+            startsWith(trans->input, func_exists_qb) ||
+            startsWith(trans->input, func_getApproved_qb) ||
+            startsWith(trans->input, func_isApprovedForAll_qb) ||
+            startsWith(trans->input, func_ownerOf_qb) ||
             startsWith(trans->input, func_symbol_qb) ||
             startsWith(trans->input, func_totalSupply_qb) ||
             startsWith(trans->input, func_version_qb)
