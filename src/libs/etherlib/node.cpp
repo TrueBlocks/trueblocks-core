5389701312cd6fe9756fd3f9ae16d7355048ce20
Thomas Jay Rush <jrush@quickblocks.io>
2018-07-12 13:52:09 -0400

    Removing junk

--- src/libs/etherlib/node.cpp
+++ src/libs/etherlib/node.cpp
@@ -367,10 +367,10 @@ extern void registerQuitHandler(QUITHANDLER qh);
 
     //-------------------------------------------------------------------------
     bool nodeHasTraces(void) {
-        // At block 50871 transaction 0, (hash: 0x0ec3f2488a93839524add10ea229e773f6bc891b4eb4794c3337d4495263790b)
+        // At block 50871 transaction 0, (hash: 0x6df0b4a0d15ae3b925b9819646a0cff4d1bc0a53b294c0d84d884865302d13a5)
         // we know there were exactly 23 traces as per Parity. We check that here to see if the node is
         // running with --tracing enabled. Not sure how this works with Geth
-        size_t count = getTraceCount("0x0ec3f2488a93839524add10ea229e773f6bc891b4eb4794c3337d4495263790b");
+        size_t count = getTraceCount("0x6df0b4a0d15ae3b925b9819646a0cff4d1bc0a53b294c0d84d884865302d13a5");
         return (count == 23);
     }
 
