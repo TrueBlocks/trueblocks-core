5d38c7feeb57bbb7e8582e8c785bc0df29c717d1
Thomas Jay Rush <jrush@quickblocks.io>
2018-07-23 18:36:54 -0400

    Fixing conversions

--- src/libs/utillib/conversions.h
+++ src/libs/utillib/conversions.h
@@ -40,56 +40,57 @@ namespace qblocks {
     using SFBloomArray   = vector < SFBloom   >;
 
     //-------------------------------------------------------------------------
-    extern string_q  chr_2_HexStr (const string_q& str);
-    extern string_q  bnu_2_Hex    (const SFUintBN& bu);
-    extern string_q  uint_2_Hex   (uint64_t num);
-    extern string_q  wei_2_Ether  (const string_q& str);
-    extern string_q  wei_2_Ether  (SFUintBN val);
-    extern string_q  bloom_2_Bytes(const SFBloom& bl);
-    extern string_q  bloom_2_Bits (const SFBloom& bl);
+    extern bool        str_2_Bool   (const string_q& str);
+    extern int64_t     str_2_Int    (const string_q& str);
+    extern uint64_t    str_2_Uint   (const string_q& str);
+    extern string_q    str_2_Hex    (const string_q& str);
+    extern SFGas       str_2_Gas    (const string_q& str);
+    extern double      str_2_Double (const string_q& str);
+    extern SFIntBN     str_2_BigInt (const string_q& str);
+    extern SFUintBN    str_2_BigUint(const string_q& str);
+    extern SFAddress   str_2_Addr   (const string_q& str);
+    extern SFHash      str_2_Hash   (const string_q& str);
+    extern SFUintBN    str_2_Wei    (const string_q& str);
+    extern SFBloom     str_2_Bloom  (const string_q& str);
+    extern SFUintBN    str_2_Topic  (const string_q& str);
+    extern timestamp_t str_2_Ts     (const string_q& str);
 
     //-------------------------------------------------------------------------
-    extern bool      str_2_Bool   (const string_q& str);
-    extern int64_t   str_2_Int    (const string_q& str);
-    extern uint64_t  str_2_Uint   (const string_q& str);
-    extern string_q  str_2_Hex    (const string_q& str);
-    extern SFGas     str_2_Gas    (const string_q& str);
-    extern double    str_2_Double (const string_q& str);
-    extern SFIntBN   str_2_BigInt (const string_q& str);
-    extern SFUintBN  str_2_BigUint(const string_q& str);
-    extern SFAddress str_2_Addr   (const string_q& str);
-    extern SFHash    str_2_Hash   (const string_q& str);
-    extern SFUintBN  str_2_Wei    (const string_q& str);
-    extern SFBloom   str_2_Bloom  (const string_q& str);
-    extern SFUintBN  str_2_Topic  (const string_q& str);
+    extern string_q    bool_2_Str   (bool num);
+    extern string_q    int_2_Str    (int64_t num);
+    extern string_q    uint_2_Str   (uint64_t num);
+    extern string_q    gas_2_Str    (const SFGas& gas);
+    extern string_q    double_2_Str (double f, size_t nDecimals = 10);
+    extern string_q    bni_2_Str    (const SFIntBN& bn);
+    extern string_q    bnu_2_Str    (const SFUintBN& bu);
+    extern string_q    addr_2_Str   (const SFAddress& addr);
+    extern string_q    hash_2_Str   (const SFHash& hash);
+    extern string_q    wei_2_Str    (const SFWei& wei);
+    extern string_q    bloom_2_Str  (const SFBloom& bloom);
+    extern string_q    topic_2_Str  (const SFUintBN& topic);
+    extern string_q    ts_2_Str     (timestamp_t ts);
 
     //-------------------------------------------------------------------------
-    extern string_q  int_2_Str    (int64_t num);
-    extern string_q  uint_2_Str   (uint64_t num);
-    extern string_q  gas_2_Str    (const SFGas& gas);
-    extern string_q  double_2_Str (double f, size_t nDecimals = 10);
-    extern string_q  bni_2_Str    (const SFIntBN& bn);
-    extern string_q  bnu_2_Str    (const SFUintBN& bu);
-    extern string_q  addr_2_Str   (const SFAddress& addr);
-    extern string_q  hash_2_Str   (const SFHash& hash);
-    extern string_q  wei_2_Str    (const SFWei& wei);
-    extern string_q  bloom_2_Str  (const SFBloom& bloom);
-    extern string_q  topic_2_Str  (const SFUintBN& topic);
+    extern string_q    chr_2_HexStr (const string_q& str);
+    extern string_q    bnu_2_Hex    (const SFUintBN& bu);
+    extern string_q    uint_2_Hex   (uint64_t num);
+    extern string_q    wei_2_Ether  (const string_q& str);
+    extern string_q    wei_2_Ether  (SFUintBN val);
+    extern string_q    bloom_2_Bytes(const SFBloom& bl);
+    extern string_q    bloom_2_Bits (const SFBloom& bl);
 
     //--------------------------------------------------------------------
     class SFTime;
-    extern timestamp_t str_2_Ts (const string_q& timeIn);
-    extern string_q    ts_2_Str (timestamp_t ts);
     extern timestamp_t date_2_Ts(const SFTime&   timeIn);
     extern SFTime      ts_2_Date(timestamp_t tsIn);
 
     //--------------------------------------------------------------------
-    extern bool      isZeroHash   (const SFHash& hash);
-    extern bool      isZeroAddr   (const SFAddress& addr);
-    extern bool      isNumeral    (const string_q& test);
-    extern bool      isHexStr     (const string_q& str);
-    extern bool      isAddress    (const SFAddress& addr);
-    extern bool      isHash       (const SFHash& hashIn);
-    extern bool      isUnsigned   (const string_q& in);
+    extern bool        isZeroHash   (const SFHash& hash);
+    extern bool        isZeroAddr   (const SFAddress& addr);
+    extern bool        isNumeral    (const string_q& test);
+    extern bool        isHexStr     (const string_q& str);
+    extern bool        isAddress    (const SFAddress& addr);
+    extern bool        isHash       (const SFHash& hashIn);
+    extern bool        isUnsigned   (const string_q& in);
 
 }  // namespace qblocks
