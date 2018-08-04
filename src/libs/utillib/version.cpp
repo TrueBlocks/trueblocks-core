43fb028c25ed397be7b911aaaeed4c689c66dbd6
Thomas Jay Rush <jrush@quickblocks.io>
2018-08-04 01:56:46 -0400

    Bumping version to 0.5.5

--- src/libs/utillib/version.cpp
+++ src/libs/utillib/version.cpp
@@ -17,7 +17,7 @@ namespace qblocks {
 
 #define MAJOR 0
 #define MINOR 5
-#define BUILD 2
+#define BUILD 5
 #define SUBVERS "alpha"
     //--------------------------------------------------------------------------------
     uint32_t getVersionNum(void) {
