2e08f992d026333ad9fb419b7ecb88803f2f8e27
Thomas Jay Rush <jrush@quickblocks.io>
2018-08-15 08:17:30 -0400

    Cleaning up qblocks master command

--- src/tools/qblocks/qblocks.cpp
+++ src/tools/qblocks/qblocks.cpp
@@ -22,14 +22,24 @@ int main(int argc, const char *argv[]) {
     COptions options;
     options.setProgramName(basename((char*)argv[0]));
 
-    // If --help or -h is the first item in the arg list, the user is asking for help on this command,
-    // otherwise, we send every command option to the underlying command
+    // Handle help on this tool's command line for this tool only if there are not enough args or if
+    // --help is the first item in the command list. Otherwise, we need to pass it to subcommand
+    bool isHelp = (argc == 1);
     CStringArray h = { "--help", "-h" };
     for (auto i : h) {
-        if (argc == 2 && i == argv[1])
-            return usage();
+        if (argc >= 2 && i == argv[1]) {
+            if (!isHelp)
+                isHelp = true;
+        }
+    }
+    if (isHelp) {
+        optionOn(OPT_HELP);
+        if (isTestMode())
+            colorsOff();
+        return usage();
     }
 
+    // Act normally
     if (!options.prepareArguments(argc, argv))
         return 0;
 
@@ -37,38 +47,11 @@ int main(int argc, const char *argv[]) {
         string_q command = nextTokenClear(options.commandList, '\n');
         if (!options.parseArguments(command))
             return 0;
-        string_q cmd = options.cmd + " " + options.args + " --redir_stderr";
+        string_q cmd = options.toolName.first + " " + options.args + " --qblocks";
         if (isTestMode())
             cout << "calling: " << cmd << "\n";
-        cout << unconvert(doCommand(cmd)) << "\n";
+        if (system(cmd.c_str())) { }  // Don't remove. Silences compiler warnings
+        cout << "\n";
     }
     return 0;
-}
-
-//--------------------------------------------------------------------------------
-string_q convert(const string_q& str) {
-    string_q ret = str;
-    replace(ret, "blocks",   "getBlock");
-    replace(ret, "txs",      "getTrans");
-    replace(ret, "receipts", "getReceipt");
-    replace(ret, "logs",     "getLogs");
-    replace(ret, "traces",   "getTraces");
-    replace(ret, "names",    "ethName");
-    replace(ret, "when",     "whenBlock");
-    replace(ret, "where",    "whereBlock");
-    return ret;
-}
-
-//--------------------------------------------------------------------------------
-string_q unconvert(const string_q& str) {
-    string_q ret = str;
-    replace(ret, "getBlock",   "qblocks blocks");
-    replace(ret, "getTrans",   "qblocks txs");
-    replace(ret, "getReceipt", "qblocks receipts");
-    replace(ret, "getLogs",    "qblocks logs");
-    replace(ret, "getTraces",  "qblocks traces");
-    replace(ret, "ethName",    "qblocks names");
-    replace(ret, "whenBlock",  "qblocks when");
-    replace(ret, "whereBlock", "qblocks where");
-    return ret;
 }
