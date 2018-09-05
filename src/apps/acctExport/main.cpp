16a99bf5e44c343776d954e9572e45e71e4814dd
Thomas Jay Rush <jrush@quickblocks.io>
2018-09-05 18:54:41 -0400

    Adding acctExport app to public code

---
+++ src/apps/acctExport/main.cpp
@@ -0,0 +1,32 @@
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+#include "tokenlib.h"
+#include "options.h"
+
+//-----------------------------------------------------------------------
+int main(int argc, const char *argv[]) {
+
+    etherlib_init();
+    tokenlib_init();
+
+    // Parse command line, allowing for command files
+    COptions options;
+    if (!options.prepareArguments(argc, argv))
+        return 0;
+
+    while (!options.commandList.empty()) {
+
+        string_q command = nextTokenClear(options.commandList, '\n');
+        if (!options.parseArguments(command))
+            return 0;
+
+        options.exportData();
+    }
+
+    return 0;
+}
--- src/apps/acctExport/options.h
+++ src/apps/acctExport/options.h
@@ -6,24 +6,63 @@
  *
  * The LICENSE at the root of this repo details your rights (if any)
  *------------------------------------------------------------------------*/
+#include "etherlib.h"
 #include "acctlib.h"
 #include "tokenlib.h"
-#ifndef NOWALLETLIB
 #include "walletlib.h"
-#endif
-#include "options.h"
 
 //-----------------------------------------------------------------------
 class COptions : public COptionsBase {
 public:
-    CAccountWatchArray watches;
-    
-public:
-             COptions        (void);
-            ~COptions        (void);
+    blknum_t blk_nBlocks;
+    blknum_t blk_firstBlock;
+    blknum_t blk_lastBlock;
+    blknum_t blk_minWatchBlock;
+    blknum_t blk_maxWatchBlock;
+    uint64_t tx_nAccountedFor;
+    uint64_t tx_nDisplayed;
+    blknum_t theStartBlock;
+    bool accounting_on;
+    bool logs_on;
+    bool trace_on;
+    bool autocorrect_on;
+    bool json_on;
+    bool report_bals;
+    string_q hideFields;
+    string_q showFields;
+    blknum_t kBlock;
+    bool parseArguments(string_q& command);
+    void Init(void);
 
-    bool     parseArguments  (string_q& command);
-    void     Init            (void);
-    bool     loadWatches     (const CToml& toml);
-    bool     displayFromCache(void);
+    CAccountWatchArray watches;
+    CAccountWatchArray named;
+
+    string_q transFmt;
+    string_q traceFmt;
+
+    COptions(void);
+    ~COptions(void);
+
+    bool openIncomeStatement      (const CBlock& block);
+    bool accountForExtTransaction (const CBlock& block, const CTransaction *trans);
+    bool accountForIntTransaction (const CBlock& block, const CTransaction *trans, const CTrace *trace);
+    bool closeIncomeStatement     (const CBlock& block);
+    bool exportData               (void);
+
+    bool     loadWatches    (const CToml& toml);
+    string_q annotate       (const string_q& strIn) const;
+
+    typedef bool (*NAMEVISITFUNC)(string_q& str, const CAccountWatch& watch);
+
+    void displayTransaction (ostream& os, const CTransaction *theTrans) const;
+    void displayTrace (ostream& os, const CTransaction *theTrans) const;
+    void renameItems(string_q& str, const CAccountWatchArray& array) const;
 };
+
+//------------------------------------------------------------------------
+extern bool articulateTransaction(CTransaction *p);
+extern bool articulateEvent(CLogEntry *p);
+
+//-----------------------------------------------------------------------------
+extern CStringArray signatures;
+extern CStringArray topics;
