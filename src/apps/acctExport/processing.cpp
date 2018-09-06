97d9456f3abaa82f7304e02786d5bc8d24751355
Thomas Jay Rush <jrush@quickblocks.io>
2018-09-05 23:43:15 -0400

    acctExport

--- src/apps/acctExport/processing.cpp
+++ src/apps/acctExport/processing.cpp
@@ -8,10 +8,48 @@
 #include "options.h"
 
 //-----------------------------------------------------------------------
-bool COptions::exportData(void) {
+bool upgradeData(COptions& options) {
+
+    string_q fileName = "./cache/" + options.watches[0].address + ".acct.bin";
+
+    // If we've already upgraded the file, we've deleted it and we're done...
+    if (!fileExists(fileName))
+        return true;
+
+    // If the file is locked, we need to tell the user.
+    if (fileExists(fileName + ".lck"))
+        return usage("The cache lock file is present. The program is either already "
+                     "running or it did not end cleanly the\n\tlast time it ran. "
+                     "Quit the already running program or, if it is not running, "
+                     "remove the lock\n\tfile: " + fileName + ".lck'. Quitting...");
+
+    uint64_t size = fileSize(fileName);
+    if (!size)
+        return usage("Old style cache file is present, but empty. Remove it to continue.");
+
+    uint64_t *buffer = new uint64_t[size];
+    bzero(buffer, size);
+
+    CArchive txCache(READING_ARCHIVE);
+    if (txCache.Lock(fileName, binaryReadOnly, LOCK_NOWAIT)) {
+        uint64_t bytesRead = txCache.Read(buffer, sizeof(uint64_t), size);
+        cout << "bytesRead: " << bytesRead << "\n";
+        txCache.Release();
+    } else {
+        return usage("Could not open old style cache file. Quiting...");
+    }
+
+    for (size_t i = 0 ; i < size / 2 ; i++)
+        cout << buffer[i*2] << "\t" << buffer[(i*2)+1] << "\n";
+
+    return true;
+}
+
+//-----------------------------------------------------------------------
+bool exportData(COptions& options) {
 
     // Make sure we have a cache file...
-    string_q cacheFileName = "./cache/" + watches[0].address + ".acct.bin";
+    string_q cacheFileName = "./cache/" + options.watches[0].address + ".acct.bin";
     if (!fileExists(cacheFileName))
         return false;
 
@@ -22,13 +60,6 @@ bool COptions::exportData(void) {
                          "Quit the already running program or, if it is not running, "
                          "remove the lock\n\tfile: " + cacheFileName + ".lck'. Quitting...");
 
-#if 0
-    if (export_on) {
-        CTransaction t;
-        cout << toLower(t.Format(substitute(substitute(substitute(transFmt, "{IS","{IS_"), "w:10:INPUT","encoding"), "{", "{p:"))) << "\n";
-    }
-#endif
-    
     CBlock latest;
     getBlock(latest, "latest");
 
@@ -42,18 +73,18 @@ bool COptions::exportData(void) {
         txCache.Seek( (-1 * (long)(2*sizeof(uint64_t))), SEEK_END);  // NOLINT
         txCache.Read(endBlock);
         txCache.Seek( 0, SEEK_SET);
-        if (json_on)
+        if (options.defaultFmt == "json")
             cout << "[";
 
         while (!txCache.Eof()) {
 
             uint64_t transID = NOPOS, blockNum = NOPOS;
             txCache >> blockNum >> transID;
-            if (blockNum != NOPOS && transID != NOPOS && blockNum >= theStartBlock) {
+            if (blockNum != NOPOS && transID != NOPOS && blockNum >= options.theStartBlock) {
 
                 if (blockNum > lastBlock) {
 
-                    if (!closeIncomeStatement(block)) {
+                    if (!options.closeIncomeStatement(block)) {
                         cerr << "Quitting debugger.\n";
                         txCache.Release();
                         return false; // return false since user hit 'quit' on debugger
@@ -74,7 +105,7 @@ bool COptions::exportData(void) {
                         }
                     }
 
-                    if (!openIncomeStatement(block))  {
+                    if (!options.openIncomeStatement(block))  {
                         cerr << "Quitting debugger.\n";
                         txCache.Release();
                         return false; // return false since user hit 'quit' on debugger
@@ -88,30 +119,30 @@ bool COptions::exportData(void) {
                     CTransaction *trans = &block.transactions.at(transID);
                     trans->pBlock = &block;
                     // TODO(tjayrush) we only get traces on non-Ddos blocks. We need to handle this better
-                    if ((json_on || accounting_on || trace_on) &&
+                    if (((options.defaultFmt == "json") || options.accounting_on || options.trace_on) &&
                         (!ddosRange(block.blockNumber) || getTraceCount(trans->hash) < 250)) {
                         getTraces(trans->traces, trans->hash);
                     }
 
                     ostringstream os;
-                    accountForExtTransaction(block, trans);
+                    options.accountForExtTransaction(block, trans);
                     articulateTransaction(trans);
-                    displayTransaction(os, trans);
-                    if (json_on && blockNum < endBlock)
+                    options.displayTransaction(os, trans);
+                    if ((options.defaultFmt == "json") && blockNum < endBlock)
                             os << ",";
                     os << endl;
                     os.flush();
-                    cout << annotate(os.str());
+                    cout << options.annotate(os.str());
                     cout.flush();
-                    tx_nDisplayed++;
+                    options.tx_nDisplayed++;
                 }
             }
         }
-        if (json_on)
+        if ((options.defaultFmt == "json"))
             cout << "]";
 
         // Ignore the return since we're done anway
-        closeIncomeStatement(block);
+        options.closeIncomeStatement(block);
         txCache.Release();
     }
     return true;
@@ -120,16 +151,16 @@ bool COptions::exportData(void) {
 //-----------------------------------------------------------------------
 void COptions::renameItems(string_q& str, const CAccountWatchArray& watchArray) const {
     for (auto watch : watchArray) {
-        if (json_on) {
+        if ((defaultFmt == "json")) {
             CStringArray fields = { "to", "from", "address", "contractAddress" };
             for (auto field : fields) {
                 string_q target = "\"" + field + "\": \"" + watch.address + "\"";
                 str = substitute(str, target, target + ", \"" + field + "Name\": \"" + watch.name + "\"");
             }
         } else {
-            str = substitute(str, watch.address, watch.color + watch.displayName(false,true,true,8) + cOff);
+            str = substitute(str, watch.address, watch.color + watch.displayName(false,true,true,8));
             str = substitute(str, "000000000000000000000000" + extract(watch.address, 2),
-                                watch.color + "000000000000000000000000" + extract(watch.address, 2) + cWhite);
+                                watch.color + "000000000000000000000000" + extract(watch.address, 2));
         }
     }
 }
@@ -173,6 +204,6 @@ bool COptions::loadWatches(const CToml& toml) {
         blk_maxWatchBlock = max(blk_maxWatchBlock, watch->lastBlock);
     }
 
-    watches.push_back(CAccountWatch("Others", "Other Accts", 0, UINT32_MAX, bRed));
+    watches.push_back(CAccountWatch("Others", "Other Accts", 0, UINT32_MAX, ""));
     return true;
 }
