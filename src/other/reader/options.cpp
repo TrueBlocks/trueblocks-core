eee2eddcef46e6f44026b574534d17bcaee05da1
Thomas Jay Rush <jrush@quickblocks.io>
2018-09-11 22:26:54 -0400

    Adding qbsort

---
+++ src/other/reader/options.cpp
@@ -0,0 +1,65 @@
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *
+ * The LICENSE at the root of this repo details your rights (if any)
+ *------------------------------------------------------------------------*/
+#include "options.h"
+
+//---------------------------------------------------------------------------------------------------
+static COption params[] = {
+    COption("fileName", "file"),
+    COption("-sort",    "sort em"),
+    COption("",         "This is what the program does.\n"),
+};
+static size_t nParams = sizeof(params) / sizeof(COption);
+
+//---------------------------------------------------------------------------------------------------
+bool COptions::parseArguments(string_q& command) {
+
+    if (!standardOptions(command))
+        return false;
+
+    Init();
+    while (!command.empty()) {
+        string_q arg = nextTokenClear(command, ' ');
+        if (arg == "-s" || arg == "--sort") {
+            sort = true;
+
+        } else if (startsWith(arg, '-')) {  // do not collapse
+            if (!builtInCmd(arg)) {
+                return usage("Invalid option: " + arg);
+            }
+        } else {
+            fileName = arg;
+            if (!fileExists(fileName))
+                return usage("No such file: " + fileName + ". Quitting...");
+        }
+    }
+
+    return true;
+}
+
+//---------------------------------------------------------------------------------------------------
+void COptions::Init(void) {
+    paramsPtr = params;
+    nParamsRef = nParams;
+
+    fileName = "";
+    sort = false;
+}
+
+//---------------------------------------------------------------------------------------------------
+COptions::COptions(void) {
+    // will sort the fields in these classes if --parity is given
+    sorts[0] = GETRUNTIME_CLASS(CBlock);
+    sorts[1] = GETRUNTIME_CLASS(CTransaction);
+    sorts[2] = GETRUNTIME_CLASS(CReceipt);
+
+    Init();
+}
+
+//--------------------------------------------------------------------------------
+COptions::~COptions(void) {
+}
