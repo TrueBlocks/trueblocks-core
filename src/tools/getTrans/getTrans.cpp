9f8356207b23da0b50deefd174c3ef1c0c666b3d
Thomas Jay Rush <jrush@quickblocks.io>
2018-09-27 21:10:11 -0400

    Working on gold test cases

--- src/tools/getTrans/getTrans.cpp
+++ src/tools/getTrans/getTrans.cpp
@@ -36,8 +36,8 @@ int main(int argc, const char *argv[]) {
             forEveryTransactionInList(checkBelongs, &options, options.transList.queries);
             if (!options.belongs) {
                 for (auto addr : options.filters) {
-                    cout << "\taddress " << cRed << addr << cOff << " not found";
-                    cout << string_q(70,' ') << "\n";
+                    cout << "\taddress " << cRed << addr << cOff << " not found ";
+                    cout << options.transList.queries << "\n";
                 }
             }
         } else {
@@ -50,18 +50,36 @@ int main(int argc, const char *argv[]) {
     return 0;
 }
 
+#define EARLY_QUIT 1
 //----------------------------------------------------------------
 bool visitAddrs(const CAddressAppearance& item, void *data) {
     COptions *opt = (COptions*)data;
+#ifdef EARLY_QUIT
     if (opt->belongs)
         return false;
+#endif
 
     for (auto addr : opt->filters) {
         if (addr % item.addr) {
-            cout << "\t" << cGreen << " found at " << cTeal << item << cOff << "                     \r";
+            cout << "\t" << cGreen << " found at " << cTeal << item << cOff << "                     ";
+#if 0
+            if (contains(item.reason, "input") || contains(item.reason, "data") ||
+                contains(item.reason, "generator") || (contains(item.reason, "trace") && contains(item.reason, "to")) ||
+                (contains(item.reason, "log") && contains(item.reason, "topic"))
+                )
+                cout << "\r";
+            else
+                cout << "\n";
+#else
+            cout << "\r";
+#endif
             cout.flush();
             opt->belongs = true;
-            return true; //false;  // we're done
+#ifdef EARLY_QUIT
+            return true;  // we're done
+#else
+            return false;
+#endif
         }
     }
     return true;
