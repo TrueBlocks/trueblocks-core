5e7e9dae06a8a5d1017dd101db20390a261a6ca9
wildmolasses <changes@gmail.com>
2018-11-15 20:49:05 -0500

    quickQuitHandler added to getBlock, getTrans

--- src/tools/getTrans/getTrans.cpp
+++ src/tools/getTrans/getTrans.cpp
@@ -19,7 +19,7 @@ extern bool checkBelongs(CTransaction& trans, void *data);
 //--------------------------------------------------------------
 int main(int argc, const char *argv[]) {
 
-    etherlib_init();
+    etherlib_init(quickQuitHandler);
 
     // Parse command line, allowing for command files
     COptions options;
