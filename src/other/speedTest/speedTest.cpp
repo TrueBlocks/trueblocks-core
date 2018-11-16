139e2dc15d49959be0cbec2383a99e05b0f94047
wildmolasses <changes@gmail.com>
2018-11-15 21:06:56 -0500

    change quitHandler to applicable in /other

--- src/other/speedTest/speedTest.cpp
+++ src/other/speedTest/speedTest.cpp
@@ -21,7 +21,7 @@ extern bool visitBlock(CBlock& block, void *data);
 //--------------------------------------------------------------
 int main(int argc, const char *argv[]) {
 
-    etherlib_init();
+    etherlib_init(quickQuitHandler);
 
     // Parse command line, allowing for command files
     COptions options;
