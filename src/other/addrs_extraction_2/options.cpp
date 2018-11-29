a4cf065f7c3ab9959dbaea2ec91d3b85e6a1393a
Thomas Jay Rush <jrush@quickblocks.io>
2018-11-29 13:21:33 -0500

    Adding in address index stuff temporarily

---
+++ src/other/addrs_extraction_2/options.cpp
@@ -0,0 +1,52 @@
+/*-------------------------------------------------------------------------------------------
+ * qblocks - fast, easily-accessible, fully-decentralized data from blockchains
+ * copyright (c) 2018 Great Hill Corporation (http://greathill.com)
+ *
+ * This program is free software: you may redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version. This program is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ *-------------------------------------------------------------------------------------------*/
+#include "options.h"
+
+//---------------------------------------------------------------------------------------------------
+static COption params[] = {
+    COption("", "Move address appearances into sorted 50 MB files.\n"),
+};
+static size_t nParams = sizeof(params) / sizeof(COption);
+
+//---------------------------------------------------------------------------------------------------
+bool COptions::parseArguments(string_q& command) {
+
+    if (!standardOptions(command))
+        return false;
+
+    Init();
+    explode(arguments, command, ' ');
+    for (auto arg : arguments) {
+        return usage("Invalid option: " + arg);
+    }
+    return true;
+}
+
+//---------------------------------------------------------------------------------------------------
+void COptions::Init(void) {
+    arguments.clear();
+    paramsPtr = params;
+    nParamsRef = nParams;
+
+    minArgs = 0;
+}
+
+//---------------------------------------------------------------------------------------------------
+COptions::COptions(void) {
+    Init();
+}
+
+//--------------------------------------------------------------------------------
+COptions::~COptions(void) {
+}
+
