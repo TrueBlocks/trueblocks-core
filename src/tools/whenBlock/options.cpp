5b75d8d49876a2e5485815a926a0d50c9684cb1e
Thomas Jay Rush <jrush@quickblocks.io>
2018-11-29 22:37:57 -0500

    Fixing the same issue as before

--- src/tools/whenBlock/options.cpp
+++ src/tools/whenBlock/options.cpp
@@ -154,10 +154,12 @@ COptions::COptions(void) {
     if (toml.getVersion() < getVersionNum()) {
         toml.setConfigStr("version", "current", getVersionStr());
         string_q ss = toml.getConfigStr("specials", "list", "");
-        ss = substitute(ss, ",{ name : \"constantinople\",",
-                        ",{ name : \"kitties\", value : \"4605167\" },{ name : \"constantinople\",");
-        toml.setConfigStr("specials", "list", ss);
-        toml.writeFile();
+        if (!contains(ss, "kitties")) {
+            ss = substitute(ss, ",{ name : \"constantinople\",",
+                            ",{ name : \"kitties\", value : \"4605167\" },{ name : \"constantinople\",");
+            toml.setConfigStr("specials", "list", ss);
+            toml.writeFile();
+        }
     }
     Init();
 }
