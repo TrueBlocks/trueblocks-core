9fcf8352a75d2b58ddc95f3aa5670a5af0a7befa
Thomas Jay Rush <jrush@greathill.com>
2018-12-29 13:00:03 +0100

    Adding code for difficulty stats calc

---
+++ src/other/difficulty/difficulty.cpp
@@ -0,0 +1,39 @@
+/*-------------------------------------------------------------------------------------------
+ * qblocks - fast, easily-accessible, fully-decentralized data from blockchains
+ * copyright (c) 2018 Great Hill Corporation (http://greathill.com)
+ *
+ * This program is free software: you may redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version. This program is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ *-------------------------------------------------------------------------------------------*/
+#include "etherlib.h"
+#include "options.h"
+
+//--------------------------------------------------------------
+int main(int argc, const char *argv[]) {
+
+    etherlib_init("binary", quickQuitHandler);
+
+    // Parse command line, allowing for command files
+    COptions options;
+    if (!options.prepareArguments(argc, argv))
+        return 0;
+
+    for (auto command : options.commandLines) {
+        if (!options.parseArguments(command))
+            return 0;
+
+        blknum_t last = getLatestBlockFromClient();
+        for (blknum_t bn = 6591474 ; bn <= last ; bn++) {
+            CBlock block;
+            getBlock(block, bn);
+            cerr << bGreen << (last - block.blockNumber) << ": " << cOff; cerr.flush();
+            cout << block.blockNumber << "," << block.timestamp << "," << block.difficulty << endl;
+        }
+    }
+    return 0;
+}
