76ed1100f18301cfa6839e542158d2e0ea79e02f
Thomas Jay Rush <jrush@quickblocks.io>
2019-02-07 13:55:13 -0500

    Adding junk data for docker image demo

--- src/apps/acctScrape/acctScrape.cpp
+++ src/apps/acctScrape/acctScrape.cpp
@@ -15,6 +15,7 @@ extern string_q report         (const COptions& options, double start, double st
 //-----------------------------------------------------------------------
 int main(int argc, const char *argv[]) {
     acctlib_init(defaultQuitHandler);
+//TODO: remove this line and figure out how to turn this off for docker
     colorsOff();
 
     COptions options;
