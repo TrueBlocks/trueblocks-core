dafa3ae7fce705522badc94f8ce63bf5d91d1627
Thomas Jay Rush <jrush@quickblocks.io>
2019-02-25 09:28:56 -0500

    Trying to fix test case on linux

--- src/libs/test-libs/etherlib/decode_abi/options.h
+++ src/libs/test-libs/etherlib/decode_abi/options.h
@@ -1,4 +1,27 @@
 #pragma once
+/*-------------------------------------------------------------------------------------------
+ * qblocks - fast, easily-accessible, fully-decentralized data from blockchains
+ * copyright (c) 2018 Great Hill Corporation (http://greathill.com)
+ *
+ * This program is free software: you may redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version. This program is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ *-------------------------------------------------------------------------------------------*/
+#include "etherlib.h"
+
+//-----------------------------------------------------------------------------
+class COptions : public COptionsBase {
+public:
+    COptions(void);
+    ~COptions(void);
+
+    bool parseArguments(string_q& command);
+    void Init(void);
+};
 
 //---------------------------------------------------------------------
 class T {
@@ -9,9 +32,9 @@ public:
     string_q expected;
     bool enabled;
     T(const string_q& d, bool v, const string_q& in, const string_q& ex)
-        : desc(d), bVal(v), input(in), expected(ex) { enabled = true; }
+    : desc(d), bVal(v), input(in), expected(ex) { enabled = true; }
     T(const string_q& d, const string_q& in, const string_q& ex)
-        : desc(d), input(in), expected(ex) { enabled = true; bVal = false; }
+    : desc(d), input(in), expected(ex) { enabled = true; bVal = false; }
     T(void) : desc(""), bVal(false), input(""), expected("") { enabled = true; }
     string_q report(const string_q& res) {
         ostringstream os;
