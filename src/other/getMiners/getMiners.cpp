c222685f09b165c9553c8f44456698bff6bb5365
Thomas Jay Rush <jrush@quickblocks.io>
2019-03-18 22:52:03 -0400

    Adding threads and getMiner tools

---
+++ src/other/getMiners/getMiners.cpp
@@ -0,0 +1,29 @@
+/*-------------------------------------------------------------------------------------------
+ * qblocks - fast, easily-accessible, fully-decentralized data from blockchains
+ * copyright (c) 2018 Great Hill Corporation (http://greathill.com)
+ *
+ * This program is free software: you may redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version. This program is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ *-------------------------------------------------------------------------------------------*/
+#include "etherlib.h"
+
+//----------------------------------------------------------------
+int main(int argc, const char *argv[]) {
+
+    etherlib_init(quickQuitHandler);
+
+    for (blknum_t bn = 0 ; bn < getLastBlock_client() ; bn++) {
+        CBlock block;
+        getBlock(block, bn);
+        cout << block.blockNumber << "\t" << block.miner << endl;
+    }
+
+    etherlib_cleanup();
+
+    return 1;
+}
