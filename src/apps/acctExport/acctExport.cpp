861a5dd449656f53f4473e1b70d210314eba611b
Thomas Jay Rush <jrush@quickblocks.io>
2019-04-09 05:48:19 -0400

    Using acctlib in acctExport

--- src/apps/acctExport/acctExport.cpp
+++ src/apps/acctExport/acctExport.cpp
@@ -8,9 +8,9 @@
 
 //-----------------------------------------------------------------------
 int main(int argc, const char *argv[]) {
-    etherlib_init(quickQuitHandler);
 
-    // Parse command line, allowing for command files
+    acctlib_init(quickQuitHandler);
+
     COptions options;
     if (!options.prepareArguments(argc, argv))
         return 0;
@@ -18,10 +18,10 @@ int main(int argc, const char *argv[]) {
     for (auto command : options.commandLines) {
         if (!options.parseArguments(command))
             return 0;
-
         loadData(options);
         exportData(options);
     }
 
+    acctlib_cleanup();
     return 0;
 }
