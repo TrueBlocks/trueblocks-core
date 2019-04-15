f6c081df922e5fb7ab5ce530599c978b03a439ed
Thomas Jay Rush <jrush@quickblocks.io>
2019-04-15 09:40:43 -0400

    Cleaning up ethName, enabling custom names file

---
+++ src/dev_tools/buildBlooms/newbloom.h
@@ -0,0 +1,13 @@
+//
+//  newbloom.h
+//  proj
+//
+//  Created by Thomas Jay Rush on 4/15/19.
+//  Copyright © 2019 com.GHC. All rights reserved.
+//
+
+#ifndef newbloom_h
+#define newbloom_h
+
+
+#endif /* newbloom_h */
