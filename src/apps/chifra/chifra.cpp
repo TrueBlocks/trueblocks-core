7a93483285530fbf921f23c5da739b2c53211c41
Thomas Jay Rush <jrush@quickblocks.io>
2019-05-01 21:24:34 -0400

    Allowing for use of any of the quickBlocks tools from the API

--- src/apps/chifra/chifra.cpp
+++ src/apps/chifra/chifra.cpp
@@ -30,7 +30,7 @@ int main(int argc, const char *argv[]) {
         else if (options.mode == "rm")        RETURN(options.handle_rm())
         else if (options.mode == "accounts")  RETURN(options.handle_accounts())
         else if (options.mode == "functions") RETURN(options.handle_functions())
-        else if (options.mode == "blocks")    RETURN(options.handle_blocks())
+        else if (options.mode == "data")      RETURN(options.handle_data())
         else if (options.mode == "config")    RETURN(options.handle_config())
         else cerr << "Should not happen.";
     }
