117f1bc08253fa9a0df532511fd4f1a9170378c7
Thomas Jay Rush <jrush@quickblocks.io>
2019-06-12 12:03:14 -0400

    Preparing for bounty for python wrapper

--- src/other/python-ext/setup.py
+++ src/other/python-ext/setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages
 from distutils.core import setup, Extension
 
 extension = Extension(name='_quickblocks',
-        include_dirs = ['/usr/local/include', '/usr/include/python3.6','/usr/local/qblocks/include' ],
+        include_dirs = ['/usr/local/include', '/usr/include/python3.6','/usr/local/qblocks/include', '../../libs/etherlib', '../../libs/utillib' ],
         libraries = ['pthread'],
         sources = ['extension.cpp'],
         language='c++',
         extra_compile_args = ['-std=c++11', '-Werror', '-Wall', '-O2', '-fPIC'],
-        extra_link_args = ['-L/usr/local/qblocks/lib', '-lacct', '-lether', '-lutil', '-lcurl'])
+        extra_link_args = ['-L/usr/local/qblocks/lib', '-l../../libs/acct', '-l../../libs/ether', '-l../../libs/util', '-lcurl'])
 
 setup(name = 'QuickBlocks',
         version = '0.1.0',
