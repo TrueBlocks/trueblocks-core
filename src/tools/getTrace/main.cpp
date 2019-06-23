e17b6a2b66e165bab99e345abf635f63a89e4407
Thomas Jay Rush <jrush@quickblocks.io>
2019-06-23 07:50:49 -0400

    Adds handling for suicides, creations, etc in getTrace. #853

--- src/tools/getTrace/main.cpp
+++ src/tools/getTrace/main.cpp
@@ -86,32 +86,78 @@ bool visitTransaction(CTransaction& trans, void *data) {
         opt->abi_spec.loadAbiByAddress(trans.to);
         opt->abi_spec.articulateTransaction(&trans);
     }
+
     for (auto trace : trans.traces) {
         if (opt->articulate) {
             opt->abi_spec.loadAbiByAddress(trace.action.to);
             opt->abi_spec.articulateTrace(&trace);
         }
         manageFields("CFunction:message", !trace.articulatedTrace.message.empty());
-        if (trace.action.refundAddress != "" && !getEnvStr("API_MODE").empty()) {
-            trace.action.from = trace.action.address;
-            trace.action.to = trace.action.refundAddress;
-            trace.action.callType = "suicide";
-            trace.action.value = trace.action.balance;
-            trace.traceAddress.push_back("s");
-            trace.transactionHash = uint_2_Hex(trace.blockNumber * 100000 + trace.transactionPosition);
-            trace.action.input = "0x";
+
+        bool isSuicide = trace.action.address != "";
+        bool isCreation = trace.result.address != "";
+
+        // do not colapse
+        if (isCreation) {
+            displayAsTrace(opt, trace);
+            displayAsCreation(opt, trace);
         }
-        if (isText) {
-            cout << trim(trace.Format(expContext().fmtMap["format"]), '\t') << endl;
-        } else {
-            if (!opt->first)
-                cout << ",";
-            cout << "  ";
-            incIndent();
-            trace.doExport(cout);
-            decIndent();
-            opt->first = false;
+        // do not colapse
+        if (isSuicide) {
+            displayAsSuicide(opt, trace);
         }
+        // do not colapse
+        if (!isCreation && !isSuicide)
+            displayAsTrace(opt, trace);
+    }
+    return true;
+}
+
+//--------------------------------------------------------------
+bool displayAsCreation(COptions *opt, const CTrace& trace) {
+    if (trace.result.address == "")
+        return false;
+    CTrace copy = trace;
+    copy.action.from = "0x0";
+    copy.action.to = trace.result.address;
+    copy.action.callType = "creation";
+    copy.action.value = trace.action.value;
+    if (copy.traceAddress.size() == 0)
+        copy.traceAddress.push_back("null");
+    copy.traceAddress.push_back("s");
+    copy.transactionHash = uint_2_Hex(trace.blockNumber * 100000 + trace.transactionPosition);
+    copy.action.input = trace.action.input;
+    return displayAsTrace(opt, copy);
+}
+
+//--------------------------------------------------------------
+bool displayAsSuicide(COptions *opt, const CTrace& trace) {
+    if (trace.action.refundAddress == "")
+        return false;
+    CTrace copy = trace;
+    copy.action.from = trace.action.address;
+    copy.action.to = trace.action.refundAddress;
+    copy.action.callType = "suicide";
+    copy.action.value = trace.action.balance;
+    copy.traceAddress.push_back("s");
+    copy.transactionHash = uint_2_Hex(trace.blockNumber * 100000 + trace.transactionPosition);
+    copy.action.input = "0x";
+    return displayAsTrace(opt, copy);
+}
+
+//--------------------------------------------------------------
+bool displayAsTrace(COptions *opt, const CTrace& trace) {
+    bool isText = (opt->exportFmt & (TXT1|CSV1));
+    if (isText) {
+        cout << trim(trace.Format(expContext().fmtMap["format"]), '\t') << endl;
+    } else {
+        if (!opt->first)
+            cout << ",";
+        cout << "  ";
+        incIndent();
+        trace.doExport(cout);
+        decIndent();
+        opt->first = false;
     }
     return true;
 }
