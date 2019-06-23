a16448564ca8f4d5b5347e797d7a4ad1ca9d17f4
Thomas Jay Rush <jrush@quickblocks.io>
2019-06-23 08:04:20 -0400

    Moving hard coded options to blockScrape to header to make them more exposed

--- src/apps/blockScrape/scraper_context.h
+++ src/apps/blockScrape/scraper_context.h
@@ -41,3 +41,7 @@ protected:
 
 extern bool notePotential(const CAppearance& item, void *data);
 extern void foundPotential(ADDRESSFUNC func, void *data, blknum_t bn, blknum_t tx, blknum_t tc, const string_q& potList);
+
+#define lastPrecompile address_t("0x0000000000000000000000000000000000000008")
+#define MARKER 50
+#define MINER_MARKER 99999
