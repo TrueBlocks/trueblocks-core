58f513d2a0e26c2dd360b33bc61c3e26457e46b0
johhonn <35551051+johhonn@users.noreply.github.com>
2019-06-29 20:01:50 -0400

    Create print_blocks.py

---
+++ src/other/python-ext/print_blocks.py
@@ -0,0 +1,9 @@
+from quickblocks import QuickBlocks
+##add a node source in the foloowing line
+qb = QuickBlocks('', cache=True)
+print(qb.getTransaction(txhash='0x8b5df1ea9d2c7c3bde45cb4df13e51797caa89ab0909b408f6d5075c7f45747c'))
+
+##for i in range(5200000):
+  #  block = qb[i]
+   # if i % 1000 == 0:
+    #    print(block)
