a64c6e2fd9cba4b87d1790d397a41e7bca0c1b48
johhonn <35551051+johhonn@users.noreply.github.com>
2019-06-29 20:23:39 -0400

    Create Timer.py
    Decorators

---
+++ src/other/python-ext/quickblocks/Timer.py
@@ -0,0 +1,30 @@
+# Decorators
+from functools import wraps
+
+
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+def my_timer(orig_func):
+    import time
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
+
