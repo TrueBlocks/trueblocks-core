20739d2586e2028b3c587f0e286c33da07dc724c
Thomas Jay Rush <jrush@quickblocks.io>
2019-07-25 08:35:16 -0400

    Cleaning up acctScrape in order to allow --staging search for indexes

--- src/apps/acctScrape/options.cpp
+++ src/apps/acctScrape/options.cpp
@@ -8,12 +8,9 @@
 //---------------------------------------------------------------------------------------------------
 static const COption params[] = {
     COption("~addr_list",       "one or more Ethereum addresses"),
-    COption("-maxBlocks:<val>", "scan at most --maxBlocks blocks ('all' implies scan to end of chain)"),
-    COption("@ripe",            "visit ripe (old enough, but not yet staged or finalized) blocks"),
-    COption("@unripe",          "visit unripe (not old enough and not yet staged or finalized) blocks"),
     COption("@noBlooms",        "turn off bloom filters for performance testing"),
     COption("@staging",         "produce results in the staging folder instead of production folder"),
-    COption("@start:<num>",     "first block to check"),
+    COption("@unripe",          "visit unripe (not old enough and not yet staged or finalized) blocks"),
     COption("",                 "Index transactions for a given Ethereum address (or series of addresses).\n"),
 };
 static const size_t nParams = sizeof(params) / sizeof(COption);
@@ -24,35 +21,14 @@ bool COptions::parseArguments(string_q& command) {
     if (!standardOptions(command))
         return false;
 
-    bool isAll = false;
-    blknum_t maxBlocks = 10000;
-    blknum_t start     = 0;
-
     Init();
     explode(arguments, command, ' ');
     for (auto arg : arguments) {
-        if (arg == "--all" || arg == "-a") arg = "--maxBlocks:all";
-
-        if (startsWith(arg, "-m:") || startsWith(arg, "--maxBlocks:")) {
-            arg = substitute(substitute(arg, "-m:", ""), "--maxBlocks:", "");
-            if (arg == "all") {
-                isAll = true;
-            } else {
-                if (isUnsigned(arg))
-                    maxBlocks = str_2_Uint(arg);
-                else
-                    return usage("Please provide an integer value of maxBlocks. Quitting...");
-            }
-
-        } else if (startsWith(arg, "-s:") || startsWith(arg, "--start:")) {
-            arg = substitute(substitute(arg, "-s:", ""), "--start:", "");
-            if (isUnsigned(arg))
-                start = str_2_Uint(arg);
-            else
-                return usage("Please provide an integer value for start. Quitting...");
-
-        } else if (arg == "-u" || arg == "--unripe") {
+        if (arg == "-u" || arg == "--unripe") {
             visitTypes |= VIS_UNRIPE;
+
+        } else if (arg == "-s" || arg == "--staging") {
+            visitTypes |= VIS_STAGING;
 
         } else if (arg == "-n" || arg == "--noBlooms") {
             useBlooms = false;
@@ -62,7 +38,7 @@ bool COptions::parseArguments(string_q& command) {
                 return usage(arg + " does not appear to be a valid address. Quitting...");
 
             // TODO(tjayrush): HARD CODED LIMIT???
-            if (monitors.size() < 30) {
+            if (monitors.size() < 100) {
                 CAccountWatch watch;
                 watch.setValueByName("address", toLower(arg)); // don't change, sets bloom value also
                 watch.setValueByName("name", toLower(arg));
@@ -86,80 +62,47 @@ bool COptions::parseArguments(string_q& command) {
     if (monitors.size() == 0)
         return usage("You must provide at least one Ethereum address. Quitting...");
 
-    // show certain fields and hide others
-    manageFields(defHide, false);
-    manageFields(defShow, true);
-    primary = monitors[0];
-    string_q configFile = "./" + primary.address + ".toml";
-    if (fileExists(configFile)) {
-        CToml *toml = new CToml(configFile);
-        if (toml) {
-            manageFields(toml->getConfigStr("fields", "hide", ""), false);
-            manageFields(toml->getConfigStr("fields", "show", ""), true );
-            delete toml;
-        }
-    }
-
     if (getCurlContext()->nodeRequired) {
-        CBlock latest;
-        getBlock_light(latest, "latest");
-
         if (!isParity() || !nodeHasTraces())
-            return usage("This tool will only run if it is running against a Parity node that has tracing enabled. Quitting...");
+            return usage("This tool requires a running Parity node with --tracing enabled. Quitting...");
     }
 
-    if (isAll)
-        maxBlocks = INT_MAX;
+    establishFolder(getMonitorPath("", FM_PRODUCTION));
+    establishFolder(getMonitorPath("", FM_STAGING));
+    establishFolder(indexFolder_finalized);
+    establishFolder(indexFolder_blooms);
+    establishFolder(indexFolder_staging);
+    establishFolder(indexFolder_ripe);
 
-    string_q prodPath = getMonitorPath("", FM_PRODUCTION);
-    if (!folderExists(prodPath)) {
-        cerr << "The cache folder '" << prodPath << "' not found. Trying to create it." << endl;
-        establishFolder(prodPath);
-        if (!folderExists(prodPath))
-            return usage("The cache folder '" + prodPath + "' not created. Quiting...");
-    }
-
-    string_q stagePath = getMonitorPath("", FM_STAGING);
-    if (!folderExists(stagePath)) {
-        cerr << "The cache folder '" << stagePath << "' not found. Trying to create it." << endl;
-        establishFolder(stagePath);
-        if (!folderExists(stagePath))
-            return usage("The cache folder '" + stagePath + "' not created. Quiting...");
-    }
-
-    blknum_t lastInCache = getLastBlock_cache_final();
+    scanRange.first = UINT_MAX;
     for (auto monitor : monitors) {
-        // Note: We've cleaned the staging folder, so we only have to check for locks in the production folder
-        string_q fn = getMonitorLast(monitor.address);
-        if (fileExists(fn))
-            scanRange.first = min(max(scanRange.first, str_2_Uint(asciiFileToString(fn))), lastInCache);
-
-        if (fileExists(fn + ".lck"))
-            return usage("The cache file '" + fn + "' is locked. Quitting...");
-
-        fn = getMonitorLast(monitor.address);
-        if (fileExists(fn + ".lck"))
-            return usage("The last block file '" + fn + "' is locked. Quitting...");
-
+        string_q fn1 = getMonitorPath(monitor.address);
+        if (fileExists(fn1 + ".lck"))
+            return usage("The cache file '" + fn1 + "' is locked. Quitting...");
+        string_q fn2 = getMonitorLast(monitor.address);
+        if (fileExists(fn2 + ".lck"))
+            return usage("The last block file '" + fn2 + "' is locked. Quitting...");
         cerr << "freshening " << monitor.address << "..." << endl;
-    }
-    if (start > scanRange.first && start <= lastInCache)
-        scanRange.first = start;
-
-    if (!folderExists(indexFolder_finalized))
-        return usage("Address index path '" + indexFolder_finalized + "' not found. Quitting...");
-
-    // How many should we scrape?
-    ASSERT(earliestStart <= lastInCache);
-    scrapeCnt = min(lastInCache - scanRange.first, maxBlocks);
-
-    if (verbose) {
-        for (auto monitor : monitors) {
-            cerr << "Freshening " << monitor.address << "\r";cerr.flush();
-        }
+        // If file doesn't exist, this will report '0'
+        scanRange.first = min(scanRange.first, str_2_Uint(asciiFileToString(fn2)));
     }
 
-    return scanRange.first < lastInCache;
+    blknum_t unripe, ripe, staging, finalized, client;
+    getLastBlocks(unripe, ripe, staging, finalized, client);
+
+    scanRange.second = finalized;
+    if (visitTypes & VIS_STAGING)
+        scanRange.second = staging;
+    if (visitTypes & VIS_UNRIPE)
+        scanRange.second = unripe;
+
+    // This would fail, for example, if the accounts are scraped further than the blocks (i.e. we
+    // cleared the block index cache, but we didn't clear the account monitor cache
+    if (scanRange.first >= scanRange.second) {
+        cerr << "Account scrape is up to date." << endl;
+        return false;
+    }
+    return true;
 }
 
 //---------------------------------------------------------------------------------------------------
@@ -171,8 +114,7 @@ void COptions::Init(void) {
     // optionOn(OPT_OUTPUT);
 
     minArgs         = 0;
-    scrapeCnt       = 0;
-    visitTypes      = (VIS_STAGING | VIS_FINAL);
+    visitTypes      = VIS_FINAL;
     useBlooms       = true;
 }
 
@@ -186,3 +128,19 @@ COptions::~COptions(void) {
     // just some cleanup of the screen
     cerr << string_q(150,' ') << "\r";
 }
+
+//--------------------------------------------------------------------------------
+string_q COptions::postProcess(const string_q& which, const string_q& str) const {
+
+    if (which == "options") {
+        return substitute(str, "addr_list", "<address> [address...]");
+
+    } else if (which == "notes" && (verbose || COptions::isReadme)) {
+
+        string_q ret;
+        ret += "[{addresses}] must start with '0x' and be forty two characters long.\n";
+        return ret;
+    }
+    return str;
+}
+
