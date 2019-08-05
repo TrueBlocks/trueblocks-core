f3d0252a66f900894d228f3ef592ff85e8199240
Thomas Jay Rush <jrush@quickblocks.io>
2019-08-04 22:30:48 -0400

    Trying to get docker build to work

--- src/other/install/docker/clean_for_docker.sh
+++ src/other/install/docker/clean_for_docker.sh
@@ -3,3 +3,4 @@ cp  -pf  "../src/other/install/docker/libs/CMakeLists.txt"            "../src/li
 cp  -pf  "../src/other/install/docker/tools/CMakeLists.txt"           "../src/tools/"
 cp  -pf  "../src/other/install/docker/dev_tools/CMakeLists.txt"       "../src/dev_tools/"
 cp  -pf  "../src/other/install/docker/other/install/qb_config.cmake"  "../src/other/install/"
+ls -lR "../src/other/install"
