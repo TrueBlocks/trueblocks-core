4c2c4f06fbf8c5e9a5d05f397f0a1f10e6e51834
Thomas Jay Rush <jrush@quickblocks.io>
2019-08-08 13:15:39 -0400

    Starting to add /status route to chifra. Moved /balances route to /export?balances option. Re-enabled message route

--- src/apps/chifra/options.h
+++ src/apps/chifra/options.h
@@ -26,7 +26,6 @@ public:
 
     bool handle_list     (void);
     bool handle_export   (void);
-    bool handle_balances (void);
     bool handle_leech    (void);
     bool handle_scrape   (void);
     bool handle_slurp    (void);
