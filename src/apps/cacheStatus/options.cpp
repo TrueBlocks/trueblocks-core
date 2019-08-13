ab90f76b22a7e6f2f57a5ef05b7ed2dfcf360493
Thomas Jay Rush <jrush@quickblocks.io>
2019-08-13 12:16:14 -0400

    First version with /status route in chifra

--- src/apps/cacheStatus/options.cpp
+++ src/apps/cacheStatus/options.cpp
@@ -7,11 +7,13 @@
 
 //---------------------------------------------------------------------------------------------------
 static const COption params[] = {
-    // BEG_CODE_OPTIONS
-    COption("mode", "m", "string", OPT_REQUIRED | OPT_POSITIONAL, "one of enum[scraper|monitors|names|abis|blocks|txs|traces|slurps|prices|some*|all]"),
-    COption("details", "d", "boolean", OPT_SWITCH, "include details about items found in monitors, slurp, abi, or price caches"),
-    COption("", "", "", 0, "Display JSON information about the various TrueBlocks caches.\n"),
-    // END_CODE_OPTIONS
+// BEG_CODE_OPTIONS
+    COption("mode_list", "", "list<enum[scraper|monitors|names|abis|blocks|txs|traces|slurps|prices|some*|all]>", OPT_REQUIRED | OPT_POSITIONAL, "one or more of [scraper|monitors|names|abis|blocks|txs|traces|slurps|prices|some*|all]"),
+    COption("details", "d", "", OPT_SWITCH, "include details about items found in monitors, slurps, abis, or price caches"),
+    COption("list", "l", "", OPT_SWITCH, "display results in Linux ls -l format (assumes --detail)"),
+    COption("fmt", "x", "enum[none|json*|txt|csv|api]", OPT_HIDDEN | OPT_FLAG, "export format (one of [none|json*|txt|csv|api])"),
+    COption("", "", "", 0, "Report on status of one or more TrueBlocks caches."),
+// END_CODE_OPTIONS
 };
 static const size_t nParams = sizeof(params) / sizeof(COption);
 
@@ -27,11 +29,14 @@ bool COptions::parseArguments(string_q& command) {
     for (auto arg : arguments) {
         if (arg == "-d" || arg == "--details") {
             details = true;
+        } else if (arg == "-l" || arg == "--list") {
+            ls = true;
+
         } else {
             string_q options = params[0].description;
             replaceAny(options, "[]*", "|");
             if (!contains(options, "|" + arg + "|"))
-                return usage("Please provide one of " + substitute(params[0].description, "enum", "") + " for --mode. Quitting.");
+                return usage("Provided value for 'mode' (" + arg + ") not " + substitute(params[0].description, "enum", "") + ". Quitting.");
             mode += (arg + "|");
         }
     }
@@ -60,6 +65,7 @@ void COptions::Init(void) {
     optionOn(OPT_PREFUND | OPT_OUTPUT);
     optionOff(OPT_VERBOSE);
 
+    ls = false;
     details = false;
     mode = "";
     char hostname[HOST_NAME_MAX];  gethostname(hostname, HOST_NAME_MAX);
@@ -117,6 +123,9 @@ COptions::~COptions(void) {
 
 //--------------------------------------------------------------------------------
 void COptions::getStatus(ostream& os) {
+
+    if (ls)
+        cout << "Listing mode:" << endl;
 
     CAddressCache aid;
     if (contains(mode, "|scraper|")) {
@@ -311,4 +320,155 @@ bool notePrice(const string_q& path, void *data) {
     }
     return true;
 }
+
+#if 0
+/*-------------------------------------------------------------------------
+ * This source code is confidential proprietary information which is
+ * Copyright (c) 2017 by Great Hill Corporation.
+ * All Rights Reserved
+ *------------------------------------------------------------------------*/
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "options.h"
+#include "question.h"
+
+//------------------------------------------------------------------------------------------------
+bool COptions::handle_status(void) {
+
+    ENTER4("handle_" + mode);
+    nodeNotRequired();
+
+    ostringstream os;
+    os << cGreen << "Monitor path: " << cWhite << getMonitorPath("") << endl;
+
+    bool stats = contains(tool_flags, "-l") || contains(tool_flags, "--long");
+
+    CStringArray files;
+    if (isTestMode()) {
+        files.push_back("0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359.accts.bin");
+
+    } else {
+        if (stats && addrs.size()) {
+            for (auto addr : addrs) {
+                string_q fn = getMonitorPath(addr + ".acct.bin");
+                if (fileExists(fn))
+                    files.push_back(fn);
+                else
+                    LOG_WARN(fn, " not found.");
+            }
+        } else {
+            listFilesInFolder(files, getMonitorPath("*.*"), false);
+        }
+    }
+
+    CAccountNameArray accounts;
+    for (auto file : files) {
+        if (endsWith(file, ".acct.bin")) {
+            replace(file, getMonitorPath(""), "");
+            CAccountName item;
+            item.address = nextTokenClear(file, '.');
+            getNamedAccount(item, item.address);
+            item.name = substitute(substitute(item.name, "(", ""), ")", "");
+            item.path = substitute(getMonitorPath(item.address), getCachePath(""), "./");
+            item.sizeInBytes = fileSize(getMonitorPath(item.address));
+            item.latestAppearance = str_2_Uint(asciiFileToString(getMonitorLast(item.address)));
+            item.lastExport = str_2_Uint(asciiFileToString(getMonitorExpt(item.address)));
+            item.nRecords = fileSize(getMonitorPath(item.address)) / sizeof(CAppearance_base);
+            accounts.push_back(item);
+        }
+    }
+    if (accounts.size() == 0) {
+        if (api_mode) {
+            CAccountName item;
+            item.address = "0x0";
+            item.name = "none";
+            accounts.push_back(item);
+        } else {
+            LOG_WARN("No monitors found.");
+            EXIT_NOMSG4(true);
+        }
+    }
+    sort(accounts.begin(), accounts.end());
+
+    if (api_mode) {
+        SHOW_FIELD(CAccountName, "path");
+        SHOW_FIELD(CAccountName, "sizeInBytes");
+        SHOW_FIELD(CAccountName, "latestAppearance");
+        SHOW_FIELD(CAccountName, "lastExport");
+        SHOW_FIELD(CAccountName, "nRecords");
+        ostringstream oss;
+        if (accounts.size() > 1)
+            oss << "[";
+        bool first = true;
+        for (auto acct : accounts) {
+            if (!first)
+                oss << ",";
+            oss << acct.Format();
+            first = false;
+        }
+        if (accounts.size() > 1)
+            oss << "]";
+        cout << substitute(substitute(oss.str(), "\n", ""), "\t", "") << endl;
+        EXIT_NOMSG4(true);
+    }
+
+    if (stats) {
+        for (auto acct : accounts) {
+            string_q fmt =
+                "[Address:  -c1-{ADDRESS}-off-\n]"
+                "[\tName:        -c2-{NAME}-off-\n]"
+                "[\tFile name:   -c2-{PATH}-off-\n]"
+                "[\tFile size:   -c2-{SIZEINBYTES}-off-\n]"
+                "[\tLast block:  -c2-{LATESTAPPEARANCE}-off-\n]"
+                "[\tLast export: -c2-{LASTEXPORT}-off-\n]"
+                "[\tnRecords:    -c2-{NRECORDS}-off-\n]";
+            replaceAll(fmt, "-c1-", cTeal);
+            replaceAll(fmt, "-c2-", cYellow);
+            replaceAll(fmt, "-off-", cOff);
+            os << acct.Format(fmt);
+        }
+    } else {
+#ifdef TIOCGSIZE
+        struct ttysize ts;
+        ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
+        size_t ncols = ts.ts_cols;
+#elif defined(TIOCGWINSZ)
+        struct winsize ts;
+        ioctl(STDIN_FILENO, TIOCGWINSZ, &ts);
+        size_t ncols = ts.ws_col;
+#else
+        size_t ncols = 1;
+#endif /* TIOCGSIZE */
+        ncols = max(ncols, (size_t)1);
+
+        os << cGreen << "Current monitors:" << cOff << endl;
+
+        // find the longest name (max 25 chars)
+        size_t longest = 0;
+        for (auto acct : accounts)
+            longest = max(longest, acct.name.length() + 3); // two parens
+        longest = min(longest, (size_t)23); // max 23
+        ncols = max(((size_t)1), (ncols / (size_t(42) + longest)));
+
+        uint64_t cnt = 0;
+        for (auto acct : accounts) {
+            string_q name = acct.name;
+            if (!name.empty())
+                name = "(" + name.substr(0,20) + ") ";
+            os << " " << cTeal << acct.address << " " << padRight(name, longest);
+            if (!(++cnt % ncols))
+                os << endl;
+        }
+        os << cOff << endl;
+    }
+
+    if (isTestMode())
+        cout << substitute(os.str(), getCachePath(""), "$BLOCK_CACHE/");
+    else
+        cout << os.str();
+
+    EXIT_NOMSG4(true);
+}
+#endif
 
--- src/apps/chifra/handle_status.cpp
+++ src/apps/chifra/handle_status.cpp
@@ -3,11 +3,7 @@
  * Copyright (c) 2017 by Great Hill Corporation.
  * All Rights Reserved
  *------------------------------------------------------------------------*/
-#include <sys/ioctl.h>
-#include <stdio.h>
-#include <unistd.h>
 #include "options.h"
-#include "question.h"
 
 //------------------------------------------------------------------------------------------------
 bool COptions::handle_status(void) {
@@ -15,134 +11,13 @@ bool COptions::handle_status(void) {
     ENTER4("handle_" + mode);
     nodeNotRequired();
 
+    LOG5("tool_flags: " + tool_flags);
     ostringstream os;
-    os << cGreen << "Monitor path: " << cWhite << getMonitorPath("") << endl;
-
-    bool stats = contains(tool_flags, "-l") || contains(tool_flags, "--long");
-
-    CStringArray files;
-    if (isTestMode()) {
-        files.push_back("0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359.accts.bin");
-
-    } else {
-        if (stats && addrs.size()) {
-            for (auto addr : addrs) {
-                string_q fn = getMonitorPath(addr + ".acct.bin");
-                if (fileExists(fn))
-                    files.push_back(fn);
-                else
-                    LOG_WARN(fn, " not found.");
-            }
-        } else {
-            listFilesInFolder(files, getMonitorPath("*.*"), false);
-        }
-    }
-
-    CAccountNameArray accounts;
-    for (auto file : files) {
-        if (endsWith(file, ".acct.bin")) {
-            replace(file, getMonitorPath(""), "");
-            CAccountName item;
-            item.address = nextTokenClear(file, '.');
-            getNamedAccount(item, item.address);
-            item.name = substitute(substitute(item.name, "(", ""), ")", "");
-            item.path = substitute(getMonitorPath(item.address), getCachePath(""), "./");
-            item.sizeInBytes = fileSize(getMonitorPath(item.address));
-            item.latestAppearance = str_2_Uint(asciiFileToString(getMonitorLast(item.address)));
-            item.lastExport = str_2_Uint(asciiFileToString(getMonitorExpt(item.address)));
-            item.nRecords = fileSize(getMonitorPath(item.address)) / sizeof(CAppearance_base);
-            accounts.push_back(item);
-        }
-    }
-    if (accounts.size() == 0) {
-        if (api_mode) {
-            CAccountName item;
-            item.address = "0x0";
-            item.name = "none";
-            accounts.push_back(item);
-        } else {
-            LOG_WARN("No monitors found.");
-            EXIT_NOMSG4(true);
-        }
-    }
-    sort(accounts.begin(), accounts.end());
-
-    if (api_mode) {
-        SHOW_FIELD(CAccountName, "path");
-        SHOW_FIELD(CAccountName, "sizeInBytes");
-        SHOW_FIELD(CAccountName, "latestAppearance");
-        SHOW_FIELD(CAccountName, "lastExport");
-        SHOW_FIELD(CAccountName, "nRecords");
-        ostringstream oss;
-        if (accounts.size() > 1)
-            oss << "[";
-        bool first = true;
-        for (auto acct : accounts) {
-            if (!first)
-                oss << ",";
-            oss << acct.Format();
-            first = false;
-        }
-        if (accounts.size() > 1)
-            oss << "]";
-        cout << substitute(substitute(oss.str(), "\n", ""), "\t", "") << endl;
-        EXIT_NOMSG4(true);
-    }
-
-    if (stats) {
-        for (auto acct : accounts) {
-            string_q fmt =
-                "[Address:  -c1-{ADDRESS}-off-\n]"
-                "[\tName:        -c2-{NAME}-off-\n]"
-                "[\tFile name:   -c2-{PATH}-off-\n]"
-                "[\tFile size:   -c2-{SIZEINBYTES}-off-\n]"
-                "[\tLast block:  -c2-{LATESTAPPEARANCE}-off-\n]"
-                "[\tLast export: -c2-{LASTEXPORT}-off-\n]"
-                "[\tnRecords:    -c2-{NRECORDS}-off-\n]";
-            replaceAll(fmt, "-c1-", cTeal);
-            replaceAll(fmt, "-c2-", cYellow);
-            replaceAll(fmt, "-off-", cOff);
-            os << acct.Format(fmt);
-        }
-    } else {
-#ifdef TIOCGSIZE
-        struct ttysize ts;
-        ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
-        size_t ncols = ts.ts_cols;
-#elif defined(TIOCGWINSZ)
-        struct winsize ts;
-        ioctl(STDIN_FILENO, TIOCGWINSZ, &ts);
-        size_t ncols = ts.ws_col;
-#else
-        size_t ncols = 1;
-#endif /* TIOCGSIZE */
-        ncols = max(ncols, (size_t)1);
-
-        os << cGreen << "Current monitors:" << cOff << endl;
-
-        // find the longest name (max 25 chars)
-        size_t longest = 0;
-        for (auto acct : accounts)
-            longest = max(longest, acct.name.length() + 3); // two parens
-        longest = min(longest, (size_t)23); // max 23
-        ncols = max(((size_t)1), (ncols / (size_t(42) + longest)));
-
-        uint64_t cnt = 0;
-        for (auto acct : accounts) {
-            string_q name = acct.name;
-            if (!name.empty())
-                name = "(" + name.substr(0,20) + ") ";
-            os << " " << cTeal << acct.address << " " << padRight(name, longest);
-            if (!(++cnt % ncols))
-                os << endl;
-        }
-        os << cOff << endl;
-    }
-
+    os << "cacheStatus " << tool_flags << " ; ";
     if (isTestMode())
-        cout << substitute(os.str(), getCachePath(""), "$BLOCK_CACHE/");
+        cout << substitute(os.str(), getCachePath(""), "$BLOCK_CACHE/") << endl;
     else
-        cout << os.str();
+        if (system(os.str().c_str())) { }  // Don't remove. Silences compiler warnings
 
     EXIT_NOMSG4(true);
 }
