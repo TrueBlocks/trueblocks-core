e2c81de7992f89f9eb0f70d119dd690b81f37a00
Thomas Jay Rush <jrush@greathill.com>
2019-08-18 00:32:27 +0200

    Making mode_list not required in cacheStatus and other test fixes

--- src/apps/cacheStatus/options.cpp
+++ src/apps/cacheStatus/options.cpp
@@ -8,7 +8,7 @@
 //---------------------------------------------------------------------------------------------------
 static const COption params[] = {
 // BEG_CODE_OPTIONS
-    COption("mode_list", "", "list<enum[scraper|monitors|names|abis|blocks|txs|traces|slurps|prices|some*|all]>", OPT_REQUIRED | OPT_POSITIONAL, "one or more of [scraper|monitors|names|abis|blocks|txs|traces|slurps|prices|some*|all]"),
+    COption("mode_list", "", "list<enum[scraper|monitors|names|abis|blocks|txs|traces|slurps|prices|some*|all]>", OPT_POSITIONAL, "one or more of [scraper|monitors|names|abis|blocks|txs|traces|slurps|prices|some*|all]"),
     COption("details", "d", "", OPT_SWITCH, "include details about items found in monitors, slurps, abis, or price caches"),
     COption("list", "l", "", OPT_SWITCH, "display results in Linux ls -l format (assumes --detail)"),
     COption("fmt", "x", "enum[none|json*|txt|csv|api]", OPT_HIDDEN | OPT_FLAG, "export format (one of [none|json*|txt|csv|api])"),
