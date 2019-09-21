8dfcef59195c7d6897c908f810ae3a708cdd62ee
Thomas Jay Rush <jrush@quickblocks.io>
2019-09-21 10:10:32 -0400

    Updating tests

---
+++ src/libs/test-libs/utillib/tomlTest2/README.md
@@ -0,0 +1,6 @@
+## tomlTest
+
+Please refer to the source code of the test case for information on this folder.
+
+**Powered by Qblocks<sup>&trade;</sup>**
+
---
+++ src/libs/test-libs/utillib/tomlTest2/options.cpp
@@ -0,0 +1,49 @@
+/*-------------------------------------------------------------------------------------------
+ * qblocks - fast, easily-accessible, fully-decentralized data from blockchains
+ * copyright (c) 2018 Great Hill Corporation (http://greathill.com)
+ *
+ * This program is free software: you may redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version. This program is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ *-------------------------------------------------------------------------------------------*/
+#include "options.h"
+
+//---------------------------------------------------------------------------------------------------
+static const COption params[] = {
+    COption("filename", "f", "<fn>", OPT_REQUIRED | OPT_POSITIONAL, "toml filename to test"),
+    COption("", "", "", OPT_DESCRIPTION, "Simple test program for toml files.\n"),
+};
+static const size_t nParams = sizeof(params) / sizeof(COption);
+
+//---------------------------------------------------------------------------------------------------
+bool COptions::parseArguments(string_q& command) {
+
+    if (!standardOptions(command))
+        return false;
+
+    Init();
+    while (!command.empty())
+        filename = ("tests/" + nextTokenClear(command, ' '));
+
+    return true;
+}
+
+//---------------------------------------------------------------------------------------------------
+void COptions::Init(void) {
+    registerOptions(nParams, params);
+
+    filename = "";
+}
+
+//---------------------------------------------------------------------------------------------------
+COptions::COptions(void) {
+    Init();
+}
+
+//--------------------------------------------------------------------------------
+COptions::~COptions(void) {
+}
