b1d7680bc34e1c339b364e0059b65091a28576de
Thomas Jay Rush <jrush@quickblocks.io>
2019-09-24 09:52:56 -0400

    Cleaning up options generator

--- src/apps/cacheStatus/options.cpp
+++ src/apps/cacheStatus/options.cpp
@@ -14,7 +14,7 @@ static const COption params[] = {
     COption("start", "", "<blknum>", OPT_FLAG, "starting block for data retreival"),
     COption("fmt", "x", "enum[none|json*|txt|csv|api]", OPT_HIDDEN | OPT_FLAG, "export format (one of [none|json*|txt|csv|api])"),
     COption("config-get", "g", "", OPT_HIDDEN | OPT_SWITCH, "returns JSON data of the editable configuration file items"),
-    COption("config-set", "s", "", OPT_HIDDEN | OPT_FLAG, "accepts JSON config data and writes it to configuration files"),
+    COption("config-set", "s", "", OPT_HIDDEN | OPT_SWITCH, "accepts JSON in an env variable and writes it to configuration files"),
     COption("", "", "", OPT_DESCRIPTION, "Report on status of one or more TrueBlocks caches."),
 // END_CODE_OPTIONS
 };
