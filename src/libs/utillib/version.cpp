790e32d24c347ec6e5ede4e028d74f13e3eb3e42
Thomas Jay Rush <jrush@quickblocks.io>
2020-02-07 23:53:21 -0500

    Updating version string to 0.6.7 for test cases

--- src/libs/utillib/version.cpp
+++ src/libs/utillib/version.cpp
@@ -17,7 +17,7 @@ namespace qblocks {
 
 #define MAJOR 0
 #define MINOR 6
-#define BUILD 6
+#define BUILD 7
 #define SUBVERS "alpha"
 #define PRODUCT_NAME "GHC-TrueBlocks//"
 //--------------------------------------------------------------------------------
