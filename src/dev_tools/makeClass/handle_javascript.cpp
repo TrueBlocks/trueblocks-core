584cfd0d77815e02f4054211d6d4be67824a9de6
tjayrush <jrush@greathill.com>
2020-04-14 12:20:31 -0400

    Updating javascript auto generation code

--- src/dev_tools/makeClass/handle_javascript.cpp
+++ src/dev_tools/makeClass/handle_javascript.cpp
@@ -344,7 +344,7 @@ bool COptions::handle_generate_js_menus(void) {
             menuStream << "      label: '" << page.properName << "'," << endl;
             menuStream << "      exact: true," << endl;
             if (page.subpages.size() == 1 && page.subpages[0].route == "/")
-                menuStream << "      path: '/'," << endl;
+                menuStream << "      route: '/'," << endl;
             if (page.subpages.size() > 1)
                 menuStream << "      items: [" << endl;
 
@@ -366,7 +366,7 @@ bool COptions::handle_generate_js_menus(void) {
                 else if (sub.subpage != "") {
                     menuStream << "        { label: '" << sub.subpage << "'";
                     if (!sub.route.empty())
-                        menuStream << ", path: '" << sub.route << "'";
+                        menuStream << ", route: '" << sub.route << "'";
                     menuStream << " }," << endl;
                 }
             }
@@ -386,124 +386,3 @@ bool COptions::handle_generate_js_menus(void) {
 
     return true;
 }
-
-//    // auto-generate: pages-menus
-//    { label: 'Dashboard', exact: true, path: '/' },
-//    { label: 'Separator' },
-//    {
-//      label: 'Projects',
-//      exact: true,
-//      items: [
-//        {
-//          label: 'View...',
-//          enableFunc: (page) => {
-//            return page === 'view';
-//          },
-//        },
-//        { label: 'Edit...' },
-//        { label: 'Save' },
-//        { label: 'Export' },
-//      ],
-//    },
-//    {
-//      label: 'Monitors',
-//      exact: true,
-//      items: [
-//        { label: 'Your Monitors', path: 'yours' },
-//        { label: 'Shared Monitors', path: 'shared' },
-//      ],
-//    },
-//    {
-//      label: 'Explorer',
-//      exact: true,
-//      items: [
-//        { label: 'Accounts' },
-//        { label: 'Blocks' },
-//        { label: 'Transactions' },
-//        { label: 'Receipts' },
-//        { label: 'Logs' },
-//        { label: 'Traces' },
-//      ],
-//    },
-//    { label: 'Separator' },
-//    // auto-generate: pages-menus
-//    {
-//      label: 'Names',
-//      exact: true,
-//      items: [
-//        { label: 'Your Names', path: 'yours' },
-//        { label: 'Wallets' },
-//        { label: 'Tokens' },
-//        { label: 'Prefunds' },
-//        { label: 'Other Names', path: 'other' },
-//        { label: 'Groups' },
-//      ],
-//    },
-//    {
-//      label: 'Signatures',
-//      exact: true,
-//      items: [
-//        { label: 'Known' },
-//        { label: 'Monitored' },
-//        { label: 'Separator' },
-//        { label: 'Names' },
-//        { label: 'Params' },
-//        { label: 'Cross' },
-//      ],
-//    },
-//    {
-//      label: 'Digests',
-//      exact: true,
-//      items: [
-//        { label: 'Finalized' },
-//        { label: 'Staged' },
-//        { label: 'Unripe' },
-//        { label: 'Separator' },
-//        { label: 'Columns' },
-//      ],
-//    },
-//    {
-//      label: 'Caches',
-//      exact: true,
-//      items: [
-//        { label: 'Blocks' },
-//        { label: 'Transactions' },
-//        { label: 'Traces' },
-//        { label: 'Separator' },
-//        { label: 'Abis' },
-//        { label: 'Slurps' },
-//        { label: 'Prices' },
-//      ],
-//    },
-//    {
-//      label: 'Other',
-//      exact: true,
-//      items: [{ label: 'Your Blocks' }, { label: 'Known Blocks' }, { label: 'Dated Blocks' }],
-//    },
-//    { label: 'Separator' },
-//    {
-//      label: 'Settings',
-//      exact: true,
-//      items: [
-//        { label: 'API Config', path: 'api' },
-//        { label: 'Node Config', path: 'node' },
-//        { label: 'Scraper Config', path: 'scraper' },
-//        { label: 'Sharing Config', path: 'sharing' },
-//        { label: 'Separator' },
-//        { label: 'Skins' },
-//        { label: 'Schemas' },
-//      ],
-//    },
-//    {
-//      label: 'Support',
-//      exact: true,
-//      items: [
-//        { label: 'Contact Us', path: 'contact' },
-//        { label: 'Separator' },
-//        { label: 'Hot Keys', path: 'keys' },
-//        { label: 'Documentation' },
-//        { label: 'Separator' },
-//        { label: 'Licensing' },
-//        { label: 'About Us', path: 'about' },
-//      ],
-//    },
