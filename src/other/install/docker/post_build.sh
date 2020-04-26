61454281311fdd3e770314b81d63606cc8b03257
Thomas Jay Rush <jrush@quickblocks.io>
2020-04-26 14:00:25 -0400

    Working on docker build

--- src/other/install/docker/post_build.sh
+++ src/other/install/docker/post_build.sh
@@ -1,3 +1,16 @@
 touch /root/.quickBlocks/names/names_prefunds.txt
 echo "apiProvider=\"http://localhost:80\"" >>/root/.quickBlocks/quickBlocks.toml
+mkdir -pv /root/.quickBlocks/cache/addr_index/finalized
+mkdir -pv /root/.quickBlocks/cache/addr_index/blooms
+mkdir -pv /root/.quickBlocks/cache/addr_index/staging
+mkdir -pv /root/.quickBlocks/cache/addr_index/ripe
+mkdir -pv /root/.quickBlocks/cache/addr_index/unripe
+mkdir -pv /root/.quickBlocks/cache/addr_index/finalized
+mkdir -pv /root/.quickBlocks/cache/monitors
+mkdir -pv /root/.quickBlocks/cache/prices
+mkdir -pv /root/.quickBlocks/cache/tmp
+mkdir -pv /root/.quickBlocks/cache/blocks
+mkdir -pv /root/.quickBlocks/cache/txs
+mkdir -pv /root/.quickBlocks/cache/traces
+mkdir -pv /root/.quickBlocks/cache/slurps
 cat "/root/.quickBlocks/quickBlocks.toml"
