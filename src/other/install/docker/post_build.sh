afbc34e369218a2eab309ee4697cd0b6fd2fe177
Thomas Jay Rush <jrush@quickblocks.io>
2020-04-26 11:22:50 -0400

    Working on docker build

--- src/other/install/docker/post_build.sh
+++ src/other/install/docker/post_build.sh
@@ -1,2 +1,3 @@
 touch /root/.quickBlocks/names/names_prefunds.txt
-ls -lR "/root/.quickBlocks/"
+echo "apiProvider=\"http://localhost:80\" >>/root/.quickBlocks/quickBlocks.toml
+cat "/root/.quickBlocks/quickBlocks.toml"
