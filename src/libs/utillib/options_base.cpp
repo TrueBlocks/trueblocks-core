b822555bcedecb28024d616881c2aa51024151c0
tjayrush <jrush@quickblocks.io>
2021-01-18 12:26:55 -0500

    Updating build

--- src/libs/utillib/options_base.cpp
+++ src/libs/utillib/options_base.cpp
@@ -76,14 +76,19 @@ bool COptionsBase::prepareArguments(int argCountIn, const char* argvIn[]) {
         }
         cerr << endl;
         cerr << getProgName() << " " << os.str() << endl;
+        CStringArray envs = {
+            "API_MODE",     "DOCKER_MODE",     "PROG_NAME",      "FRESHEN_FLAGS",    "HIDE_NAMES",
+            "IPFS_PATH",    "SILENCE",         "NO_CACHE",       "NO_NAMES",         "NO_PROGRESS",
+            "NO_SCHEMAS",   "TB_NAME_ADDRESS", "TB_NAME_CUSTOM", "TB_NAME_DECIMALS", "TB_NAME_DESCR",
+            "TB_NAME_NAME", "TB_NAME_SOURCE",  "TB_NAME_SYMBOL", "TB_NAME_TAG"
+            // "TEST_MODE", "NO_COLOR", "REDIR_CERR", "EDITOR",
+        };
+        for (auto key : envs) {
+            string_q val = getEnvStr(key);
+            if (!val.empty())
+                cerr << key << " = [" << val << "]" << endl;
+        }
     }
-
-    //        // send out the environment, if any non-default
-    //        if (isTestMode() || isLevelOn(sev_debug4)) {
-    //            if (isApiMode())                     { LOG4("API_MODE=",    getEnvStr("API_MODE")); }
-    //            if (isDockerMode())                  { LOG4("DOCKER_MODE=", getEnvStr("DOCKER_MODE")); }
-    //            if (!getEnvStr("IPFS_PATH").empty()) { LOG4("IPFS_PATH=",   getEnvStr("IPFS_PATH")); }
-    //        }
 
     CStringArray argumentsIn;
     for (auto arg : separatedArgs) {
