3edd2d77d3e733d373bb8d6e2920321bde8b560a
user1 <user1@iMac.lan>
2021-01-20 16:16:17 -0500

    Working on a different user

--- test/gold/tools/grabABI/generated/grabABI.h
+++ test/gold/tools/grabABI/generated/grabABI.h
@@ -13,36 +13,6 @@
 #include "etherlib.h"
 #include "tokenlib.h"
 #include "walletlib.h"
-#include "constructor.h"
-#include "approve.h"
-#include "changeallowedrecipients.h"
-#include "changeproposaldeposit.h"
-#include "createtokenproxy.h"
-#include "executeproposal.h"
-#include "getmyreward.h"
-#include "halveminquorum.h"
-#include "newcontract.h"
-#include "newproposal.h"
-#include "receiveether.h"
-#include "refund.h"
-#include "retrievedaoreward.h"
-#include "splitdao.h"
-#include "transfer.h"
-#include "transferfrom.h"
-#include "transferfromwithoutreward.h"
-#include "transferwithoutreward.h"
-#include "unblockme.h"
-#include "vote.h"
-#include "allowedrecipientchangedevent.h"
-#include "approvalevent.h"
-#include "createdtokenevent.h"
-#include "fuelingtodateevent.h"
-#include "newcuratorevent.h"
-#include "proposaladdedevent.h"
-#include "proposaltalliedevent.h"
-#include "refundevent.h"
-#include "transferevent.h"
-#include "votedevent.h"
 #include "visitor.h"
 
 //------------------------------------------------------------------------
@@ -51,65 +21,9 @@ extern const CTransaction *promoteToFunc(const CTransaction *p);
 extern const CLogEntry *promoteToEvent(const CLogEntry *p);
 
 //------------------------------------------------------------------------
-extern const string_q func_;
-extern const string_q func_DAOpaidOut;
-extern const string_q func_DAOrewardAccount;
-extern const string_q func_actualBalance;
-extern const string_q func_allowance;
-extern const string_q func_allowedRecipients;
-extern const string_q func_approve;
-extern const string_q func_balanceOf;
-extern const string_q func_blocked;
-extern const string_q func_changeAllowedRecipients;
-extern const string_q func_changeProposalDeposit;
-extern const string_q func_checkProposalCode;
-extern const string_q func_closingTime;
-extern const string_q func_createTokenProxy;
-extern const string_q func_curator;
-extern const string_q func_daoCreator;
-extern const string_q func_divisor;
-extern const string_q func_executeProposal;
-extern const string_q func_extraBalance;
-extern const string_q func_getMyReward;
-extern const string_q func_getNewDAOAddress;
-extern const string_q func_halveMinQuorum;
-extern const string_q func_isFueled;
-extern const string_q func_lastTimeMinQuorumMet;
-extern const string_q func_minQuorumDivisor;
-extern const string_q func_minTokensToCreate;
-extern const string_q func_newContract;
-extern const string_q func_newProposal;
-extern const string_q func_numberOfProposals;
-extern const string_q func_paidOut;
-extern const string_q func_privateCreation;
-extern const string_q func_proposalDeposit;
-extern const string_q func_proposals;
-extern const string_q func_receiveEther;
-extern const string_q func_refund;
-extern const string_q func_retrieveDAOReward;
-extern const string_q func_rewardAccount;
-extern const string_q func_rewardToken;
-extern const string_q func_splitDAO;
-extern const string_q func_totalRewardToken;
-extern const string_q func_totalSupply;
-extern const string_q func_transfer;
-extern const string_q func_transferFrom;
-extern const string_q func_transferFromWithoutReward;
-extern const string_q func_transferWithoutReward;
-extern const string_q func_unblockMe;
-extern const string_q func_vote;
-
+// No functions
 //------------------------------------------------------------------------
-extern const string_q evt_AllowedRecipientChanged;
-extern const string_q evt_Approval;
-extern const string_q evt_CreatedToken;
-extern const string_q evt_FuelingToDate;
-extern const string_q evt_NewCurator;
-extern const string_q evt_ProposalAdded;
-extern const string_q evt_ProposalTallied;
-extern const string_q evt_Refund;
-extern const string_q evt_Transfer;
-extern const string_q evt_Voted;
+// No events
 
 //-----------------------------------------------------------------------------
 extern const string_q sigs[];
