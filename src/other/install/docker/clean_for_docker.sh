17c54ad1ed8dcc5f782b43f4330da6a6fbf1d93d
tjayrush <jrush@quickblocks.io>
2021-03-02 21:21:52 -0500

    Cleaning up docker build

--- src/other/install/docker/clean_for_docker.sh
+++ src/other/install/docker/clean_for_docker.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 cp  -pf  "../src/other/install/docker/CMakeLists.txt"                 "../src/"
 cp  -pf  "../src/other/install/docker/libs/CMakeLists.txt"            "../src/libs/"
 cp  -pf  "../src/other/install/docker/tools/CMakeLists.txt"           "../src/tools/"
 cp  -pf  "../src/other/install/docker/dev_tools/CMakeLists.txt"       "../src/dev_tools/"
 cp  -pf  "../src/other/install/docker/other/install/qb_config.cmake"  "../src/other/install/"
-ls -lR "../src/other/install"
+# ls -lR "../src/other/install"
--- src/other/install/docker/post_build.sh
+++ src/other/install/docker/post_build.sh
@@ -1,28 +1,27 @@
-touch /root/.quickBlocks/names/names_prefunds.txt
-echo "apiProvider=\"http://localhost:80\"" >>/root/.quickBlocks/quickBlocks.toml
+#!/usr/bin/env bash
 
-mkdir /root/.quickBlocks/cache/
-mkdir /root/.quickBlocks/cache/abis
-mkdir /root/.quickBlocks/cache/addr_index
-mkdir /root/.quickBlocks/cache/addr_index/blooms
-mkdir /root/.quickBlocks/cache/addr_index/finalized
-mkdir /root/.quickBlocks/cache/addr_index/ripe
-mkdir /root/.quickBlocks/cache/addr_index/staging
-mkdir /root/.quickBlocks/cache/addr_index/unripe
-mkdir /root/.quickBlocks/cache/monitors
-mkdir /root/.quickBlocks/cache/monitors/staging
-mkdir /root/.quickBlocks/cache/prices
-mkdir /root/.quickBlocks/cache/tmp
+# touch /root/.quickBlocks/names/names*.txt
+# echo "apiProvider=\"http://localhost:80\"" >>/root/.quickBlocks/quickBlocks.toml
 
-mkdir /root/.quickBlocks/abis
-mkdir /root/.quickBlocks/chifra
-mkdir /root/.quickBlocks/grabABI
-mkdir /root/.quickBlocks/grabABI/docs
-mkdir /root/.quickBlocks/grabABI/parselib
-mkdir /root/.quickBlocks/manifest
-mkdir /root/.quickBlocks/makeClass
-mkdir /root/.quickBlocks/names
-
-#cat "/root/.quickBlocks/quickBlocks.toml"
-
-ls -lR "/root/.quickBlocks/"
+# mkdir /root/.quickBlocks/cache/
+# mkdir /root/.quickBlocks/cache/abis
+# mkdir /root/.quickBlocks/cache/addr_index
+# mkdir /root/.quickBlocks/cache/addr_index/blooms
+# mkdir /root/.quickBlocks/cache/addr_index/finalized
+# mkdir /root/.quickBlocks/cache/addr_index/ripe
+# mkdir /root/.quickBlocks/cache/addr_index/staging
+# mkdir /root/.quickBlocks/cache/addr_index/unripe
+# mkdir /root/.quickBlocks/cache/monitors
+# mkdir /root/.quickBlocks/cache/monitors/staging
+# mkdir /root/.quickBlocks/cache/prices
+# mkdir /root/.quickBlocks/cache/tmp
+# mkdir /root/.quickBlocks/abis
+# mkdir /root/.quickBlocks/chifra
+# mkdir /root/.quickBlocks/grabABI
+# mkdir /root/.quickBlocks/grabABI/docs
+# mkdir /root/.quickBlocks/grabABI/parselib
+# mkdir /root/.quickBlocks/manifest
+# mkdir /root/.quickBlocks/makeClass
+# mkdir /root/.quickBlocks/names
+# cat "/root/.quickBlocks/quickBlocks.toml"
+# ls -lR "/root/.quickBlocks/"
