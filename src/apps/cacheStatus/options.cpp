18ccb55cf5869534dfc591522495dc3b5ad8cd8b
tjayrush <jrush@quickblocks.io>
2021-03-11 03:40:17 -0500

    Fixing typo

--- src/apps/cacheStatus/options.cpp
+++ src/apps/cacheStatus/options.cpp
@@ -245,7 +245,7 @@ void COptions::Init(void) {
     status.is_tracing = isTracingNode();
     status.has_eskey = getGlobalConfig("")->getConfigStr("settings", "etherscan_key", "<not_set>") != "<not_set>";
     status.has_pinkey =
-        getGlobalConfig("blcokScrape")->getConfigStr("settings", "pinata_api_key", "<not_set>") != "<not_set>";
+        getGlobalConfig("blockScrape")->getConfigStr("settings", "pinata_api_key", "<not_set>") != "<not_set>";
 }
 
 //---------------------------------------------------------------------------------------------------
