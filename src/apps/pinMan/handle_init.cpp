bd42d6100192e95314d92c057fb2de0c7c224899
tjayrush <jrush@quickblocks.io>
2021-04-17 20:15:27 -0400

    Working on chifra pins

--- src/apps/pinMan/handle_init.cpp
+++ src/apps/pinMan/handle_init.cpp
@@ -4,14 +4,33 @@
 bool COptions::handle_init(void) {
     if (!freshen_from_remote())
         return usage("Could not freshen remote pin data");
+
     CPinnedChunkArray remotePins;
     pinlib_readPinList(remotePins, true, false);
     for (auto pin : remotePins) {
-        // string_q outFile = getIndexPath("blooms/" + pin.fileName + ".bloom.gz ");
         string_q outFile = "blooms/" + pin.fileName + ".bloom";
-        string_q zipFile = outFile + ".gz";
-        if (!fileExists(outFile) && !fileExists(zipFile)) {
-            cout << "curl -o " << zipFile << " \"http://gateway.ipfs.io/ipfs/" << pin.bloomHash << "\"" << endl;
+        if (!fileExists(getIndexPath(outFile))) {
+            string_q zipFile = outFile + ".gz";
+            if (!fileExists(getIndexPath(zipFile))) {
+                // download from ipfs gateway
+                ostringstream cmd;
+                cmd << "curl -o " << getIndexPath(zipFile) << " \"http://gateway.ipfs.io/ipfs/" << pin.bloomHash << "\" ";
+                // LOG_INFO("Downloading " + zipFile);
+                LOG_INFO("Downloading " + cmd.str());
+                doCommand(cmd.str());
+            }
+            if (!fileExists(getIndexPath(zipFile))) {
+                LOG_WARN("Could download bloom file zip ", zipFile);
+            } else {
+                ostringstream cmd;
+                cmd << "cd " << getIndexPath("") << " && gunzip --keep " << zipFile << " && cd -";
+                doCommand(cmd.str());
+                if (!fileExists(getIndexPath(outFile))) {
+                    LOG_WARN("Could not create bloom file ", outFile);
+                }
+            }
+        } else {
+            LOG_INFO("Bloom file ", outFile, " exists.");
         }
     }
     return true;  // do not continue
