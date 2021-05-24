2e152dad1aa84504885ed6651482c6a3869baf85
tjayrush <jrush@quickblocks.io>
2021-05-24 11:21:14 -0400

    Cleaning up testing routines

--- src/other/build_assets/test-all.sh
+++ src/other/build_assets/test-all.sh
@@ -4,7 +4,8 @@ cd ..
 export QUICKBLOCKS=`pwd`
 export BUILD_FOLDER=$QUICKBLOCKS/build/
 export TEST_FOLDER=$QUICKBLOCKS/test/
-export NAMES_FOLDER=$QUICKBLOCKS/src/other/install/names/
+export NAMES_SOURCE=$QUICKBLOCKS/src/other/install/names/names.tab
+export NAMES_CUSTOM=$QUICKBLOCKS/src/other/install/names/names_custom.tab
 
 #echo "Making..."
 cd "$BUILD_FOLDER/"
@@ -17,9 +18,25 @@ make -j 8
 
 test-api.sh --filter all --mode both --clean --report $@
 
-cp -f "$NAMES_FOLDER/names.tab" "$HOME/Library/Application Support/TrueBlocks/names/"
-touch "$HOME/Library/Application Support/TrueBlocks/names/names.tab"
-rm -fR "$HOME/Library/Application Support/TrueBlocks/cache/names"
+# Special case for customized customized names
+if [[ -f "$HOME/Desktop/names_custom.tab" ]]
+then
+    export NAMES_CUSTOM="$HOME/Desktop/names_custom.tab"
+fi
+
+export DEST_FOLDER=$HOME/Library/Application\ Support/TrueBlocks/names
+if [[ -d $DEST_FOLDER ]]
+then
+    # do nothing
+    echo ""
+else
+    export DEST_FOLDER="$HOME/.local/share/TrueBlocks/names"
+fi
+
+cp -f "$NAMES_SOURCE" "$DEST_FOLDER"
+cp -f "$NAMES_CUSTOM" "$DEST_FOLDER"
+touch "$DEST_FOLDER/names.tab"
+touch "$DEST_FOLDER/names_custom.tab"
 
 cd $BUILD_FOLDER
 echo "Done..."
