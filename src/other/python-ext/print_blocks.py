e2c98910d415883ad6b05d8b18e068d6cd072af1
tjayrush <jrush@quickblocks.io>
2021-06-03 21:36:01 -0400

    Playing with python:

--- src/other/python-ext/print_blocks.py
+++ src/other/python-ext/print_blocks.py
@@ -1,6 +1,6 @@
 from quickblocks import QuickBlocks
 ##add a node source in the foloowing line
-qb = QuickBlocks('', cache=True)
+qb = QuickBlocks('http://localhost:36963', cache=True)
 print(qb.getTransaction(txhash='0x8b5df1ea9d2c7c3bde45cb4df13e51797caa89ab0909b408f6d5075c7f45747c'))
 
 ##for i in range(5200000):
