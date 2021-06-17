15aa223bfb9598f3e0402adef8006bc735f2490e
tjayrush <jrush@quickblocks.io>
2021-06-16 23:14:24 -0400

    Working on python wrapper

--- src/other/python-ext/print_blocks.py
+++ src/other/python-ext/print_blocks.py
@@ -1,9 +1,8 @@
 from quickblocks import QuickBlocks
 ##add a node source in the foloowing line
 qb = QuickBlocks('http://localhost:36963', cache=True)
-print(qb.getTransaction(txhash='0x8b5df1ea9d2c7c3bde45cb4df13e51797caa89ab0909b408f6d5075c7f45747c'))
+# print(qb.getTransaction(txhash='0x8b5df1ea9d2c7c3bde45cb4df13e51797caa89ab0909b408f6d5075c7f45747c'))
 
-##for i in range(5200000):
-  #  block = qb[i]
-   # if i % 1000 == 0:
-    #    print(block)
+for i in range(5200000):
+    block = qb[i]
+    print(block)
