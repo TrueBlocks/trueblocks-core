441b500864bda1ee8707c86a3871762e246010c0
tjayrush <jrush@quickblocks.io>
2021-08-26 10:28:13 -0400

    Breaking the build on purpose to check ci

--- src/apps/acctExport/main.cpp
+++ src/apps/acctExport/main.cpp
@@ -1,3 +1,4 @@
+x
 /*-------------------------------------------------------------------------
  * This source code is confidential proprietary information which is
  * copyright (c) 2016, 2021 TrueBlocks, LLC (http://trueblocks.io)
