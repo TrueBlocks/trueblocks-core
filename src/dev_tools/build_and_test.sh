83a0b63e02076bfe2d4af97920e3e7faa06109c3
Dawid Szlachta <git@makingprogress.dev>
2021-09-01 17:01:01 +0200

    Move commands to a script file

---
+++ src/dev_tools/build_and_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo $(pwd)
+
+# Build the binaries
+mkdir build && cd build
+cmake ../src
+make -j 4
+
+# Set the correct PATH
+export PATH=$(pwd)/../bin:$(pwd)/../bin/test:$PATH
+
+# Run test
+make test-all
