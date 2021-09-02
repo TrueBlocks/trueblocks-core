5c0f79142881327cab7b6863add1cd24c165dccf
Dawid Szlachta <git@makingprogress.dev>
2021-09-02 13:25:15 +0200

    Temporarily disabled building

--- src/dev_tools/build_and_test.sh
+++ src/dev_tools/build_and_test.sh
@@ -3,9 +3,12 @@
 echo $(pwd)
 
 # Build the binaries
-mkdir build && cd build
-cmake ../src
-make -j 4
+
+# FIXME: uncomment this
+
+# mkdir build && cd build
+# cmake ../src
+# make -j 4
 
 # Set the correct PATH
 export PATH=$(pwd)/../bin:$(pwd)/../bin/test:$PATH
