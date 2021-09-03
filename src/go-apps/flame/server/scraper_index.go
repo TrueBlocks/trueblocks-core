e37b818453dbf90351f6b400e01410c353d027ee
tjayrush <jrush@quickblocks.io>
2021-09-02 23:14:46 -0400

    Moves n_blocks to config item, enables --pin and --sleep options to chifra scrape

--- src/go-apps/flame/server/scraper_index.go
+++ src/go-apps/flame/server/scraper_index.go
@@ -37,6 +37,9 @@ func RunIndexScraper() {
 			if IndexScraper.Verbose > 0 {
 				options += " --verbose " + strconv.Itoa(int(IndexScraper.Verbose))
 			}
+			if Options.Pin == true {
+				options += " --pins "
+			}
 			log.Print(IndexScraper.Color, IndexScraper.Name, ": blockScrape", options, utils.Off, "\n")
 			cmd := exec.Command(GetCommandPath("blockScrape"), options)
 			stderrPipe, err := cmd.StderrPipe()
