336ca2b0c4bd212215e5e0ecbd2ea2ff5f59ba8f
tjayrush <jrush@quickblocks.io>
2021-09-21 22:59:04 -0400

    Adding all commands, added testing

--- src/apps/chifra-new/cmd/serve.go
+++ src/apps/chifra-new/cmd/serve.go
@@ -18,9 +18,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// exploreCmd represents the explore command
-var exploreCmd = &cobra.Command{
-	Use:   "explore",
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -29,20 +29,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("explore called")
+		fmt.Println("serve called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(exploreCmd)
+	rootCmd.AddCommand(serveCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// exploreCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// exploreCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- src/apps/chifra-new/cmd/transactions.go
+++ src/apps/chifra-new/cmd/transactions.go
@@ -18,9 +18,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// exploreCmd represents the explore command
-var exploreCmd = &cobra.Command{
-	Use:   "explore",
+// transactionsCmd represents the transactions command
+var transactionsCmd = &cobra.Command{
+	Use:   "transactions",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -29,20 +29,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("explore called")
+		fmt.Println("transactions called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(exploreCmd)
+	rootCmd.AddCommand(transactionsCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// exploreCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// transactionsCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// exploreCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// transactionsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
