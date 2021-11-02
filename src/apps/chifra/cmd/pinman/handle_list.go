c1edd3ff9affdcf212390dd7898e08ec52225dd9
tjayrush <jrush@quickblocks.io>
2021-11-01 23:19:22 -0400

    Turned off a few tests, but documented where in pinMan.csv test cases

--- src/apps/chifra/cmd/pinman/handle_list.go
+++ src/apps/chifra/cmd/pinman/handle_list.go
@@ -13,11 +13,13 @@
 package pinman
 
 import (
+	"fmt"
 	"sort"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/output"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/pinlib/manifest"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/utils"
 )
 
 // HandleList loads manifest, sorts pins and prints them out
@@ -32,29 +34,43 @@ func HandleList(format string) {
 	sort.Slice(manifestData.NewPins, func(i, j int) bool {
 		iPin := manifestData.NewPins[i]
 		jPin := manifestData.NewPins[j]
-
 		return iPin.FileName < jPin.FileName
 	})
 
-	if format == "json" {
+	// Shorten the array for testing
+	if utils.IsTestMode() {
+		manifestData.NewPins = manifestData.NewPins[:100]
+	}
+
+	// TODO: if Root.to_file == true, write the output to a filename
+	// TODO: if Root.output == <fn>, write the output to a <fn>
+
+	if format == "" || format == "none" {
+		if utils.IsApiMode() {
+			format = "api"
+		} else {
+			format = "txt"
+		}
+	}
+
+	outFmt := "%s\t%s\t%s\n"
+	switch format {
+	case "txt":
+		// do nothing
+	case "csv":
+		outFmt = "\"%s\",\"%s\",\"%s\"\n"
+	case "json":
+		fallthrough
+	case "api":
 		err := output.PrintJson(manifestData.NewPins)
 		if err != nil {
 			logger.Fatal(err)
 		}
-
 		return
 	}
 
-	table := &output.Table{}
-	table.New()
-	table.Header([]string{
-		"filename",
-		"bloomhash",
-		"indexhash",
-	})
-
+	fmt.Printf(outFmt, "filename", "bloomhash", "indexhash")
 	for _, pin := range manifestData.NewPins {
-		table.Row([]string{pin.FileName, pin.BloomHash, pin.IndexHash})
+		fmt.Printf(outFmt, pin.FileName, pin.BloomHash, pin.IndexHash)
 	}
-	table.Print()
 }
