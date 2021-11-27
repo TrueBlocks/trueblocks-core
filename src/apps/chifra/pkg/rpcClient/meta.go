50f7f19539003851d56e8524a87a5a3a0de00700
tjayrush <jrush@quickblocks.io>
2021-11-26 20:00:19 -0500

    Updating license text at top of go files

--- src/apps/chifra/pkg/rpcClient/meta.go
+++ src/apps/chifra/pkg/rpcClient/meta.go
@@ -1,17 +1,8 @@
-package rpcClient
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
 
-/*-------------------------------------------------------------------------------------------
- * qblocks - fast, easily-accessible, fully-decentralized data from blockchains
- * copyright (c) 2016, 2021 TrueBlocks, LLC (http://trueblocks.io)
- *
- * This program is free software: you may redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version. This program is
- * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details. You should have received a copy of the GNU General
- * Public License along with this program. If not, see http://www.gnu.org/licenses/.
- *-------------------------------------------------------------------------------------------*/
+package rpcClient
 
 import (
 	"context"
--- src/apps/chifra/server/server.go
+++ src/apps/chifra/server/server.go
@@ -1,3 +1,7 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+
 package servePkg
 
 import (
