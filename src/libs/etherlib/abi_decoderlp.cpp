315776cc58eaccf1a2ce80122e8b59d348946ce5
tjayrush <jrush@quickblocks.io>
2021-12-12 20:01:45 -0500

    Trying to fix #1963

--- src/libs/etherlib/abi_decoderlp.cpp
+++ src/libs/etherlib/abi_decoderlp.cpp
@@ -10,7 +10,7 @@
  * General Public License for more details. You should have received a copy of the GNU General
  * Public License along with this program. If not, see http://www.gnu.org/licenses/.
  *-------------------------------------------------------------------------------------------*/
-#define LOGGING_LEVEL_TEST
+// #define LOGGING_LEVEL_TEST
 #include <algorithm>
 #include "abi.h"
 #include "node.h"
