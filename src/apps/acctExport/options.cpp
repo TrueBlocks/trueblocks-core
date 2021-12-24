7d9e975c98f9403999b393174ed66fdd8692ec92
tjayrush <jrush@quickblocks.io>
2021-12-24 16:35:18 -0500

    Only load preloads if we have an appearance at block zero

--- src/apps/acctExport/options.cpp
+++ src/apps/acctExport/options.cpp
@@ -328,7 +328,7 @@ bool COptions::parseArguments(string_q& command) {
     for (auto t : topic)
         logFilter.topics.push_back(t);
 
-    if (!loadNamesPrefunds())
+    if (!loadNames())
         return usage("Could not load names database.");
 
     // Where will we start?
