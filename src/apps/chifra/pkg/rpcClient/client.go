7bfbc610425e9653a247921a3260013281c8cf8c
tjayrush <jrush@quickblocks.io>
2022-01-28 21:03:24 -0500

    Messing with Dial

--- src/apps/chifra/pkg/rpcClient/client.go
+++ src/apps/chifra/pkg/rpcClient/client.go
@@ -14,15 +14,22 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-// Get sets up a client instance and returns it
-func GetClient(provider string) ethclient.Client {
-	// TODO: I don't like the fact that we Dail In every time we want to us this
-	// TODO: If we make this a cached item, it needs to be cached per chain, see timestamps
-	ec, err := ethclient.Dial(provider)
-	if err != nil {
-		log.Fatalln(err)
+// TODO: this looks weird, but before we were creating and deleting the client with every call which
+// TODO: overran the number of TPC connection the OS would create (on a Mac). Since then, we now
+// TODO: open the connection once and just use it allowing the operating system to clean it up
+var theClient *ethclient.Client
+
+func GetClient(provider string) *ethclient.Client {
+	if theClient == nil {
+		// TODO: I don't like the fact that we Dail In every time we want to us this
+		// TODO: If we make this a cached item, it needs to be cached per chain, see timestamps
+		ec, err := ethclient.Dial(provider)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		theClient = ec
 	}
-	return *ec
+	return theClient
 }
 
 // BlockNumber returns the front of chain block
