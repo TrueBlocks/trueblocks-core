0f053720b56840269fcf61bdca97f5618a0f4f08
tjayrush <jrush@quickblocks.io>
2022-02-07 18:04:56 -0500

    Nothing

--- src/other/single/single.cpp
+++ src/other/single/single.cpp
@@ -18,7 +18,7 @@ int main(int argc, const char* argv[]) {
     etherlib_init(quickQuitHandler);
     forEveryFileInFolder("./", visitFile, NULL);
     etherlib_cleanup();
-    return 1;
+    return 0;
 }
 
 //----------------------------------------------------------------
