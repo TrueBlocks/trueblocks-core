4ba1a86458672be13b67c9d0fd5ee9b8c3b4e652
tjayrush <jrush@quickblocks.io>
2022-02-07 21:28:37 -0500

    Fixing tests

--- src/apps/chifra/internal/explore/validate_integration_test.go
+++ src/apps/chifra/internal/explore/validate_integration_test.go
@@ -1,3 +1,5 @@
+// +build integration
+
 // Copyright 2021 The TrueBlocks Authors. All rights reserved.
 // Use of this source code is governed by a license that can
 // be found in the LICENSE file.
