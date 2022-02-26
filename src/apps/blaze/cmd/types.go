ffc9dc45d112ca16712c23921923fcb9cd8c20b2
tjayrush <jrush@quickblocks.io>
2022-02-25 21:28:26 -0500

    Moved blaze to the apps folder, removed old go-apps folder

---
+++ src/apps/blaze/cmd/types.go
@@ -0,0 +1,124 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+
+package cmd
+
+// BlockHeader carries values returned by the `eth_getBlock` RPC command
+type BlockHeader struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Result  struct {
+		Author           string   `json:"author"`
+		Difficulty       string   `json:"difficulty"`
+		ExtraData        string   `json:"extraData"`
+		GasLimit         string   `json:"gasLimit"`
+		GasUsed          string   `json:"gasUsed"`
+		Hash             string   `json:"hash"`
+		LogsBloom        string   `json:"logsBloom"`
+		Miner            string   `json:"miner"`
+		MixHash          string   `json:"mixHash"`
+		Nonce            string   `json:"nonce"`
+		Number           string   `json:"number"`
+		ParentHash       string   `json:"parentHash"`
+		ReceiptsRoot     string   `json:"receiptsRoot"`
+		SealFields       []string `json:"sealFields"`
+		Sha3Uncles       string   `json:"sha3Uncles"`
+		Size             string   `json:"size"`
+		StateRoot        string   `json:"stateRoot"`
+		Timestamp        string   `json:"timestamp"`
+		TransactionsRoot string   `json:"transactionsRoot"`
+	} `json:"result"`
+	ID int `json:"id"`
+}
+
+// Trace carries values returned by Parity's `trace_block` RPC command
+type Trace struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Result  []struct {
+		Action struct {
+			CallType      string `json:"callType"` // call
+			From          string `json:"from"`
+			Gas           string `json:"gas"`
+			Input         string `json:"input"`
+			To            string `json:"to"`
+			Value         string `json:"value"`
+			Author        string `json:"author"` // reward
+			RewardType    string `json:"rewardType"`
+			Address       string `json:"address"` // suicide
+			Balance       string `json:"balance"`
+			RefundAddress string `json:"refundAddress"`
+			Init          string `json:"init"` // create
+		} `json:"action,omitempty"`
+		BlockHash   string `json:"blockHash"`
+		BlockNumber int    `json:"blockNumber"`
+		Error       string `json:"error"`
+		Result      struct {
+			GasUsed string `json:"gasUsed"` // call
+			Output  string `json:"output"`
+			Address string `json:"address"` // create
+		} `json:"result"`
+		Subtraces           int           `json:"subtraces"`
+		TraceAddress        []interface{} `json:"traceAddress"`
+		TransactionHash     string        `json:"transactionHash"`
+		TransactionPosition int           `json:"transactionPosition"`
+		Type                string        `json:"type"`
+	} `json:"result"`
+	ID int `json:"id"`
+}
+
+// Log carries values returned by the eth_getLogs RPC command
+type Log struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Result  []struct {
+		Address             string   `json:"address"`
+		BlockHash           string   `json:"blockHash"`
+		BlockNumber         string   `json:"blockNumber"`
+		Data                string   `json:"data"`
+		LogIndex            string   `json:"logIndex"`
+		Removed             bool     `json:"removed"`
+		Topics              []string `json:"topics"`
+		TransactionHash     string   `json:"transactionHash"`
+		TransactionIndex    string   `json:"transactionIndex"`
+		TransactionLogIndex string   `json:"transactionLogIndex"`
+		Type                string   `json:"type"`
+	} `json:"result"`
+	ID int `json:"id"`
+}
+
+// Receipt carries values returned by the eth_getReceipt RPC call
+type Receipt struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Result  struct {
+		BlockHash         string        `json:"blockHash"`
+		BlockNumber       string        `json:"blockNumber"`
+		ContractAddress   string        `json:"contractAddress"`
+		CumulativeGasUsed string        `json:"cumulativeGasUsed"`
+		From              string        `json:"from"`
+		GasUsed           string        `json:"gasUsed"`
+		Logs              []interface{} `json:"logs"`
+		LogsBloom         string        `json:"logsBloom"`
+		Root              string        `json:"root"`
+		Status            interface{}   `json:"status"`
+		To                interface{}   `json:"to"`
+		TransactionHash   string        `json:"transactionHash"`
+		TransactionIndex  string        `json:"transactionIndex"`
+	} `json:"result"`
+	ID int `json:"id"`
+}
+
+// RPCParams are used during calls to the RPC.
+type RPCParams []interface{}
+
+// RPCPayload is used during to make calls to the RPC.
+type RPCPayload struct {
+	Jsonrpc   string `json:"jsonrpc"`
+	Method    string `json:"method"`
+	RPCParams `json:"params"`
+	ID        int `json:"id"`
+}
+
+// LogFilter is used the eth_getLogs RPC call to identify the block range to query
+type LogFilter struct {
+	Fromblock string `json:"fromBlock"`
+	Toblock   string `json:"toBlock"`
+}
