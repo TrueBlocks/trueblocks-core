1c91230cfb76c5d36ba4110de77c64750078e5e6
tjayrush <jrush@quickblocks.io>
2022-02-26 19:49:40 -0500

    Improves difficulty calc

--- src/other/difficulty/difficulty.cpp
+++ src/other/difficulty/difficulty.cpp
@@ -17,7 +17,6 @@
 #define START 14071609
 #define END 14274880
 
-#if 1
 int main(int argc, const char* argv[]) {
     loadEnvironmentPaths();
     etherlib_init(quickQuitHandler);
@@ -30,50 +29,3 @@ int main(int argc, const char* argv[]) {
     }
     return 0;
 }
-#endif
-
-// #define byzantium Block        4370000 - 3000000 = 1370000 --> period 13
-//#define constantinople Block   7280000 - 5000000 = 2280000 --> period 22
-//#define instanbul Block        9069000 - 9000000 = 69000
-// static blknum_t last = NOPOS;
-// bool oneLine(const char* str, void* data) {
-//    CStringArray parts;
-//    explode(parts, substitute(str, "\n", ""), ',');
-//    blknum_t bn = str_2_Uint(parts[0]);
-//    uint64_t fake_block = bn;
-//    if (bn > instanbul Block)
-//        fake_block = bn - 9000000;
-//    else if (bn > constantinople Block)
-//        fake_block = bn - 5000000;
-//    else if (bn > byzantium Block)
-//        fake_block = bn - 3000000;
-////    uint64_t period = (uint64_t)floor(fake_block / 100000);
-////    cout << bn << " ---- " << parts[1] << " ---- " << parts[2] << " --- " << fake_block << " --- " << period <<
-/// endl;
-//    last = bn;
-//    return true;
-//}
-
-// 11728437,1611625091,4459297003201165
-// jrush@iMac:~/D/t/s/o/difficulty|develop⚡*?➤ whenBlock latest
-// blocknumber timestamp   date    name
-// 11828006
-
-#if 0
-int main(int argc, const char* argv[]) {
-   // forEveryLineInAsciiFile("/Users/jrush/Development/tokenomics/explorations/difficulty/data/difficulty.csv", oneLine, NULL);
-    for (blknum_t bn = 8982001 ; bn < 10000000 ; bn++) { //last+1 ; bn < 10000000 ; bn++) {
-        uint64_t fake_block = bn;
-        if (bn > instanbul Block)
-            fake_block = bn - 9000000;
-        else if (bn > constantinople Block)
-            fake_block = bn - 5000000;
-        else if (bn > byzantiumBlock())
-            fake_block = bn - 3000000;
-        uint64_t period = (uint64_t)floor(fake_block / 100000);
-        if ((bn > instanbul Block - 200000) && !(bn % 2000))
-            cout << bn << " ---- " << 10 << " ---- " << 10 << " --- " << fake_block << " --- " << period << endl;
-    }
-    return 0;
-}
-#endif
