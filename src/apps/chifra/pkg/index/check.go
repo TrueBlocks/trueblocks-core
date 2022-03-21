e655a19f039c886440ddea271d568255e5bc79d1
tjayrush <jrush@quickblocks.io>
2022-03-21 08:29:41 -0400

    Moves old chunk package out of pinlib into index package

---
+++ src/apps/chifra/pkg/index/check.go
@@ -0,0 +1,53 @@
+package index
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+)
+
+func validateFileSize(file *os.File) error {
+	header, err := readHeader(file)
+	if err != nil {
+		return err
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	addressTableSize := AddrRecordWidth * header.AddressCount
+	appearanceTableSize := AppRecordWidth * header.AppearanceCount
+	expectedSize := HeaderWidth + addressTableSize + appearanceTableSize
+
+	if stat.Size() != int64(expectedSize) {
+		return fmt.Errorf("wrong file size, expected %d, but got %d", expectedSize, stat.Size())
+	}
+
+	return nil
+}
+
+func validateStructure(chunk IndexChunk) error {
+	// Try to read first address
+	_, err := chunk.File.Seek(chunk.AddrTableStart, 0)
+	if err != nil {
+		return err
+	}
+	err = binary.Read(chunk.File, binary.LittleEndian, &AddressRecord{})
+	if err != nil {
+		return err
+	}
+
+	// Try to read first appearance
+	_, err = chunk.File.Seek(chunk.AppTableStart, 0)
+	if err != nil {
+		return err
+	}
+	err = binary.Read(chunk.File, binary.LittleEndian, &AppearanceRecord{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
