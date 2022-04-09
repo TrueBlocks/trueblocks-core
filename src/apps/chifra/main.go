e48e27591bb3308a6ec54194b8b1346a852cf3c2
tjayrush <jrush@quickblocks.io>
2022-04-08 20:24:10 -0400

    Update

--- src/apps/chifra/main.go
+++ src/apps/chifra/main.go
@@ -31,57 +31,30 @@ func main() {
 // )
 
 // func main() {
+// 	newVersion := len(os.Args) > 1
+// 	// newVersion := true
 // 	addr := common.HexToAddress("0xf503017d7baf7fbc0fff7492b751025c6a78179b")
-// 	fmt.Println(addr)
-// 	for i := 0; i < 200; i++ {
+// 	for i := 0; i < 1000; i++ {
+// 		i := 0
 // 		cachePath := cache.NewCachePath("mainnet", cache.Index_Bloom)
-// 		bloomPath := cachePath.GetFullPath(cache.FileRange{First: 14435826, Last: 14438546}.String())
-// 		if len(os.Args) > 1 {
-// 			bloom, _ := NewChunk2(bloomPath)
-// 			fmt.Printf("%d %t\n", i, IsMember2(bloom.Bloom, addr))
+// 		filePath := cachePath.GetFullPath(cache.FileRange{First: 14435826, Last: 14438546}.String())
+// 		// fmt.Println(addr)
+// 		// fmt.Println(cachePath)
+// 		// fmt.Println(bloomPath)
+// 		if newVersion {
+// 			chunk, _ := index.NewChunk(filePath)
+// 			// fmt.Println(chunk)
+// 			fmt.Printf("%d %t\r", i, chunk.IsMember2(addr))
 // 		} else {
-// 			bloom, _ := index.NewChunk(bloomPath)
-// 			fmt.Printf("%d %t\n", i, bloom.Bloom.IsMember(addr))
+// 			chunk, _ := index.NewChunk(filePath)
+// 			err := index.ReadBloom(&chunk.Bloom, filePath)
+// 			if err != nil {
+// 				fmt.Println("Error", filePath, err)
+// 				return
+// 			}
+// 			fmt.Printf("%d %t\r", i, chunk.Bloom.IsMember(addr))
 // 		}
 // 	}
 // 	fmt.Println()
-// 	return
-// }
-
-// //----------------------------------------------------------------------
-// func IsMember2(bloom index.ChunkBloom, addr common.Address) bool {
-// 	for _, bb := range bloom.Blooms {
-// 		if IsMember(bb, addr) {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
-
-// //----------------------------------------------------------------------
-// func IsMember(bb index.BloomBytes, addr common.Address) bool {
-// 	whichBits := index.WhichBits(addr)
-// 	for _, bit := range whichBits {
-// 		if !index.IsBitLit(bit, bb.Bytes) {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
-
-// func NewChunk2(path string) (chunk index.Chunk, err error) {
-// 	bloomPath := index.ToBloomPath(path)
-// 	chunk.Range, err = cache.RangeFromFilename(bloomPath)
-// 	if err != nil {
-// 		return
-// 	}
-
-// 	err = chunk.Bloom.Read(bloomPath)
-// 	if err != nil {
-// 		return
-// 	}
-
-// 	indexPath := index.ToIndexPath(path)
-// 	chunk.Data, err = index.NewChunkData(indexPath)
 // 	return
 // }
