045107681c1d714d68e82047201cfcd67e4eb948
tjayrush <jrush@quickblocks.io>
2022-04-10 22:04:52 -0400

    Moves EstablishIndexChunk into the index package

--- src/apps/chifra/pkg/index/establish.go
+++ src/apps/chifra/pkg/index/establish.go
@@ -1,4 +1,4 @@
-package listPkg
+package index
 
 // Copyright 2021 The TrueBlocks Authors. All rights reserved.
 // Use of this source code is governed by a license that can
@@ -6,66 +6,60 @@ package listPkg
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/cache"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/colors"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/file"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/index"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/pinlib/manifest"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/progress"
 )
 
-// TODO: BOGUS - This could use some integration tests
-// establishIndexChunk accpets cache.FileRange, converts it to file name and finds
-// correspoding CID (hash) entry in the manifest. Next, it downloads the bloom filter.
-func establishIndexChunk(chain string, indexFilename string) (bool, error) {
-	if file.FileExists(indexFilename) {
-		return true, nil
-	}
+// EstablishIndexChunk a filename to an index portion, finds the correspoding CID (hash)
+// entry in the manifest, and downloads the index chunk to the local drive
+func EstablishIndexChunk(chain string, fileRange cache.FileRange) (bool, error) {
+	exists, fileName := fileRange.RangeToFilename(chain, cache.Index_Final)
 
-	// Open manifest
 	localManifest, err := manifest.FromLocalFile(chain)
 	if err != nil {
-		return false, err
+		return exists, err
 	}
 
-	fileRange, _ := cache.RangeFromFilename(indexFilename)
-
 	// Find bloom filter's CID
+	// TODO(dawid): This can be a binary search since the pin list is always sorted
 	var matchedPin manifest.PinDescriptor
 	for _, pin := range localManifest.NewPins {
-		if pin.FileName == cache.FilenameFromRange(fileRange, "") {
+		if strings.Contains(fileName, pin.FileName) {
 			matchedPin = pin
 			break
 		}
 	}
 	if matchedPin.FileName == "" {
-		return false, fmt.Errorf("filename not found in pins: %s", indexFilename)
+		return exists, fmt.Errorf("filename not found in pins: %s", fileRange)
 	}
 
 	logger.Log(logger.Info, "Downloading", colors.Blue, fileRange, colors.Off, "from IPFS.")
 
 	// Start downloading the filter
 	pins := []manifest.PinDescriptor{matchedPin}
-	chunkPath := cache.NewCachePath(chain, cache.Index_Final)
 	progressChannel := make(chan *progress.Progress)
 
 	go func() {
-		index.GetChunksFromRemote(chain, pins, &chunkPath, progressChannel)
+		chunkPath := cache.NewCachePath(chain, cache.Index_Final)
+		GetChunksFromRemote(chain, pins, &chunkPath, progressChannel)
 		close(progressChannel)
 	}()
 
 	for event := range progressChannel {
-		exists := file.FileExists(indexFilename)
 		switch event.Event {
 		case progress.AllDone:
-			return exists, nil
+			return file.FileExists(fileName), nil
 		case progress.Cancelled:
-			return exists, nil
+			return file.FileExists(fileName), nil
 		case progress.Error:
-			return exists, fmt.Errorf("error while downloading: %s", event.Message)
+			return file.FileExists(fileName), fmt.Errorf("error while downloading: %s", event.Message)
 		}
 	}
-	return file.FileExists(indexFilename), nil
+	return file.FileExists(fileName), nil
 }
---
+++ src/apps/chifra/pkg/index/establish_integration_test.go
@@ -0,0 +1,56 @@
+//go:build integration
+// +build integration
+
+package index
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/cache"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/file"
+)
+
+func Test_EstablishValidFilename(t *testing.T) {
+	// Such a range should not be listed in manifest, so the function will fail and we can investigate
+	r := cache.FileRange{First: 0, Last: 1}
+	_, err := EstablishIndexChunk("mainnet", r)
+	if err == nil {
+		t.Fatal("error expected")
+	}
+}
+
+func IndexPathFromRange(fileRange cache.FileRange) (string, error) {
+	tmp := cache.NewCachePath("mainnet", cache.Index_Final)
+	path := tmp.GetFullPath(cache.FilenameFromRange(fileRange, ""))
+	if file.FileExists(path) {
+		return path, nil
+	}
+	return path, errors.New("file not found for " + path)
+}
+
+func Test_EstablishChunk(t *testing.T) {
+
+	fileRange := cache.FileRange{First: 1, Last: 590501}
+	path, err := IndexPathFromRange(fileRange)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !file.Remove(path) {
+		t.Error("could not remove file " + path)
+	}
+
+	if file.FileExists(path) {
+		t.Error("remove file did not work for " + path)
+	}
+
+	_, err = EstablishIndexChunk("mainnet", fileRange)
+	if err != nil {
+		t.Error("EstablishIndexChunk returns error: " + err.Error())
+	}
+
+	if !file.FileExists(path) {
+		t.Error("EstablishIndexChunk did not work for " + path)
+	}
+}
