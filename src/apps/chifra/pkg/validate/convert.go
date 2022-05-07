03745929073e9f98f6453005a7007ec0665ee9b0
tjayrush <jrush@quickblocks.io>
2022-05-07 19:34:16 -0400

    Moves Convert to validate package

---
+++ src/apps/chifra/pkg/validate/convert.go
@@ -0,0 +1,17 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+
+package validate
+
+import "strconv"
+
+func Convert(blockStrs []string) []uint64 {
+	// TODO: This needs to handle string values such as specials and latest/first, etc.
+	var ret []uint64
+	for _, blockStr := range blockStrs {
+		val, _ := strconv.ParseUint(blockStr, 10, 64)
+		ret = append(ret, val)
+	}
+	return ret
+}
---
+++ src/apps/chifra/pkg/validate/convert_test.go
@@ -0,0 +1,20 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+
+package validate
+
+import (
+	"testing"
+)
+
+func TestConvert(t *testing.T) {
+	blocks := []string{"1", "1000"}
+	conv := Convert(blocks)
+	expected := []uint64{1, 1000}
+	for i := 0; i < len(blocks); i++ {
+		if conv[i] != expected[i] {
+			t.Error("Not converted correctly", conv[i], "!=", expected[i])
+		}
+	}
+}
