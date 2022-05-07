ea924483a9d8d3caba791321efd8d93b54927a19
tjayrush <jrush@quickblocks.io>
2022-05-07 18:19:34 -0400

    Difficulty calc

--- src/other/difficulty/difficulty.cpp
+++ src/other/difficulty/difficulty.cpp
@@ -12,8 +12,8 @@
  *-------------------------------------------------------------------------------------------*/
 #include "etherlib.h"
 
-#define START 14679530
-#define END 14704158
+#define START 14704158
+#define END 14719884
 
 int main(int argc, const char* argv[]) {
     loadEnvironmentPaths();
