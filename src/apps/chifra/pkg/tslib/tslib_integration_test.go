7052e19b8d1940fd851fc3302edf3df085973e64
tjayrush <jrush@quickblocks.io>
2022-05-15 17:32:32 -0400

    Adds testing for block ranges

--- src/apps/chifra/pkg/tslib/tslib_integration_test.go
+++ src/apps/chifra/pkg/tslib/tslib_integration_test.go
@@ -1,3 +1,6 @@
+//go:build integration
+// +build integration
+
 // Copyright 2021 The TrueBlocks Authors. All rights reserved.
 // Use of this source code is governed by a license that can
 // be found in the LICENSE file.
