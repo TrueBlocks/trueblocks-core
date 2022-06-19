14e2a7a9d95442a6b7f985b265032722ad7a439d
tjayrush <jrush@quickblocks.io>
2022-06-19 16:47:42 -0400

    Start preparing for unchained index

--- src/apps/chifra/pkg/manifest/download.go
+++ src/apps/chifra/pkg/manifest/download.go
@@ -7,12 +7,37 @@ package manifest
 import (
 	"errors"
 	"net/http"
+	"net/url"
+	"path"
+
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
 )
 
-// DownloadManifest downloads manifest from the given gateway and parses it into
+func DownloadRemoteManifest(chain string) (*Manifest, error) {
+	cid, err := getManifestCidFromContract(chain)
+	if err != nil {
+		return nil, err
+	}
+	logger.Log(logger.Info, "Unchained index returned CID", cid)
+
+	// Download the manifest
+	gatewayUrl := config.GetPinGateway(chain)
+	logger.Log(logger.Info, "IPFS gateway", gatewayUrl)
+
+	url, err := url.Parse(gatewayUrl)
+	if err != nil {
+		return nil, err
+	}
+	url.Path = path.Join(url.Path, cid)
+
+	return downloadManifest(chain, url.String())
+}
+
+// downloadManifest downloads manifest from the given gateway and parses it into
 // Manifest struct. Both JSON and TSV formats are supported, but the server has
 // to set the correct Content-Type header.
-func DownloadManifest(chain, gatewayUrl string) (*Manifest, error) {
+func downloadManifest(chain, gatewayUrl string) (*Manifest, error) {
 	response, err := http.Get(gatewayUrl)
 	if err != nil {
 		return nil, err
