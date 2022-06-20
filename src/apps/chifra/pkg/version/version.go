e41b343388e5094f8aebe3c226300c836937e86b
tjayrush <jrush@quickblocks.io>
2022-06-20 13:20:08 -0400

    Adds Version class for use in migration

--- src/apps/chifra/pkg/version/version.go
+++ src/apps/chifra/pkg/version/version.go
@@ -7,5 +7,55 @@
 
 package version
 
+import (
+	"strconv"
+	"strings"
+)
+
 const LibraryVersion = "GHC-TrueBlocks//0.37.0-beta"
 const ManifestVersion = "trueblocks-core@v0.40.0"
+
+type Version struct {
+	Major  int64  `json:"major"`
+	Minor  int64  `json:"minor"`
+	Build  int64  `json:"build"`
+	Aspect string `json:"aspect"`
+}
+
+func Parse(str string) (vers Version, err error) {
+	str = strings.Replace(strings.Replace(str, "-", ".", -1), "v", "", -1)
+	parts := strings.Split(str, ".")
+	if len(parts) > 0 {
+		if vers.Major, err = strconv.ParseInt(parts[0], 10, 32); err != nil {
+			return vers, err
+		}
+	}
+	if len(parts) > 1 {
+		if vers.Minor, err = strconv.ParseInt(parts[1], 10, 32); err != nil {
+			return vers, err
+		}
+	}
+	if len(parts) > 2 {
+		if vers.Build, err = strconv.ParseInt(parts[2], 10, 32); err != nil {
+			return vers, err
+		}
+	}
+	if len(parts) > 3 {
+		vers.Aspect = parts[3]
+	}
+
+	return vers, nil
+}
+
+func (ref *Version) IsBackLevel(test Version) bool {
+	if test.Major < ref.Major {
+		return true
+	}
+	if test.Minor < ref.Minor {
+		return true
+	}
+	if test.Build < ref.Build {
+		return true
+	}
+	return false
+}
