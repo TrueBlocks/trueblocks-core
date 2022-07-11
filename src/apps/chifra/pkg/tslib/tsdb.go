104c147383233403d8f4a637846c3bec6b027dab
Thomas Jay Rush <jrush@quickblocks.io>
2022-07-10 22:28:17 -0400

    Starting adding syncing protection to some write operations

--- src/apps/chifra/pkg/tslib/tsdb.go
+++ src/apps/chifra/pkg/tslib/tsdb.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"sort"
+	"sync"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/file"
@@ -139,6 +140,8 @@ func FromBn(chain string, bn uint64) (*Timestamp, error) {
 	return &perChainTimestamps[chain].memory[bn], nil
 }
 
+var writeMutex sync.Mutex
+
 func Reset(chain string, maxBn uint64) error {
 	cnt, err := NTimestamps(chain)
 	if err != nil {
@@ -157,8 +160,12 @@ func Reset(chain string, maxBn uint64) error {
 
 	truncated := perChainTimestamps[chain].memory[0:maxBn]
 
-	truncPath := config.GetPathToCache(chain) + "tmp/truncated.bin"
-	f, err := os.OpenFile(truncPath, os.O_WRONLY|os.O_CREATE, 0644)
+	// TODO: BOGUS - PROTECT WRITING AGAINST CONTROL+C AND DOUBLE ENTRY
+	// writeMutex.Lock()
+	// trapCh := sigintTrap.Enable(context.WithCancel(context.Background()))
+
+	tempPath := config.GetPathToCache(chain) + "tmp/truncated.bin"
+	fp, err := os.OpenFile(tempPath, os.O_WRONLY|os.O_CREATE, 0644)
 	defer func() {
 		// Clear the cache in even in case of failure, causes a reload at worst
 		perChainTimestamps[chain] = TimestampDatabase{
@@ -166,30 +173,31 @@ func Reset(chain string, maxBn uint64) error {
 			count:  0,
 			memory: nil,
 		}
-		if file.FileExists(truncPath) {
-			os.Remove(truncPath)
-		}
+		os.Remove(tempPath)
+		// sigintTrap.Disable(trapCh)
+		// writeMutex.Unlock()
 	}()
 	if err != nil {
 		return err
 	}
 
-	f.Seek(0, io.SeekStart)
-	err = binary.Write(f, binary.LittleEndian, truncated)
+	fp.Seek(0, io.SeekStart)
+	err = binary.Write(fp, binary.LittleEndian, truncated)
 	if err != nil {
-		f.Close()
-		os.Remove(truncPath)
+		fp.Close()
 		return err
 	}
 
-	f.Close()
+	// Don't defer this because we want it to be closed before we copy it
+	fp.Close()
+
 	// TODO: BOGUS - THIS IS NOT PROTECTIVE OF THE EXISTING FILE
 	// TODO: BOGUS - IT SHOULD BE UN-INTERUPTABLE
 	// TODO: BOGUS - IT MAY WANT TO MAKE A BACKUP AND RECOVER IF THE COPY OR REMOVAL FAILS
 	// TODO: BOGUS - IT SHOULD BE GENERALIZED INSIDE OF COPYFILE
 	tsPath := config.GetPathToIndex(chain) + "ts.bin"
 	os.Remove(tsPath)
-	_, err = file.Copy(truncPath, tsPath)
+	_, err = file.Copy(tempPath, tsPath)
 	if err != nil {
 		return nil
 	}
