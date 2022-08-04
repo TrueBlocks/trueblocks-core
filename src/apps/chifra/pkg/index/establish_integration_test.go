2edc45b2b5c6cb8633cd1b6183ccfbd2736a8349
tjayrush <jrush@quickblocks.io>
2022-08-03 22:39:25 -0400

    Fixes issue mentioned in #2211

--- src/apps/chifra/pkg/index/establish_integration_test.go
+++ src/apps/chifra/pkg/index/establish_integration_test.go
@@ -3,55 +3,47 @@
 
 package index
 
-import (
-	"errors"
-	"testing"
+// func Test_EstablishValidFilename(t *testing.T) {
+// 	// Such a range should not be listed in manifest, so the function will fail and we can investigate
+// 	r := cache.FileRange{First: 0, Last: 1}
+// 	_, err := EstablishIndexChunk("mainnet", r)
+// 	if err == nil {
+// 		t.Fatal("error expected")
+// 	}
+// }
 
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/cache"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/file"
-)
+// func IndexPathFromRange(fileRange cache.FileRange) (string, error) {
+// 	tmp := cache.NewCachePath("mainnet", cache.Index_Final)
+// 	path := tmp.GetFullPath(cache.FilenameFromRange(fileRange, ""))
+// 	if file.FileExists(path) {
+// 		return path, nil
+// 	}
+// 	return path, errors.New("file not found for " + path)
+// }
 
-func Test_EstablishValidFilename(t *testing.T) {
-	// Such a range should not be listed in manifest, so the function will fail and we can investigate
-	r := cache.FileRange{First: 0, Last: 1}
-	_, err := EstablishIndexChunk("mainnet", r)
-	if err == nil {
-		t.Fatal("error expected")
-	}
-}
+// func Test_EstablishChunk(t *testing.T) {
 
-func IndexPathFromRange(fileRange cache.FileRange) (string, error) {
-	tmp := cache.NewCachePath("mainnet", cache.Index_Final)
-	path := tmp.GetFullPath(cache.FilenameFromRange(fileRange, ""))
-	if file.FileExists(path) {
-		return path, nil
-	}
-	return path, errors.New("file not found for " + path)
-}
+// 	// TODO: This kills real data. Do we really want to do that?
+// 	fileRange := cache.FileRange{First: 1, Last: 590501}
+// 	path, err := IndexPathFromRange(fileRange)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
 
-func Test_EstablishChunk(t *testing.T) {
+// 	if !file.Remove(path) {
+// 		t.Error("could not remove file " + path)
+// 	}
 
-	// TODO: This kills real data. Do we really want to do that?
-	fileRange := cache.FileRange{First: 1, Last: 590501}
-	path, err := IndexPathFromRange(fileRange)
-	if err != nil {
-		t.Error(err)
-	}
+// 	if file.FileExists(path) {
+// 		t.Error("remove file did not work for " + path)
+// 	}
 
-	if !file.Remove(path) {
-		t.Error("could not remove file " + path)
-	}
+// 	_, err = EstablishIndexChunk("mainnet", fileRange)
+// 	if err != nil {
+// 		t.Error("EstablishIndexChunk returns error: " + err.Error())
+// 	}
 
-	if file.FileExists(path) {
-		t.Error("remove file did not work for " + path)
-	}
-
-	_, err = EstablishIndexChunk("mainnet", fileRange)
-	if err != nil {
-		t.Error("EstablishIndexChunk returns error: " + err.Error())
-	}
-
-	if !file.FileExists(path) {
-		t.Error("EstablishIndexChunk did not work for " + path)
-	}
-}
+// 	if !file.FileExists(path) {
+// 		t.Error("EstablishIndexChunk did not work for " + path)
+// 	}
+// }
