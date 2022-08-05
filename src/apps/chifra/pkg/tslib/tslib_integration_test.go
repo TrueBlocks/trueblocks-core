ba1fbef83fca2522684068d4764917596e8c646c
tjayrush <jrush@quickblocks.io>
2022-08-05 11:50:57 -0400

    Updating testing

--- src/apps/chifra/pkg/tslib/tslib_integration_test.go
+++ src/apps/chifra/pkg/tslib/tslib_integration_test.go
@@ -1,6 +1,3 @@
-//go:build integration
-// +build integration
-
 // Copyright 2021 The TrueBlocks Authors. All rights reserved.
 // Use of this source code is governed by a license that can
 // be found in the LICENSE file.
