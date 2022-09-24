bd794033506ef454f83f1e19994453780a7a2af1
tjayrush <jrush@quickblocks.io>
2022-09-24 15:42:16 -0400

    Changed error message to agree with GoLang idioms

--- src/apps/chifra/internal/receipts/output.go
+++ src/apps/chifra/internal/receipts/output.go
@@ -55,7 +55,7 @@ func (opts *ReceiptsOptions) ReceiptsInternal() (err error, handled bool) {
 		return nil, false
 	}
 
-	if len(os.Getenv("OLD")) > 0 {
+	if true || len(os.Getenv("OLD")) > 0 {
 		err = opts.Globals.PassItOn("getReceipts", opts.Globals.Chain, opts.toCmdLine(), opts.getEnvStr())
 		return err, true
 	}
