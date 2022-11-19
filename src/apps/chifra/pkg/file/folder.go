d439ee058ed375b303b7ab9f211908074e1271a0
tjayrush <jrush@quickblocks.io>
2022-11-19 12:41:01 -0500

    Adds IsFolderEmpty which will be used by Decache

---
+++ src/apps/chifra/pkg/file/folder.go
@@ -0,0 +1,23 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+
+package file
+
+import (
+	"io"
+	"os"
+)
+
+func IsFolderEmpty(folder string) (bool, error) {
+	f, err := os.Open(folder)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+	_, err = f.Readdirnames(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
+}
