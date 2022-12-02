29afdf32a029c1fbc810edb062bfc88d20781c22
tjayrush <jrush@quickblocks.io>
2022-12-02 17:16:35 -0500

    Adds --source option to chifra transactions but does not implement it.

--- src/apps/chifra/internal/transactions/validate.go
+++ src/apps/chifra/internal/transactions/validate.go
@@ -16,6 +16,10 @@ func (opts *TransactionsOptions) validateTransactions() error {
 		return opts.BadFlag
 	}
 
+	if opts.Source {
+		return validate.Usage("The --source flag is currently disabled.")
+	}
+
 	if len(opts.Flow) > 0 {
 		if !opts.Uniq {
 			return validate.Usage("The {0} option is only available with the {1} option", "--flow", "--uniq")
