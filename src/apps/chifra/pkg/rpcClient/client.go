94f6ab6c2a2dfcb45cf5578069bc6087f815a6fd
tjayrush <jrush@quickblocks.io>
2022-12-05 15:28:36 -0500

    Moves GetBlockByNumber to its own file

--- src/apps/chifra/pkg/rpcClient/client.go
+++ src/apps/chifra/pkg/rpcClient/client.go
@@ -430,45 +430,6 @@ func DecodeHex(hex string) []byte {
 	return hexutil.MustDecode(hex)
 }
 
-func GetBlockByNumber(chain string, bn uint64, withTxs bool) (types.SimpleBlock, error) {
-	var block BlockHeader
-	var payload = RPCPayload{
-		Method:    "eth_getBlockByNumber",
-		RPCParams: RPCParams{fmt.Sprintf("0x%x", bn), withTxs},
-	}
-	rpcProvider := config.GetRpcProvider(chain)
-	err := FromRpc(rpcProvider, &payload, &block)
-	if err != nil {
-		return types.SimpleBlock{}, err
-	}
-	if len(block.Result.Number) == 0 || len(block.Result.Timestamp) == 0 {
-		msg := fmt.Sprintf("block number or timestamp for %d not found", bn)
-		return types.SimpleBlock{}, fmt.Errorf(msg)
-	}
-	n, _ := strconv.ParseUint(block.Result.Number[2:], 16, 64)
-	ts, _ := strconv.ParseUint(block.Result.Timestamp[2:], 16, 64)
-	gl, _ := strconv.ParseUint(block.Result.GasLimit[2:], 16, 64)
-	gu, _ := strconv.ParseUint(block.Result.GasUsed[2:], 16, 64)
-	d, _ := strconv.ParseUint(block.Result.Difficulty[2:], 16, 64)
-	if n == 0 {
-		ts, err = GetBlockZeroTs(chain)
-		if err != nil {
-			return types.SimpleBlock{}, err
-		}
-	}
-	return types.SimpleBlock{
-		BlockNumber: n,
-		Timestamp:   int64(ts),
-		Hash:        common.HexToHash(block.Result.Hash),
-		ParentHash:  common.HexToHash(block.Result.ParentHash),
-		GasLimit:    gl,
-		GasUsed:     gu,
-		Miner:       common.HexToAddress(block.Result.Miner),
-		Difficulty:  d,
-		// Transactions: block.Result.Transactons,
-	}, nil
-}
-
 // GetBlockZeroTs for some reason block zero does not return a timestamp, so we assign block one's ts minus 14 seconds
 func GetBlockZeroTs(chain string) (uint64, error) {
 	ts := GetBlockTimestamp(config.GetRpcProvider(chain), 0)
