828e071560a896bfbb137be5f262a96b0aaae2b1
tjayrush <jrush@quickblocks.io>
2022-12-13 10:18:59 -0500

    Renames a few files

--- src/dev_tools/makeClass/handle_apifiles.cpp
+++ src/dev_tools/makeClass/handle_apifiles.cpp
@@ -70,7 +70,7 @@ bool COptions::writeOpenApiFile(void) {
     goConvertStream << "\t}" << endl;
 
     writeCodeOut(this, getPathToSource("apps/chifra/internal/daemon/routes.go"));
-    writeCodeOut(this, getPathToSource("apps/chifra/internal/daemon/caller.go"));
+    writeCodeOut(this, getPathToSource("apps/chifra/internal/daemon/handle_calls.go"));
 
     if (getEnvStr("GENERATE_YAML") != "false") {
         writeCodeOut(this, getDocsPathContent("api/openapi.yaml"));
