4d5a9b1dac820c9d1385ce9cc12dc6e649eb7218
Dawid Szlachta <git@makingprogress.dev>
2022-12-13 08:23:08 +0100

    Four-bytes cross-product database generator

---
+++ src/other/four_bytes/README.md
@@ -0,0 +1,59 @@
+# Four-bytes database generator
+
+```bash
+make
+./bin/four_bytes --outdir /where/to/put/chunks
+```
+
+It generates the cross-product database of four-bytes from the data already present in TrueBlocks.
+The input files are installed with `chifra`:
+- `abis/known-000/uniq_sigs.tab`
+- `abis/known-000/uniq_funcs.tab`
+- `abis/known-000/uniq_events.tab`
+
+The produced database is chunked and consists of 65,536 files, with sizes ranging from 100 KB to 176 KB.
+The database takes about 7 GB of disk space in total. Generation can take up to 40 minutes.
+
+## Chunk format
+
+Chunk format tries to follow Unchained Index file format as close as possible. Each chunk consist of 3 parts:
+```
+[      HEADER      ]
+[ Signature Table  ]
+[   String Table   ]
+```
+
+### Header
+
+```go
+type Header struct {
+	Magic          uint32
+	Hash           common.Hash
+	SignatureCount uint32
+}
+```
+
+### Signature Table
+
+is array of
+```go
+type SignatureRecord struct {
+	Signature common.Hash
+	// Where the string representation starts in the file?
+	Offset    uint32
+	// How long is the string representation?
+	Len       uint32
+}
+```
+
+### String Table
+
+is strings packed next to each other, e.g.:
+`islets()tixFounders()reclaimToken(address)partisans(uint256)setPurchaseValues(uint128)`
+
+## Further improvements
+
+1. Jay:
+    > This might speed things up very significantly and not lose very much data. A while ago, I looked at about 850,000 functions (I forget where I got them). I extracted the type signatures. There were about 45,000 uniq type signatures, but the attached 500 accounted for 99% of the entries.
+
+2. Cross-product database has many entries that has been never seen on chain. Maybe we could add frequency information to give priority to the *real* signatures
---
+++ src/other/four_bytes/pkg/fourBytes/chunk_file_test.go
@@ -0,0 +1,211 @@
+package fourbytes
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"strings"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func Test_write_one(t *testing.T) {
+	buf := &bytes.Buffer{}
+	w := bufio.NewWriter(buf)
+	testSignature := common.HexToHash("04c786e6d139b5bcd86f44bbd1ca9466ac277f3e25f8bc62ba39d8b4e5c13d19")
+	strSignature := "absolute()"
+	strLen := len(strSignature)
+	sigRecords := []SignatureRecord{
+		{
+			Signature: testSignature,
+			Offset:    0,
+			Len:       uint32(strLen),
+		},
+	}
+	strRecords := []StringRecord{
+		{
+			Value: strSignature,
+			Len:   strLen,
+		},
+	}
+	if err := write(w, sigRecords, strRecords); err != nil {
+		t.Fatal(err)
+	}
+	if err := w.Flush(); err != nil {
+		t.Fatal(err)
+	}
+
+	var h Header
+	binary.Read(buf, binary.LittleEndian, &h)
+	if h.Magic != 0xdeadbeef {
+		t.Fatalf("wrong magic '%x'", h.Magic)
+	}
+	if h.Hash != common.HexToHash("0x"+strings.Repeat("5", 32)) {
+		t.Fatalf("wrong hash '%x'", h.Hash)
+	}
+	if h.SignatureCount != 1 {
+		t.Fatalf("wrong magic %d", h.SignatureCount)
+	}
+
+	var sig SignatureRecord
+	binary.Read(buf, binary.LittleEndian, &sig)
+	if sig.Signature != testSignature {
+		t.Fatalf("wrong signature '%x'", sig.Signature)
+	}
+	if sig.Offset != 0 {
+		t.Fatal("wrong offset", sig.Offset)
+	}
+	if sig.Len != uint32(strLen) {
+		t.Fatal("wrong len", sig.Len)
+	}
+
+	b := make([]byte, sig.Len)
+	binary.Read(buf, binary.LittleEndian, &b)
+	str := string(b)
+	if str != strSignature {
+		t.Fatalf("wrong string signature '%s'", str)
+	}
+}
+
+func Test_write_many(t *testing.T) {
+	buf := &bytes.Buffer{}
+	w := bufio.NewWriter(buf)
+	testSignatures := []common.Hash{
+		common.HexToHash("04c786e6d139b5bcd86f44bbd1ca9466ac277f3e25f8bc62ba39d8b4e5c13d19"),
+		common.HexToHash("3884d63599bd4ea4f3deb3e2108ff8ea7043b963874fb6f80e98f2a61d8b9139"),
+	}
+	strSignatures := []string{
+		"absolute()",
+		"airdrop()",
+	}
+	strLens := []int{
+		len(strSignatures[0]),
+		len(strSignatures[1]),
+	}
+	sigRecords := []SignatureRecord{
+		{
+			Signature: testSignatures[0],
+			Offset:    0,
+			Len:       uint32(strLens[0]),
+		},
+		{
+			Signature: testSignatures[1],
+			Offset:    uint32(strLens[0]),
+			Len:       uint32(strLens[1]),
+		},
+	}
+	strRecords := []StringRecord{
+		{
+			Value: strSignatures[0],
+			Len:   strLens[0],
+		},
+		{
+			Value: strSignatures[1],
+			Len:   strLens[1],
+		},
+	}
+	if err := write(w, sigRecords, strRecords); err != nil {
+		t.Fatal(err)
+	}
+	if err := w.Flush(); err != nil {
+		t.Fatal(err)
+	}
+
+	var h Header
+	binary.Read(buf, binary.LittleEndian, &h)
+	if h.Magic != 0xdeadbeef {
+		t.Fatalf("wrong magic '%x'", h.Magic)
+	}
+	if h.Hash != common.HexToHash("0x"+strings.Repeat("5", 32)) {
+		t.Fatalf("wrong hash '%x'", h.Hash)
+	}
+	if h.SignatureCount != 2 {
+		t.Fatalf("wrong magic %d", h.SignatureCount)
+	}
+
+	sigs := make([]SignatureRecord, len(testSignatures))
+	binary.Read(buf, binary.LittleEndian, &sigs)
+	for i, sig := range sigRecords {
+		if sig.Signature != sigs[i].Signature {
+			t.Fatalf("wrong signature '%x'", sigs[i].Signature)
+		}
+		if sig.Offset != sigs[i].Offset {
+			t.Fatal("wrong offset", sigs[i].Signature)
+		}
+		if sig.Len != sigs[i].Len {
+			t.Fatal("wrong len", sigs[i].Len)
+		}
+	}
+
+	for i, sig := range sigs {
+		b := make([]byte, sig.Len)
+		binary.Read(buf, binary.LittleEndian, &b)
+		str := string(b)
+		if str != strSignatures[i] {
+			t.Fatalf("wrong string signature '%s'", str)
+		}
+	}
+}
+
+func Test_readAll(t *testing.T) {
+	buf := &bytes.Buffer{}
+	w := bufio.NewWriter(buf)
+	testSignatures := []common.Hash{
+		common.HexToHash("04c786e6d139b5bcd86f44bbd1ca9466ac277f3e25f8bc62ba39d8b4e5c13d19"),
+		common.HexToHash("3884d63599bd4ea4f3deb3e2108ff8ea7043b963874fb6f80e98f2a61d8b9139"),
+	}
+	strSignatures := []string{
+		"absolute()",
+		"airdrop()",
+	}
+	strLens := []int{
+		len(strSignatures[0]),
+		len(strSignatures[1]),
+	}
+	sigRecords := []SignatureRecord{
+		{
+			Signature: testSignatures[0],
+			Offset:    0,
+			Len:       uint32(strLens[0]),
+		},
+		{
+			Signature: testSignatures[1],
+			Offset:    uint32(strLens[0]),
+			Len:       uint32(strLens[1]),
+		},
+	}
+	strRecords := []StringRecord{
+		{
+			Value: strSignatures[0],
+			Len:   strLens[0],
+		},
+		{
+			Value: strSignatures[1],
+			Len:   strLens[1],
+		},
+	}
+	if err := write(w, sigRecords, strRecords); err != nil {
+		t.Fatal(err)
+	}
+	if err := w.Flush(); err != nil {
+		t.Fatal(err)
+	}
+
+	readSigs, readStrs, err := readAll(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i, r := range sigRecords {
+		if r != readSigs[i] {
+			t.Fatal("signature records not equal", readSigs)
+		}
+	}
+
+	for i, r := range strRecords {
+		if r.Value != readStrs[i].Value {
+			t.Fatal("string records not equal", r, readStrs[i])
+		}
+	}
+}
