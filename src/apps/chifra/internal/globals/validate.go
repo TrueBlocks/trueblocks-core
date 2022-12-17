a37fa476b4f04504a7ed06b52a03ec0649c489eb
tjayrush <jrush@quickblocks.io>
2022-12-16 20:35:59 -0500

    Update

--- src/apps/chifra/internal/globals/validate.go
+++ src/apps/chifra/internal/globals/validate.go
@@ -41,6 +41,7 @@ func (opts *GlobalOptions) Validate() error {
 		opts.ToFile = false
 	}
 
+	// TODO: Can we re-enable this? It doesn't work in Sepolia under docker. Returns a really weird message.
 	// tmpPath := filepath.Join(config.GetPathToCache(opts.Chain), "tmp", "checkProvider.txt")
 	// if !file.FileExists(tmpPath) {
 	// 	if version, err := rpcClient.GetVersion(opts.Chain); err != nil {
