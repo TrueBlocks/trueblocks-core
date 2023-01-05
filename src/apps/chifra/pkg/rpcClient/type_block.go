920541ad14a8d719c1a5ffe5d8dcf6d78f399636
Dawid Szlachta <git@makingprogress.dev>
2023-01-05 21:50:57 +0100

    Test fixes

--- src/apps/chifra/pkg/rpcClient/type_block.go
+++ src/apps/chifra/pkg/rpcClient/type_block.go
@@ -6,12 +6,41 @@ import (
 	"math/big"
 	"strconv"
 
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/cache"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
+
+// TODO: move this to ABI package when we port ABIs
+var tokenRelated = map[string]bool{
+	// functions
+	"0x095ea7b3": true, // approve(address spender, uint256 value)
+	"0xa9059cbb": true, // transfer(address from, uint256 to);
+	"0x23b872dd": true, // transferFrom(address from, address to, uint256 value)
+	"0xb3e1c718": true, // _safeMint(address, uint256)
+	"0x6a4f832b": true, // _safeMint(address, uint256, bytes)
+	"0xa1448194": true, // safeMint(address, uint256)
+	"0x8832e6e3": true, // safeMint(address, uint256, bytes)
+	"0x4e6ec247": true, // _mint(address, uint256)
+	"0x4cd4edcb": true, // _mint(address, uint256, bytes, bytes)
+	"0x40c10f19": true, // mint(address, uint256)
+	"0xcfa84fc1": true, // mint(uint256, address[], uint256[])
+	"0x278d9c41": true, // mintEventToManyUsers(uint256, address[])
+	"0x78b27221": true, // mintFungible(uint256, address[], uint256[])
+	"0xf9419088": true, // mintNonFungible(uint256, address[])
+	"0xf190ac5f": true, // mintToAddresses(address[], uint256)
+	"0xa140ae23": true, // mintToken(uint256, address)
+	"0xf980f3dc": true, // mintUserToManyEvents(uint256[], address)
+	"0x14004ef3": true, // multimint(address[], uint256[])
+	"0x6a627842": true, // mint(address)
+	"0xa0712d68": true, // mint(uint256)
+	// topics
+	"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef": true, // Transfer(address from, address to, uint256 value)
+	"0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925": true, // Approval(address owner, address spender, uint256 value)
+	"0xd4735d920b0f87494915f556dd9b54c8f309026070caea5c737245152564d266": true, // Transfer(bytes32 node, address owner)
+	"0x30385c845b448a36257a6a1716e6ad2e1bc2cbe333cde1e69fe849ad6511adfe": true, // Minted(address,uint256)
+}
 
 func GetBlockHeaderByNumber(chain string, bn uint64) (types.SimpleBlockHeader, error) {
 	rawBlock, err := getRawBlock(chain, bn, false)
@@ -71,6 +100,11 @@ func loadBlock[Tx types.BlockTransaction](chain string, bn uint64, isFinal bool,
 		return
 	}
 
+	uncles := make([]common.Hash, 0, len(rawBlock.Uncles))
+	for _, uncle := range rawBlock.Uncles {
+		uncles = append(uncles, common.HexToHash(uncle))
+	}
+
 	block = types.SimpleBlock[Tx]{
 		BlockNumber: blockNumber,
 		Timestamp:   int64(timestamp),
@@ -81,6 +115,7 @@ func loadBlock[Tx types.BlockTransaction](chain string, bn uint64, isFinal bool,
 		Miner:       common.HexToAddress(rawBlock.Miner),
 		Difficulty:  difficulty,
 		Finalized:   isFinal,
+		Uncles:      uncles,
 	}
 	return
 }
@@ -88,11 +123,13 @@ func loadBlock[Tx types.BlockTransaction](chain string, bn uint64, isFinal bool,
 // GetBlockByNumberWithTxs fetches the block with transactions from the RPC.
 func GetBlockByNumberWithTxs(chain string, bn uint64, isFinal bool) (block types.SimpleBlock[types.SimpleTransaction], err error) {
 	// load from cache if possible
-	cached, _ := cache.GetBlock(chain, bn)
-	if cached != nil {
-		block = *cached
-		return
-	}
+	// FIXME: without updating the block in cache (writing) some value are not
+	// FIXME: filled in and the tests fail
+	// cached, _ := cache.GetBlock(chain, bn)
+	// if cached != nil {
+	// 	block = *cached
+	// 	return
+	// }
 
 	mustParseUint := func(input any) (result uint64) {
 		result, _ = strconv.ParseUint(fmt.Sprint(input), 0, 64)
@@ -118,8 +155,14 @@ func GetBlockByNumberWithTxs(chain string, bn uint64, isFinal bool) (block types
 
 		txHash := common.HexToHash(fmt.Sprint(t["hash"]))
 		txGasPrice := mustParseUint(t["gasPrice"])
+		input := fmt.Sprint(t["input"])
 		value := big.NewInt(0)
 		value.SetString(fmt.Sprint(t["value"]), 0)
+
+		var hasToken bool
+		if len(input) >= 10 {
+			hasToken = tokenRelated[input[0:9]]
+		}
 
 		// Get the receipt
 		var receipt types.SimpleReceipt
@@ -145,9 +188,9 @@ func GetBlockByNumberWithTxs(chain string, bn uint64, isFinal bool) (block types
 			GasUsed:              receipt.GasUsed,
 			MaxFeePerGas:         mustParseUint(t["maxFeePerGas"]),
 			MaxPriorityFeePerGas: mustParseUint(t["maxPriorityFeePerGas"]),
-			Input:                fmt.Sprint(t["input"]),
-			// IsError: bool(t["isError"]),
-			// HasToken             bool            `json:"hasToken"`
+			Input:                input,
+			IsError:              receipt.IsError,
+			HasToken:             hasToken,
 			// Cachebits            uint8           `json:"cachebits"`
 			// Reserved2            uint8           `json:"reserved2"`
 			Receipt: &receipt,
