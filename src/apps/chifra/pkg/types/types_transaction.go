83c56a8841f21dedf2d2a96003e740fd09638b46
tjayrush <jrush@quickblocks.io>
2023-01-11 09:27:48 -0500

    Fixes broken tests for transactions

--- src/apps/chifra/pkg/types/types_transaction.go
+++ src/apps/chifra/pkg/types/types_transaction.go
@@ -1,9 +1,13 @@
 package types
 
 import (
+	"math/big"
 	"strings"
 
+	"github.com/bykof/gostradamus"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/params"
 )
 
 type RawTransaction struct {
@@ -49,23 +53,40 @@ func (s *SimpleTransaction) SetRaw(rawTransaction RawTransaction) {
 	s.raw = &rawTransaction
 }
 
+// TODO: This can be made a utility
+func weiToEther(wei *big.Int) *big.Float {
+	return new(big.Float).Quo(new(big.Float).SetInt(wei), big.NewFloat(params.Ether))
+}
+
 func (s *SimpleTransaction) Model(showHidden bool, format string, extraOptions map[string]any) Model {
+	to := hexutil.Encode(s.To.Bytes())
+	if to == "0x0000000000000000000000000000000000000000" {
+		to = "0x0" // weird special case to preserve what RPC does
+	}
+
 	model := map[string]interface{}{
 		"blockNumber":      s.BlockNumber,
 		"transactionIndex": s.TransactionIndex,
 		"timestamp":        s.Timestamp,
 		"from":             s.From,
-		"to":               s.To,
+		"to":               to,
 		"gasUsed":          s.GasUsed,
 		"hash":             s.Hash,
 		"isError":          s.IsError,
+		// TODO: creates a dependacy of being in a block, so may need to be calculated on the fly, but we may
+		// TODO: want to remove `finalized` as described in issue #2667
+		"finalized":        extraOptions["finalized"],
 	}
 
+	// TODO: these date-related values could be done when RPC is queried and cached
+	date := gostradamus.FromUnixTimestamp(int64(s.Timestamp))
 	if format == "json" {
 		model["blockHash"] = s.BlockHash
 		model["nonce"] = s.Nonce
 		model["value"] = s.Value
 		model["gas"] = s.Gas
+		// TODO: this value could be created when RPC is queried and cached
+		model["ether"] = weiToEther(&s.Value)
 		model["gasPrice"] = s.GasPrice
 		model["maxFeePerGas"] = s.MaxPriorityFeePerGas
 		model["maxPriorityFeePerGas"] = s.MaxPriorityFeePerGas
@@ -74,11 +95,16 @@ func (s *SimpleTransaction) Model(showHidden bool, format string, extraOptions m
 		model["receipt"] = s.Receipt
 		model["value"] = s.Value.String()
 		model["receipt"] = nil
+		// TODO: these date-related values could be done when RPC is queried and cached
+		model["date"] = date.Format("2006-01-02 15:04:05") + " UTC"
+		model["datesh"] = date.Format("2006-01-02")
+		model["time"] = date.Format("15:04:05") + " UTC"
 
 		if s.Receipt != nil {
-			contractAddress := s.Receipt.ContractAddress.Hex()
-			if s.Receipt.ContractAddress == common.HexToAddress("0x0") {
-				contractAddress = string(contractAddress[:3])
+			// TODO: this should be a utility (and used above as well). May be available in go-ethereum. We should check.
+			contractAddress := hexutil.Encode(s.Receipt.ContractAddress.Bytes())
+			if contractAddress == "0x0000000000000000000000000000000000000000" {
+				contractAddress = "0x0"
 			}
 			// TODO: this should not be hardcoded here. We have tslib.GetSpecials(), but there
 			// TODO: are 2 issues with it: 1. circular dependency with types package, 2. every
@@ -95,18 +121,16 @@ func (s *SimpleTransaction) Model(showHidden bool, format string, extraOptions m
 				"status":            status,
 			}
 
-			if len(s.Receipt.Logs) > 0 {
-				logs := make([]map[string]any, 0, len(s.Receipt.Logs))
-				for _, log := range s.Receipt.Logs {
-					logs = append(logs, map[string]any{
-						"address":  strings.ToLower(log.Address.Hex()),
-						"logIndex": log.LogIndex,
-						"topics":   log.Topics,
-						"data":     log.Data,
-					})
-				}
-				receiptModel["logs"] = logs
+			logs := make([]map[string]any, 0, len(s.Receipt.Logs))
+			for _, log := range s.Receipt.Logs {
+				logs = append(logs, map[string]any{
+					"address":  strings.ToLower(log.Address.Hex()),
+					"logIndex": log.LogIndex,
+					"topics":   log.Topics,
+					"data":     log.Data,
+				})
 			}
+			receiptModel["logs"] = logs
 			model["receipt"] = receiptModel
 		}
 
@@ -116,8 +140,6 @@ func (s *SimpleTransaction) Model(showHidden bool, format string, extraOptions m
 	}
 
 	// TODO: add missing
-	// "date": s.Date,
-	// "ether": s.Ether,
 	// "ethGasPrice": s.EthGasPrice,
 	// "encoding": s.Encoding,
 	// "compressedTx": s.CompressedTx,
