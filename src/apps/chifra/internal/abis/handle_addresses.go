29025c400179b37585c9aa0c8a18a19042872904
CyMule <nhfranck@gmail.com>
2023-02-19 18:02:03 -0500

    Denest logic for finding abi
    This may be opinionated, but I believe that having less nesting improves
    code readability.

--- src/apps/chifra/internal/abis/handle_addresses.go
+++ src/apps/chifra/internal/abis/handle_addresses.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 	"sort"
+    "fmt"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/abi"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/contract"
@@ -22,28 +23,35 @@ func (opts *AbisOptions) HandleAddresses() (err error) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	fetchData := func(modelChan chan types.Modeler[types.RawFunction], errorChan chan error) {
-		// Note here, that known ABIs are not downloaded. They are only loaded from the local cache.
 		for _, addr := range opts.Addrs {
 			address := types.HexToAddress(addr)
-			if err = abi.LoadAbiFromAddress(opts.Globals.Chain, address, result); err != nil {
-				if !os.IsNotExist(err) {
-					// The error was not due to a missing file...
-					errorChan <- err
-					cancel()
-				}
-				// Let's try to download the file from somewhere
-				if contract, err := contract.IsContractAt(opts.Globals.Chain, address, nil); err != nil {
-					errorChan <- err
-					cancel()
-				} else if !contract {
-					logger.Log(logger.Info, "Address", address, "is not a smart contract. Skipping...")
-					continue
-				} else {
-					// It's okay to not find the ABI. We report an error, but do not stop processing
-					if err = abi.DownloadAbi(opts.Globals.Chain, address, result); err != nil {
-						errorChan <- err
-					}
-				}
+			err := abi.LoadAbiFromAddress(opts.Globals.Chain, address, result)
+			// If there is no error the LoadAbiFromAddress was successful so the ABI is in the result
+			if err == nil {
+				continue
+			}
+
+			// There is an error if the abi wasn't already in cache and we need to download it
+			// The error should be an NotExist error because the LoadAbiFromAddress tries to read the cache file for the address
+			if !os.IsNotExist(err) {
+				// The error was not due to a missing file which means it is some other unexpected error
+				errorChan <- err
+				cancel()
+			}
+
+			// Let's try to download the file from somewhere
+			if contract, err := contract.IsContractAt(opts.Globals.Chain, address, nil); err != nil {
+				errorChan <- err
+				cancel()
+			} else if !contract {
+				logger.Log(logger.Info, "Address", address, "is not a smart contract. Skipping...")
+				continue
+			}
+
+            // The address is a contract so let's try to download the ABI
+			// It's okay to not find the ABI. We report an error, but do not stop processing
+			if err = abi.DownloadAbi(opts.Globals.Chain, address, result); err != nil {
+				errorChan <- err
 			}
 		}
 
