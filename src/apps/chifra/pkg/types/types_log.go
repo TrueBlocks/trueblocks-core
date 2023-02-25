3296f691b11cd75eb8d5b9f207e32d837b9c8b37
tjayrush <jrush@quickblocks.io>
2023-02-25 08:24:59 -0500

    Starts adding logs.txt to autogen

--- src/apps/chifra/pkg/types/types_log.go
+++ src/apps/chifra/pkg/types/types_log.go
@@ -1,12 +1,17 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+/*
+ * Parts of this file were generated with makeClass --run. Edit only those parts of
+ * the code inside of 'EXISTING_CODE' tags.
+ */
+
 package types
 
+// EXISTING_CODE
 import "github.com/ethereum/go-ethereum/common"
 
-// TODO: Enable auto code generation here
-// These types are used to unmarshal responses directly from node. We want
-// to allow users to get node output directly if they want to. In other
-// cases, we want to transform the data so that it is easier to work with
-// in the codebase and makes more sense where printing.
+// EXISTING_CODE
 
 type RawLog struct {
 	Address          string   `json:"address"`
@@ -30,4 +35,59 @@ type SimpleLog struct {
 	Timestamp        uint64          `json:"timestamp,omitempty"`
 	Topics           []common.Hash   `json:"topics,omitempty"`
 	TransactionIndex uint32          `json:"transactionIndex"`
+	raw              *RawLog         `json:"-"`
 }
+
+func (s *SimpleLog) Raw() *RawLog {
+	return s.raw
+}
+
+func (s *SimpleLog) SetRaw(raw *RawLog) {
+	s.raw = raw
+}
+
+func (s *SimpleLog) Model(showHidden bool, format string, extraOptions map[string]any) Model {
+	// EXISTING_CODE
+	// EXISTING_CODE
+
+	model := map[string]interface{}{
+		"address":          s.Address,
+		"articulatedLog":   s.ArticulatedLog,
+		"blockNumber":      s.BlockNumber,
+		"compressedLog":    s.CompressedLog,
+		"data":             s.Data,
+		"logIndex":         s.LogIndex,
+		"timestamp":        s.Timestamp,
+		"transactionIndex": s.TransactionIndex,
+	}
+
+	order := []string{
+		"topic3",
+		"blockHash",
+		"transactionLogIndex",
+		"topic2",
+		"topic1",
+		"topic0",
+		"type",
+		"blockNumber",
+		"transactionIndex",
+		"logIndex",
+		"transactionHash",
+		"timestamp",
+		"address",
+		"data",
+		"articulatedLog",
+		"compressedLog",
+	}
+
+	// EXISTING_CODE
+	// EXISTING_CODE
+
+	return Model{
+		Data:  model,
+		Order: order,
+	}
+}
+
+// EXISTING_CODE
+// EXISTING_CODE
