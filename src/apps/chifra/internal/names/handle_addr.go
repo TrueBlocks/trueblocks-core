28a20cb8cc4a07e09aa789ffa4f8bcb91d887945
tjayrush <jrush@quickblocks.io>
2023-03-15 22:27:09 -0400

    Last time was not the last time.

--- src/apps/chifra/internal/names/handle_addr.go
+++ src/apps/chifra/internal/names/handle_addr.go
@@ -32,11 +32,12 @@ func (opts *NamesOptions) HandleAddr() error {
 		}
 	}
 
+	opts.Globals.NoHeader = true
 	return output.StreamMany(ctx, fetchData, output.OutputOptions{
 		Writer:     opts.Globals.Writer,
 		Chain:      opts.Globals.Chain,
 		TestMode:   opts.Globals.TestMode,
-		NoHeader:   true,
+		NoHeader:   opts.Globals.NoHeader,
 		ShowRaw:    opts.Globals.ShowRaw,
 		Verbose:    opts.Globals.Verbose,
 		LogLevel:   opts.Globals.LogLevel,
