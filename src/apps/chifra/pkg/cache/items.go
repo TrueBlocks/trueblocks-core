649faa2c74056750e0edac750667543a0c7b7240
tjayrush <jrush@quickblocks.io>
2023-03-21 22:03:06 -0400

    Renames CacheItems enumeration (and values) to prepare for combining with paths package

--- src/apps/chifra/pkg/cache/items.go
+++ src/apps/chifra/pkg/cache/items.go
@@ -1,9 +1,17 @@
 package cache
 
-type CacheItem uint
+type CacheType uint
 
 const (
-	ItemABI CacheItem = iota
-	ItemBlock
-	ItemTransaction
+	Cache_Abis CacheType = iota
+	Cache_Blocks
+	Cache_Transactions
+	Cache_Traces
 )
+
+var itemToDirectory = map[CacheType]string{
+	Cache_Abis:         "abis",
+	Cache_Blocks:       "blocks",
+	Cache_Transactions: "txs",
+	Cache_Traces:       "traces",
+}
--- src/apps/chifra/pkg/cache/paths_test.go
+++ src/apps/chifra/pkg/cache/paths_test.go
@@ -6,7 +6,7 @@ import (
 
 // TODO(cache): Just noting that eventually this will be expanded to all cache paths.
 func Test_getPathByBlock(t *testing.T) {
-	path := getPathByBlock(ItemBlock, 92590)
+	path := getPathByBlock(Cache_Blocks, 92590)
 	if path != "blocks/00/00/92/000092590.bin" {
 		t.Fatal("wrong result", path)
 	}
