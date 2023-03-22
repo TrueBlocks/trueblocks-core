a25b9c14244e27620290d5f7440bc242fa0ed573
tjayrush <jrush@quickblocks.io>
2023-03-21 21:14:57 -0400

    Reworks paths package to combine with cache where it belongs

---
+++ src/apps/chifra/pkg/cache/items_test.go
@@ -0,0 +1,99 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+
+package cache
+
+import "testing"
+
+func TestCacheLayout(t *testing.T) {
+	// TODO: turn these back on if possible
+	// indexPath := config.GetPathToIndex(utils.GetTestChain())
+	// cachePath := config.GetPathToCache(utils.GetTestChain())
+
+	// tests := []struct {
+	// 	on        bool
+	// 	name      string
+	// 	cacheType CacheType
+	// 	param     string
+	// 	expected  CachePath12
+	// 	path      string
+	// 	wantErr   bool
+	// }{
+	// 	{
+	// 		on:    true,
+	// 		name:  "index chunk path",
+	// 		param: "0010000000-0010200000",
+	// 		expected: CachePath12{
+	// 			Type:      Index_Final,
+	// 			RootPath:  indexPath,
+	// 			Subdir:    "finalized/",
+	// 			Extension: ".bin",
+	// 		},
+	// 		path:    "finalized/0010000000-0010200000.bin",
+	// 		wantErr: false,
+	// 	},
+	// 	{
+	// 		on:    true,
+	// 		name:  "Bloom filter path",
+	// 		param: "0010000000-0010200000",
+	// 		expected: CachePath12{
+	// 			Type:      Index_Bloom,
+	// 			RootPath:  indexPath,
+	// 			Subdir:    "blooms/",
+	// 			Extension: ".bloom",
+	// 		},
+	// 		path:    "blooms/0010000000-0010200000.bloom",
+	// 		wantErr: false,
+	// 	},
+	// 	{
+	// 		on:    false,
+	// 		name:  "Block cache path",
+	// 		param: "001001001",
+	// 		expected: CachePath12{
+	// 			Type:      Cache_Block,
+	// 			RootPath:  cachePath,
+	// 			Subdir:    "blocks/",
+	// 			Extension: ".bin",
+	// 		},
+	// 		path:    "blocks/00/10/01/001001001.bin",
+	// 		wantErr: false,
+	// 	},
+	// 	{
+	// 		on:    false,
+	// 		name:  "Transaction cache path",
+	// 		param: "1001001.20",
+	// 		expected: CachePath12{
+	// 			Type:      Cache_Tx,
+	// 			RootPath:  cachePath,
+	// 			Subdir:    "txs/",
+	// 			Extension: ".bin",
+	// 		},
+	// 		path:    "txs/00/10/01/001001001-00020.bin",
+	// 		wantErr: false,
+	// 	},
+	// 	// TraceFn:      $CACHE_PATH/traces/00/10/01/001001001-00020-10.bin
+	// 	// NeighborFn:   $CACHE_PATH/neighbors/00/10/01/001001001-00020.bin
+	// 	// ReconFn:      $CACHE_PATH/recons/c011/a724/00e58ecd99ee497cf89e3775d4bd732f/000000012.00013.bin
+	// }
+
+	// for _, tt := range tests {
+	// 	if !tt.on {
+	// 		continue
+	// 	}
+
+	// 	t.Run(tt.name, func(t *testing.T) {
+	// 		cachePath := NewCachePath(utils.GetTestChain(), tt.expected.Type)
+	// 		if cachePath.Extension != tt.expected.Extension {
+	// 			t.Error("Wrong extension", cachePath.Extension)
+	// 		}
+	// 		if cachePath.Subdir != tt.expected.Subdir {
+	// 			t.Error("Wrong subdir", cachePath.Subdir)
+	// 		}
+	// 		p := cachePath.GetFullPath(tt.param)
+	// 		if p != filepath.Join(tt.expected.RootPath, tt.path) {
+	// 			t.Error("Wrong full path", p)
+	// 		}
+	// 	})
+	// }
+}
---
+++ src/apps/chifra/pkg/cache/walk.go
@@ -0,0 +1,86 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+
+package cache
+
+import (
+	"io/fs"
+	"path/filepath"
+
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/base"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
+)
+
+type IndexFileInfo struct {
+	Type  CacheType
+	Path  string
+	Range base.FileRange
+}
+
+func WalkIndexFolder(chain string, cacheType CacheType, filenameChan chan<- IndexFileInfo) {
+	defer func() {
+		filenameChan <- IndexFileInfo{Type: None}
+	}()
+
+	path := filepath.Join(config.GetPathToIndex(chain), tailFolder(chain, cacheType))
+	if cacheType == Index_Bloom {
+		path = ToBloomPath(path)
+	}
+
+	filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			// If the scraper is running, this will sometimes send an error for a file, for example, that existed
+			// when it was first seen, but the scraper deletes before this call. We ignore any file system errors
+			// this routine, but if we experience problems, we can uncomment this line
+			// fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if !info.IsDir() {
+			rng := base.RangeFromFilename(path)
+			filenameChan <- IndexFileInfo{Type: cacheType, Path: path, Range: rng}
+		}
+		return nil
+	})
+}
+
+func tailFolder(chain string, ct CacheType) string {
+	descrs := map[CacheType]string{
+		None:          "unknown",
+		Index_Bloom:   "blooms",
+		Index_Final:   "finalized",
+		Index_Staging: "staging",
+		Index_Ripe:    "ripe",
+		Index_Unripe:  "unripe",
+		Cache_Abis:    "abis",
+	}
+	return descrs[ct]
+}
+
+type CacheFileInfo struct {
+	Type CacheType
+	Path string
+}
+
+func WalkCacheFolder(chain string, cacheType CacheType, filenameChan chan<- CacheFileInfo) {
+	defer func() {
+		filenameChan <- CacheFileInfo{Type: None}
+	}()
+
+	path := filepath.Join(config.GetPathToCache(chain), tailFolder(chain, cacheType))
+	filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			// If the scraper is running, this will sometimes send an error for a file, for example, that existed
+			// when it was first seen, but the scraper deletes before this call. We ignore any file system errors
+			// this routine, but if we experience problems, we can uncomment this line
+			// fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		if !info.IsDir() {
+			filenameChan <- CacheFileInfo{Type: cacheType, Path: path}
+		}
+
+		return nil
+	})
+}
