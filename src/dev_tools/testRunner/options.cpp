a18882d280982192509e824afdaa1bb659ba8f4f
tjayrush <jrush@quickblocks.io>
2023-04-20 00:22:51 -0400

    Trying to fix tests

--- src/dev_tools/testRunner/options.cpp
+++ src/dev_tools/testRunner/options.cpp
@@ -249,6 +249,23 @@ void establishTestData(void) {
     doCommand("chifra transactions --cache 47055.0");
     doCommand("chifra transactions --cache 46147.0");
 
+    doCommand("chifra monitors --decache 0xf503017d7baf7fbc0fff7492b751025c6a78179b 2>/dev/null");
+    doCommand("chifra monitors --decache 0x9531c059098e3d194ff87febb587ab07b30b1306 2>/dev/null");
+    doCommand("chifra monitors --decache 0x5deda52dc2b3a565d77e10f0f8d4bd738401d7d3 2>/dev/null");
+    doCommand("chifra monitors --decache 0xd0b3462481c33f63a288cd1923e2a261ee65b4ff 2>/dev/null");
+
+    doCommand("chifra abis --clean 0x45f783cce6b7ff23b2ab2d70e416cdb7d6055f51 2>/dev/null");
+    doCommand("chifra abis --clean 0xd7edd2f2bcccdb24afe9a4ab538264b0bbb31373 2>/dev/null");
+    doCommand("chifra abis --clean 0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359");
+    doCommand("chifra abis --clean 0x226159d592e2b063810a10ebf6dcbada94ed68b8");
+    doCommand("chifra abis --clean 0x17996cbddd23c2a912de8477c37d43a1b79770b8");
+    doCommand("chifra abis --clean 0x0000000000004946c0e9f43f4dee607b0ef1fa1c");
+    doCommand("chifra abis --clean 0x7c66550c9c730b6fdd4c03bc2e73c5462c5f7acc");
+    doCommand("chifra abis --clean 0xa478c2975ab1ea89e8196811f51a7b7ade33eb11");
+    doCommand("chifra abis --clean 0x7d655c57f71464b6f83811c55d84009cd9f5221c");
+    doCommand("chifra abis --clean 0x0000000000004946c0e9f43f4dee607b0ef1fa1c");
+    doCommand("chifra abis --clean 0x30f938fed5de6e06a9a7cd2ac3517131c317b1e7");
+
     // Forces the retreival of a few ABI files without which some tests will fail
     doCommand("chifra abis 0x45f783cce6b7ff23b2ab2d70e416cdb7d6055f51");
     doCommand("chifra abis 0xd7edd2f2bcccdb24afe9a4ab538264b0bbb31373");
@@ -260,9 +277,5 @@ void establishTestData(void) {
     doCommand("chifra abis 0xa478c2975ab1ea89e8196811f51a7b7ade33eb11");
     doCommand("chifra abis 0x7d655c57f71464b6f83811c55d84009cd9f5221c");
     doCommand("chifra abis 0x0000000000004946c0e9f43f4dee607b0ef1fa1c");
-
-    doCommand("chifra monitors --decache 0xf503017d7baf7fbc0fff7492b751025c6a78179b 2>/dev/null");
-    doCommand("chifra monitors --decache 0x9531c059098e3d194ff87febb587ab07b30b1306 2>/dev/null");
-    doCommand("chifra monitors --decache 0x5deda52dc2b3a565d77e10f0f8d4bd738401d7d3 2>/dev/null");
-    doCommand("chifra monitors --decache 0xd0b3462481c33f63a288cd1923e2a261ee65b4ff 2>/dev/null");
+    doCommand("chifra abis 0x30f938fed5de6e06a9a7cd2ac3517131c317b1e7");
 }
