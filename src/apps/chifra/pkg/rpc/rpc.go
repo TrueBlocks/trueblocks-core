84246d84105cf5e8ba8ebb4c49b5c6f7c0a74c09
Dawid Szlachta <git@makingprogress.dev>
2023-04-25 15:51:58 +0200

    Added BatchQuery to allow performing batch requests

--- src/apps/chifra/pkg/rpc/rpc.go
+++ src/apps/chifra/pkg/rpc/rpc.go
@@ -41,9 +41,39 @@ func FromRpc(rpcProvider string, payload *Payload, ret interface{}) error {
 	if err != nil {
 		return err
 	}
-	// fmt.Println(string(plBytes))
 
-	body := bytes.NewReader(plBytes)
+	return sendRpcRequest(rpcProvider, plBytes, ret)
+}
+
+func FromRpcBatch(rpcProvider string, payloads []Payload, ret interface{}) error {
+	type rpcPayload struct {
+		Jsonrpc string `json:"jsonrpc"`
+		Method  string `json:"method"`
+		Params  `json:"params"`
+		ID      int `json:"id"`
+	}
+
+	payloadToSend := make([]rpcPayload, 0, len(payloads))
+
+	for _, payload := range payloads {
+		payloadToSend = append(payloadToSend, rpcPayload{
+			Jsonrpc: "2.0",
+			Method:  payload.Method,
+			Params:  payload.Params,
+			ID:      int(atomic.AddUint32(&rpcCounter, 1)),
+		})
+	}
+
+	plBytes, err := json.Marshal(payloadToSend)
+	if err != nil {
+		return err
+	}
+
+	return sendRpcRequest(rpcProvider, plBytes, ret)
+}
+
+func sendRpcRequest(rpcProvider string, marshalled []byte, result any) error {
+	body := bytes.NewReader(marshalled)
 	req, err := http.NewRequest("POST", rpcProvider, body)
 	if err != nil {
 		return err
@@ -61,7 +91,7 @@ func FromRpc(rpcProvider string, payload *Payload, ret interface{}) error {
 		return err
 	}
 
-	return json.Unmarshal(theBytes, ret)
+	return json.Unmarshal(theBytes, result)
 }
 
 func Query[T any](chain string, method string, params Params) (*T, error) {
@@ -98,4 +128,36 @@ func QuerySlice[T any](chain string, method string, params Params) ([]T, error) 
 	}
 
 	return response.Result, err
+}
+
+// BatchPayload is a wrapper around Payload type that allows us
+// to associate a name (Key) to given request.
+type BatchPayload struct {
+	Key string
+	*Payload
+}
+
+// BatchQuery batches requests to the node. Returned values are stored in map, with the same keys as defined
+// in `batchPayload` (this way we don't have to operate on array indices)
+func BatchQuery[T any](chain string, batchPayload []BatchPayload) (map[string]*T, error) {
+	var rpcResponse []struct {
+		Result T `json:"result"`
+	}
+
+	keys := make([]string, 0, len(batchPayload))
+	payloads := make([]Payload, 0, len(batchPayload))
+	for _, config := range batchPayload {
+		keys = append(keys, config.Key)
+		payloads = append(payloads, *config.Payload)
+	}
+
+	err := FromRpcBatch(config.GetRpcProvider(chain), payloads, &rpcResponse)
+	if err != nil {
+		return nil, err
+	}
+	results := make(map[string]*T, len(batchPayload))
+	for index, key := range keys {
+		results[key] = &rpcResponse[index].Result
+	}
+	return results, err
 }
