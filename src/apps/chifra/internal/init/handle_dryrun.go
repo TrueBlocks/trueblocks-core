24cb58354f8481c7c0ce46da95525c8467ceee2a
tjayrush <jrush@quickblocks.io>
2023-06-01 19:18:00 -0400

    Fixes build

--- src/apps/chifra/internal/init/handle_dryrun.go
+++ src/apps/chifra/internal/init/handle_dryrun.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/index"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/manifest"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/output"
