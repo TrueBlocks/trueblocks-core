a4f2a8c3ac169d95ce4a348724be1c8598acffbc
tjayrush <jrush@quickblocks.io>
2023-06-04 21:45:18 -0400

    Cleaning

--- src/apps/chifra/pkg/index/uniq_appearances.go
+++ src/apps/chifra/pkg/index/uniq_appearances.go
@@ -15,6 +15,69 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
+/*
+bool assignReason(const CName& accountedFor, CAppearance& app, const CTransaction& trans) {
+    if (app.transactionIndex > 99996) {  // leave this here for searching: 99999
+        app.reason = "miner";
+        return true;
+    }
+    if (app.address == trans.from) {
+        app.reason = "from";
+        return true;
+    }
+    if (app.address == trans.to) {
+        app.reason = "to";
+        return true;
+    }
+    if (app.address == trans.receipt.contractAddress) {
+        app.reason = "creation";
+        return true;
+    }
+    bool junk = false;
+    string_q ss = substitute(app.address, "0x", "");
+    if (contains(trans.input, ss)) {
+        app.reason = "input";
+        if (accountedFor.address == app.address) {
+            return true;
+        }
+        junk = true;
+    }
+
+    for (size_t i = 0; i < trans.receipt.logs.size(); i++) {
+        const CLog* l = &trans.receipt.logs[i];
+        if (l->address == app.address) {
+            app.reason = "log_" + uint_2_Str(i) + "_generator";
+            return true;
+        }
+        if (contains(l->topics[0], ss)) {
+            app.reason = "log_" + uint_2_Str(i) + "_topic_" + uint_2_Str(0);
+            return true;
+        }
+        for (size_t j = 1; j < l->topics.size(); j++) {
+            if (contains(l->topics[j], ss)) {
+                app.reason = "log_" + uint_2_Str(i) + "_topic_" + uint_2_Str(j);
+                if (accountedFor.address == app.address) {
+                    return true;
+                }
+                junk = true;
+            }
+        }
+        if (contains(l->data, ss)) {
+            app.reason = "log_" + uint_2_Str(i) + "_data";
+            if (accountedFor.address == app.address) {
+                return true;
+            }
+            junk = true;
+        }
+    }
+
+    if (!junk) {
+        app.reason = "trace";
+    }
+    return true;  // !junk;
+}
+*/
+
 // UniqFromLogs extracts addresses from the logs
 func UniqFromLogs(chain string, logs []types.SimpleLog, addrMap AddressBooleanMap) (err error) {
 	for _, log := range logs {
