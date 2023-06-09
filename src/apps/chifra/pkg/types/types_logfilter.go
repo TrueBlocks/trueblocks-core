65388deaa39c592adfc88ed4d53709e4372c40cb
tjayrush <jrush@quickblocks.io>
2023-06-08 21:28:16 -0400

    Adds new type LogFilter and updates docs

---
+++ src/apps/chifra/pkg/types/types_logfilter.go
@@ -0,0 +1,75 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+/*
+ * Parts of this file were generated with makeClass --run. Edit only those parts of
+ * the code inside of 'EXISTING_CODE' tags.
+ */
+
+package types
+
+// EXISTING_CODE
+import (
+	"io"
+
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/base"
+)
+
+// EXISTING_CODE
+
+type RawLogFilter struct {
+	BlockHash string   `json:"blockHash"`
+	Emitters  []string `json:"emitters"`
+	FromBlock string   `json:"fromBlock"`
+	ToBlock   string   `json:"toBlock"`
+	Topics    []string `json:"topics"`
+	// EXISTING_CODE
+	// EXISTING_CODE
+}
+
+type SimpleLogFilter struct {
+	BlockHash base.Hash      `json:"blockHash"`
+	Emitters  []base.Address `json:"emitters"`
+	FromBlock base.Blknum    `json:"fromBlock"`
+	ToBlock   base.Blknum    `json:"toBlock"`
+	Topics    []base.Hash    `json:"topics"`
+	raw       *RawLogFilter  `json:"-"`
+	// EXISTING_CODE
+	// EXISTING_CODE
+}
+
+func (s *SimpleLogFilter) Raw() *RawLogFilter {
+	return s.raw
+}
+
+func (s *SimpleLogFilter) SetRaw(raw *RawLogFilter) {
+	s.raw = raw
+}
+
+func (s *SimpleLogFilter) Model(verbose bool, format string, extraOptions map[string]any) Model {
+	var model = map[string]interface{}{}
+	var order = []string{}
+
+	// EXISTING_CODE
+	// EXISTING_CODE
+
+	return Model{
+		Data:  model,
+		Order: order,
+	}
+}
+
+func (s *SimpleLogFilter) WriteTo(w io.Writer) (n int64, err error) {
+	// EXISTING_CODE
+	// EXISTING_CODE
+	return 0, nil
+}
+
+func (s *SimpleLogFilter) ReadFrom(r io.Reader) (n int64, err error) {
+	// EXISTING_CODE
+	// EXISTING_CODE
+	return 0, nil
+}
+
+// EXISTING_CODE
+// EXISTING_CODE
