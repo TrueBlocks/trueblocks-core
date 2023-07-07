4c741cabffe844f2a214db4e1f800ccf218c73be
Dawid Szlachta <git@makingprogress.dev>
2023-07-07 11:52:33 +0200

    Fixes #3020 Added argument compatibility check, better error messages

--- src/apps/chifra/pkg/abi/find.go
+++ src/apps/chifra/pkg/abi/find.go
@@ -19,16 +19,61 @@ const (
 func FindAbiFunction(mode FindMode, identifier string, arguments []*parser.ContractCallArgument, abis AbiInterfaceMap) (fn *types.SimpleFunction, suggestions []types.SimpleFunction, err error) {
 	for _, function := range abis {
 		function := function
-		// TODO: is this too naive?
+		// first we check if names match (or selectors, depending on mode)
 		if mode == FindByName && function.Name != identifier {
 			continue
 		}
 		if mode == FindBySelector && function.Encoding != strings.ToLower(identifier) {
 			continue
 		}
-		if arguments != nil && len(function.Inputs) != len(arguments) {
-			suggestions = append(suggestions, *function)
-			continue
+		// now we will compare arguments
+		if arguments != nil {
+			// we start with argument count
+			if len(function.Inputs) != len(arguments) {
+				suggestions = append(suggestions, *function)
+				continue
+			}
+			// now we check if the argument types are compatible
+			compatible := false
+			for index, input := range function.Inputs {
+				argument := arguments[index]
+				if input.InternalType == "string" && argument.String == nil {
+					break
+				}
+				if input.InternalType == "bool" && argument.Boolean == nil {
+					break
+				}
+				// address in parsed into argument.Hex.Address
+				if input.InternalType == "address" {
+					if argument.Hex == nil {
+						break
+					}
+					if argument.Hex.Address == nil {
+						break
+					}
+				}
+				// bytes32, array and tuples of any kind are only support via
+				// hashes. Hashes are parsed into argument.Hex.String
+				if input.InternalType == "bytes32" ||
+					strings.Contains(input.InternalType, "[") ||
+					strings.Contains(input.InternalType, "(") {
+					if argument.Hex == nil {
+						break
+					}
+					if argument.Hex.String == nil {
+						break
+					}
+				}
+				// we use strings.Contains here, because all integers are parsed into argument.Number
+				if strings.Contains(input.InternalType, "int") && argument.Number == nil {
+					break
+				}
+				compatible = true
+			}
+			if !compatible {
+				suggestions = append(suggestions, *function)
+				continue
+			}
 		}
 
 		return function, nil, nil
