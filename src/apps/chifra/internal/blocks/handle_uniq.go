6e11a6088ed55a41b1a63b7dbd039da380a269fe
tjayrush <jrush@quickblocks.io>
2023-08-11 09:08:26 -0400

    Cleaning uniq blocks processor

--- src/apps/chifra/internal/blocks/handle_uniq.go
+++ src/apps/chifra/internal/blocks/handle_uniq.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"errors"
 
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/index"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/names"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/output"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/utils"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/uniq"
 	"github.com/ethereum/go-ethereum"
 )
 
@@ -38,9 +36,7 @@ func (opts *BlocksOptions) HandleUniq() (err error) {
 			bar := logger.NewBar("", showProgress, int64(len(blockNums)))
 			for _, bn := range blockNums {
 				bar.Tick()
-				addrMap := make(index.AddressBooleanMap)
-				ts := opts.Conn.GetBlockTimestamp(bn)
-				if err := opts.ProcessBlockUniqs(chain, procFunc, bn, addrMap, ts); err != nil {
+				if err := uniq.GetUniqAddressesInBlock(chain, opts.Flow, opts.Conn, procFunc, bn); err != nil {
 					errorChan <- err
 					if errors.Is(err, ethereum.NotFound) {
 						continue
@@ -57,58 +53,4 @@ func (opts *BlocksOptions) HandleUniq() (err error) {
 		"uniq": true,
 	}
 	return output.StreamMany(ctx, fetchData, opts.Globals.OutputOptsWithExtra(extra))
-}
-
-func (opts *BlocksOptions) ProcessBlockUniqs(chain string, procFunc index.UniqProcFunc, bn uint64, addrMap index.AddressBooleanMap, ts int64) error {
-	if bn == 0 {
-		if namesArray, err := names.LoadNamesArray(chain, names.Prefund, names.SortByAddress, []string{}); err != nil {
-			return err
-		} else {
-			for i, name := range namesArray {
-				address := name.Address.Hex()
-				index.StreamAppearance(procFunc, opts.Flow, "genesis", address, bn, uint64(i), utils.NOPOS, ts, addrMap)
-			}
-		}
-
-	} else {
-		if block, err := opts.Conn.GetBlockBodyByNumber(bn); err != nil {
-			return err
-		} else {
-			miner := block.Miner.Hex()
-			txid := uint64(99999)
-			if block.Miner.IsZero() {
-				// Early clients allowed misconfigured miner settings with address 0x0 (reward got
-				// burned). We enter a false record with a false tx_id to account for this.
-				miner = "0xdeaddeaddeaddeaddeaddeaddeaddeaddeaddead"
-				txid = 99997
-			}
-			index.StreamAppearance(procFunc, opts.Flow, "miner", miner, bn, txid, utils.NOPOS, ts, addrMap)
-
-			if uncles, err := opts.Conn.GetUnclesByNumber(bn); err != nil {
-				return err
-			} else {
-				for _, uncle := range uncles {
-					unc := uncle.Miner.Hex()
-					txid = uint64(99998)
-					if uncle.Miner.IsZero() {
-						// Early clients allowed misconfigured miner settings with address 0x0 (reward got
-						// burned). We enter a false record with a false tx_id to account for this.
-						unc = "0xdeaddeaddeaddeaddeaddeaddeaddeaddeaddead"
-						txid = 99998 // do not change this!
-					}
-					index.StreamAppearance(procFunc, opts.Flow, "uncle", unc, bn, txid, utils.NOPOS, ts, addrMap)
-				}
-			}
-
-			for _, trans := range block.Transactions {
-				if trans.Traces, err = opts.Conn.GetTracesByTransactionID(trans.BlockNumber, trans.TransactionIndex); err != nil {
-					return err
-				}
-				if err = index.UniqFromTransDetails(chain, procFunc, opts.Flow, &trans, ts, addrMap, opts.Conn); err != nil {
-					return err
-				}
-			}
-		}
-	}
-	return nil
 }
