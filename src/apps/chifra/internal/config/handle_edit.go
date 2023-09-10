60d5926a36dc2633c920d01fd5e677bb6660a1fd
Thomas Jay Rush <jrush@quickblocks.io>
2023-09-10 04:01:38 +0200

    Adds chifra config edit

---
+++ src/apps/chifra/internal/config/handle_edit.go
@@ -0,0 +1,17 @@
+package configPkg
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/utils"
+)
+
+func (opts *ConfigOptions) HandleEdit() error {
+	editor := os.Getenv("EDITOR")
+	configFolder := config.GetPathToRootConfig()
+	configFile := filepath.Join(configFolder + "trueBlocks.toml")
+	utils.System(editor + " " + configFile)
+	return nil
+}
