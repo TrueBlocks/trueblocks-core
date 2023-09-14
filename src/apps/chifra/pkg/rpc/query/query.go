c6fc9bf52f92b6c720cf2fe485c9e5cb8d0b74ca
Thomas Jay Rush <jrush@quickblocks.io>
2023-09-14 11:10:16 +0200

    Implements TB_DEBUG_CURL

--- src/apps/chifra/pkg/rpc/query/query.go
+++ src/apps/chifra/pkg/rpc/query/query.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"runtime"
+	"strings"
 	"sync/atomic"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
@@ -64,21 +66,48 @@ func Query[T any](chain string, method string, params Params) (*T, error) {
 
 var rpcCounter uint32
 
-// fromRpc Returns all traces for a given block.
-func FromRpc(rpcProvider string, payload *Payload, ret interface{}) error {
-	type rpcPayload struct {
-		Jsonrpc string `json:"jsonrpc"`
-		Method  string `json:"method"`
-		Params  `json:"params"`
-		ID      int `json:"id"`
+type rpcPayload struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Method  string `json:"method"`
+	Params  `json:"params"`
+	ID      int `json:"id"`
+}
+
+var devDebug = false
+var devDebugMethod = ""
+
+func init() {
+	devDebugMethod = os.Getenv("TB_DEBUG_CURL")
+	devDebug = len(devDebugMethod) > 0
+}
+
+func debugCurl(payload rpcPayload, rpcProvider string) {
+	if !devDebug {
+		return
 	}
 
+	if devDebugMethod != "true" && payload.Method != devDebugMethod {
+		return
+	}
+
+	bytes, _ := json.MarshalIndent(payload, "", "")
+	payloadStr := strings.Replace(string(bytes), "\n", " ", -1)
+	var curlCmd = `curl -X POST -H "Content-Type: application/json" --data '[{payload}]' [{rpcProvider}]`
+	curlCmd = strings.Replace(curlCmd, "[{payload}]", payloadStr, -1)
+	curlCmd = strings.Replace(curlCmd, "[{rpcProvider}]", rpcProvider, -1)
+	fmt.Println(curlCmd)
+}
+
+// fromRpc Returns all traces for a given block.
+func FromRpc(rpcProvider string, payload *Payload, ret interface{}) error {
 	payloadToSend := rpcPayload{
 		Jsonrpc: "2.0",
 		Method:  payload.Method,
 		Params:  payload.Params,
 		ID:      int(atomic.AddUint32(&rpcCounter, 1)),
 	}
+
+	debugCurl(payloadToSend, rpcProvider)
 
 	plBytes, err := json.Marshal(payloadToSend)
 	if err != nil {
