dc288644aba7c6ad208eaa8b9e87c3f34a628b9c
Thomas Jay Rush <jrush@quickblocks.io>
2023-10-01 14:20:43 -0400

    Stop asking for uncles after MergeBlock

--- src/apps/chifra/pkg/base/known_blocks.go
+++ src/apps/chifra/pkg/base/known_blocks.go
@@ -5,5 +5,6 @@ const (
 	ByzantiumBlock      = Blknum(4370000)
 	ConstantinopleBlock = Blknum(7280000)
 	LondonBlock         = Blknum(12965000)
+	MergeBlock          = Blknum(15537393)
 	ShanghaiBlock       = Blknum(17034870)
 )
