fe2ebf5e30b5f95221c9416452326c7b5985c4b9
Thomas Jay Rush <jrush@quickblocks.io>
2023-10-04 16:00:46 -0400

    Enables chifra config edit and chifra config show

--- src/apps/chifra/internal/config/validate.go
+++ src/apps/chifra/internal/config/validate.go
@@ -5,6 +5,8 @@
 package configPkg
 
 import (
+	"os"
+
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/validate"
 )
@@ -25,6 +27,14 @@ func (opts *ConfigOptions) validateConfig() error {
 	if err := validate.ValidateEnum("modes", opts.Mode, "[show|edit]"); err != nil {
 		return err
 	}
+
+	if !opts.Globals.TestMode && opts.Mode == "edit" && os.Getenv("EDITOR") == "" {
+		return validate.Usage("You must set the EDITOR environment variable to use the 'edit' mode.")
+	}
+
+	// if len(opts.Mode) == 0 || !opts.Paths {
+	// 	return validate.Usage("You must supply either {0} or {1}.", "a mode", "--paths")
+	// }
 
 	return opts.Globals.Validate()
 }
