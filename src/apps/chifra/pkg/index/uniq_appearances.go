a842748234a99d728e5d646b28b788e16418442d
tjayrush <jrush@quickblocks.io>
2023-10-07 09:14:30 -0400

    Fixes

--- src/apps/chifra/pkg/index/uniq_appearances.go
+++ src/apps/chifra/pkg/index/uniq_appearances.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/base"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/colors"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/rpc"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -78,26 +80,38 @@ func UniqFromTraces(chain string, traces []types.SimpleTrace, addrMap AddressBoo
 		} else if trace.TraceType == "reward" {
 			if trace.Action.RewardType == "block" {
 				author := trace.Action.Author.Hex()
-				fakeId := types.BlockReward
-				if base.IsPrecompile(author) {
-					author = base.SentinalAddr.Hex()
-					fakeId = types.MisconfigReward
+				a := base.HexToAddress(author)
+				if a.IsZero() {
+					// Early clients allowed misconfigured miner settings with address
+					// 0x0 (reward got burned). We enter a false record with a false tx_id
+					// to account for this.
+					author = "0xdeaddeaddeaddeaddeaddeaddeaddeaddeaddead"
+					addAddressToMaps(author, bn, 99997, addrMap)
+
+				} else {
+					addAddressToMaps(author, bn, 99999, addrMap)
+
 				}
-				addAddressToMaps(author, bn, fakeId, addrMap)
 
 			} else if trace.Action.RewardType == "uncle" {
 				author := trace.Action.Author.Hex()
-				fakeId := types.UncleReward
-				if base.IsPrecompile(author) {
-					author = base.SentinalAddr.Hex()
-					fakeId = types.MisconfigReward
+				a := base.HexToAddress(author)
+				if a.IsZero() {
+					// Early clients allowed misconfigured miner settings with address
+					// 0x0 (reward got burned). We enter a false record with a false tx_id
+					// to account for this.
+					author = "0xdeaddeaddeaddeaddeaddeaddeaddeaddeaddead"
+					addAddressToMaps(author, bn, 99998, addrMap)
+
+				} else {
+					addAddressToMaps(author, bn, 99998, addrMap)
+
 				}
-				addAddressToMaps(author, bn, fakeId, addrMap)
 
 			} else if trace.Action.RewardType == "external" {
 				// This only happens in xDai as far as we know...
 				author := trace.Action.Author.Hex()
-				addAddressToMaps(author, bn, types.ExternalReward, addrMap)
+				addAddressToMaps(author, bn, 99996, addrMap)
 
 			} else {
 				fmt.Println("Unknown reward type", trace.Action.RewardType)
@@ -137,7 +151,46 @@ func UniqFromTraces(chain string, traces []types.SimpleTrace, addrMap AddressBoo
 			if trace.Action.To.IsZero() {
 				if trace.Result != nil && trace.Result.Address.IsZero() {
 					if trace.Error != "" {
-						if receipt, err := conn.GetReceiptNoTimestamp(bn, txid); err == nil {
+						receipt, err := conn.GetReceiptNoTimestamp(bn, txid)
+						if err != nil {
+							msg := fmt.Sprintf("rpcCall failed at block %d, tx %d hash %s err %s", bn, txid, trace.TransactionHash, err)
+							logger.Warn(colors.Red, msg, colors.Off)
+							// TODO: This is possibly an error in Erigon - remove it when they fix this issue:
+							// TODO: https://github.com/ledgerwatch/erigon/issues/6956. It may require a
+							// TODO: full resync. Yes, the problem appears to be this specific. The follow
+							// TODO: hack (which tries to correct the problem) may well not work, but
+							// TODO: the hope is that these will have already been picked up by the traces.
+							// TODO: When fixed, we need to re-scrape from block 16,600,000. This map (which was
+							// TODO: retrieved from Nethermind) tries to repair the missing data by marking (for
+							// TODO: each transaction) any smart contracts created.
+							fixMap := map[string]string{
+								"16616983-242": "0x6784d7583cf2528daa270b555a4cb5376648488f",
+								"16618181-146": "0x86494c70df6d3416bb4f536a82533b6120c52cde",
+								"16618196-18":  "0x40d7b756557d9f7a5655ff70b3253a07f714807a",
+								"16620128-12":  "0xb8fb9a557d19d5266f1ba1724445ee2436e3c626",
+								"16620182-35":  "0x708bf2bf05492a5644787c134cf8a64e82fa4c52",
+								"16621080-107": "0x23c84318fb83ee62e059679cddb3914c923da871",
+								"16623590-179": "0xe88d3857676adf23d8324231eabee6ac390f666e",
+								"16623602-106": "0x473a0524a25c252bc65a023c8b8476b1eb6ac805",
+								"16626181-115": "0x010d9eb886f5b1a0fbef58bca722079e9ac75275",
+								"16627272-125": "0xdfd76821bebdbe589f74d311dff4f5859995cda4",
+								"16628102-66":  "0xddec22d76cfb1aded71c2f7b64ff768d207d615d",
+							}
+							key := fmt.Sprintf("%d-%d", bn, txid)
+							msg = err.Error()
+							if msg != "empty hex string" {
+								// not the error we're looking for
+								return err
+							}
+
+							if len(fixMap[key]) > 0 {
+								// both are true - the error is `empty hex string` and we have a fix
+								msg = fmt.Sprintf("Corrected %d, tx %d adds %s", bn, txid, fixMap[key])
+								logger.Warn(colors.Red, msg, colors.Off)
+								addAddressToMaps(fixMap[key], bn, txid, addrMap)
+							}
+
+						} else {
 							addr := hexutil.Encode(receipt.ContractAddress.Bytes())
 							addAddressToMaps(addr, bn, txid, addrMap)
 						}
