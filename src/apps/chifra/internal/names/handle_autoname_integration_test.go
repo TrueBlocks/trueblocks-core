496dc92c95c76782cf4c2cbf205bd6812b13f842
Thomas Jay Rush <jrush@quickblocks.io>
2023-10-11 00:34:24 -0400

    Fixes tests

--- src/apps/chifra/internal/names/handle_autoname_integration_test.go
+++ src/apps/chifra/internal/names/handle_autoname_integration_test.go
@@ -58,6 +58,7 @@ func TestNamesOptions_autoname(t *testing.T) {
 				Address:  base.HexToAddress("0x054993ab0f2b1acc0fdc65405ee203b4271bebe6"),
 				Name:     "M-Wallet 0x5499",
 				Petname:  "sadly-settling-anteater",
+				Source:   "EtherScan.io",
 				Tags:     "00-Active",
 				IsCustom: true,
 			},
