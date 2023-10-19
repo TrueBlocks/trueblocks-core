90c737252f561c38491a5760a2105070aa15762a
tjayrush <jrush@quickblocks.io>
2023-10-19 14:46:57 -0400

    Prefer OpenFile to golang shortcuts Create and Open

--- src/apps/chifra/pkg/tslib/establish.go
+++ src/apps/chifra/pkg/tslib/establish.go
@@ -102,7 +102,7 @@ func downloadCidToBinary(chain, database, outputFn, cid string) error {
 	trapChannel := sigintTrap.Enable(ctx, cancel, cleanOnQuit)
 	defer sigintTrap.Disable(trapChannel)
 
-	ff, err := os.Create(outputFn)
+	ff, err := os.OpenFile(outputFn, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
 		return err
 	}
