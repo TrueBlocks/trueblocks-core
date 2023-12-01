03c21e80fce112824195bd316b6177449d1fdb46
tjayrush <jrush@quickblocks.io>
2023-11-30 23:19:13 -0500

    sliceOfMaps method for chifra receipts, traces, and state

--- src/apps/chifra/internal/traces/handle_counts.go
+++ src/apps/chifra/internal/traces/handle_counts.go
@@ -23,69 +23,78 @@ func (opts *TracesOptions) HandleCounts() error {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	fetchData := func(modelChan chan types.Modeler[types.RawModeler], errorChan chan error) {
-		// var cnt int
-		var err error
-		var appMap map[types.SimpleAppearance]*types.SimpleTransaction
-		if appMap, _, err = identifiers.AsMap[types.SimpleTransaction](chain, opts.TransactionIds); err != nil {
+		if sliceOfMaps, cnt, err := identifiers.SliceOfMaps_AsMaps[types.SimpleTransaction](chain, opts.TransactionIds); err != nil {
 			errorChan <- err
 			cancel()
+
+		} else if cnt == 0 {
+			errorChan <- fmt.Errorf("no transactions found")
+			cancel()
+
 		} else {
 			bar := logger.NewBar(logger.BarOptions{
-				Enabled: !opts.Globals.TestMode,
-				Total:   int64(len(appMap)),
+				Enabled: !testMode && !utils.IsTerminal(),
+				Total:   int64(cnt),
 			})
 
-			iterFunc := func(app types.SimpleAppearance, value *types.SimpleTransaction) error {
-				if tx, err := opts.Conn.GetTransactionByAppearance(&app, true); err != nil {
-					errorChan <- fmt.Errorf("transaction at %s returned an error: %w", app.Orig(), err)
-					return nil
-
-				} else if tx == nil || len(tx.Traces) == 0 {
-					errorChan <- fmt.Errorf("transaction at %s has no traces", app.Orig())
-					return nil
-
-				} else {
-					*value = *tx
-					bar.Tick()
-					return nil
+			for _, thisMap := range sliceOfMaps {
+				thisMap := thisMap
+				for app := range thisMap {
+					thisMap[app] = new(types.SimpleTransaction)
 				}
-			}
 
-			iterErrorChan := make(chan error)
-			iterCtx, iterCancel := context.WithCancel(context.Background())
-			defer iterCancel()
-			go utils.IterateOverMap(iterCtx, iterErrorChan, appMap, iterFunc)
-			for err := range iterErrorChan {
-				if !testMode || nErrors == 0 {
-					errorChan <- err
-					nErrors++
+				iterFunc := func(app types.SimpleAppearance, value *types.SimpleTransaction) error {
+					if tx, err := opts.Conn.GetTransactionByAppearance(&app, true); err != nil {
+						errorChan <- fmt.Errorf("transaction at %s returned an error: %w", app.Orig(), err)
+						return nil
+
+					} else if tx == nil || len(tx.Traces) == 0 {
+						errorChan <- fmt.Errorf("transaction at %s has no traces", app.Orig())
+						return nil
+
+					} else {
+						*value = *tx
+						bar.Tick()
+						return nil
+					}
+				}
+
+				iterErrorChan := make(chan error)
+				iterCtx, iterCancel := context.WithCancel(context.Background())
+				defer iterCancel()
+				go utils.IterateOverMap(iterCtx, iterErrorChan, thisMap, iterFunc)
+				for err := range iterErrorChan {
+					if !testMode || nErrors == 0 {
+						errorChan <- err
+						nErrors++
+					}
+				}
+
+				items := make([]*types.SimpleTransaction, 0, len(thisMap))
+				for _, tx := range thisMap {
+					items = append(items, tx)
+				}
+				sort.Slice(items, func(i, j int) bool {
+					if items[i].BlockNumber == items[j].BlockNumber {
+						return items[i].TransactionIndex < items[j].TransactionIndex
+					}
+					return items[i].BlockNumber < items[j].BlockNumber
+				})
+				for _, item := range items {
+					item := item
+					if !item.BlockHash.IsZero() {
+						counter := simpleTraceCount{
+							BlockNumber:      uint64(item.BlockNumber),
+							TransactionIndex: uint64(item.TransactionIndex),
+							TransactionHash:  item.Hash,
+							Timestamp:        item.Timestamp,
+							TracesCnt:        uint64(len(item.Traces)),
+						}
+						modelChan <- &counter
+					}
 				}
 			}
 			bar.Finish(true)
-
-			items := make([]*types.SimpleTransaction, 0, len(appMap))
-			for _, tx := range appMap {
-				items = append(items, tx)
-			}
-			sort.Slice(items, func(i, j int) bool {
-				if items[i].BlockNumber == items[j].BlockNumber {
-					return items[i].TransactionIndex < items[j].TransactionIndex
-				}
-				return items[i].BlockNumber < items[j].BlockNumber
-			})
-			for _, item := range items {
-				item := item
-				if !item.BlockHash.IsZero() {
-					counter := simpleTraceCount{
-						BlockNumber:      uint64(item.BlockNumber),
-						TransactionIndex: uint64(item.TransactionIndex),
-						TransactionHash:  item.Hash,
-						Timestamp:        item.Timestamp,
-						TracesCnt:        uint64(len(item.Traces)),
-					}
-					modelChan <- &counter
-				}
-			}
 		}
 	}
 
