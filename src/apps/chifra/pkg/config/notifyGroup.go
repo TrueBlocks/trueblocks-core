999b52052ace51c3cd9c1e084cf5490f338056c6
tjayrush <jrush@quickblocks.io>
2024-02-14 19:56:16 -0500

    We're done

---
+++ src/apps/chifra/pkg/config/notifyGroup.go
@@ -0,0 +1,10 @@
+// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+
+package config
+
+type notifyGroup struct {
+	Url    string `toml:"url" json:"url,omitempty"`
+	Author string `toml:"author" json:"author,omitempty"`
+}
