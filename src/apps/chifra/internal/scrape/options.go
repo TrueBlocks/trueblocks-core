8564e90cb3436b1f31b19e2253d5b957829974e3
tjayrush <jrush@quickblocks.io>
2024-03-20 21:34:15 -0400

    From first working version of goMaker, updates headers only but produces identical code

--- src/apps/chifra/internal/scrape/options.go
+++ src/apps/chifra/internal/scrape/options.go
@@ -1,8 +1,9 @@
-// Copyright 2021 The TrueBlocks Authors. All rights reserved.
+// Copyright 2016, 2024 The TrueBlocks Authors. All rights reserved.
 // Use of this source code is governed by a license that can
 // be found in the LICENSE file.
 /*
- * This file was auto generated with makeClass --gocmds. DO NOT EDIT.
+ * Parts of this file were auto generated. Edit only those parts of
+ * the code inside of 'EXISTING_CODE' tags.
  */
 
 package scrapePkg
