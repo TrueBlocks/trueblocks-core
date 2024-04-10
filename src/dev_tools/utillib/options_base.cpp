dc760b55b860552cdaeb325ebc266f5c148fc8c7
Thomas Jay Rush <jrush@quickblocks.io>
2024-04-09 21:38:07 -0400

    Revert "Revert "Fix Typos in Comments (#3569)" (#3570)"
    This reverts commit f45e0b23adc3e7b3e71c82a621c2029b69a2f1f9.

--- src/dev_tools/utillib/options_base.cpp
+++ src/dev_tools/utillib/options_base.cpp
@@ -224,7 +224,7 @@ bool COptionsBase::prepareArguments(int argCountIn, const char* argvIn[]) {
         if (!arg.empty())
             argumentsOut3.push_back(arg);
 
-    // If we have a command file, we will use it, if not we will creat one and pretend we have one.
+    // If we have a command file, we will use it, if not we will create one and pretend we have one.
     string_q commandList = "";
     for (auto arg : argumentsOut3) {
         commandList += (arg + " ");
@@ -259,7 +259,7 @@ bool COptionsBase::standardOptions(string_q& cmdLine) {
         replaceAll(cmdLine, "--append", "");
     }
 
-    // Note: check each item individual in case more than one appears on the command line
+    // Note: check each item individually in case more than one appears on the command line
     cmdLine += " ";
     replace(cmdLine, "--output ", "--output:");
 
@@ -402,7 +402,7 @@ void COptionsBase::configureDisplay(const string_q& tool, const string_q& dataTy
         case TXT1:
         case CSV1:
             if (isTestMode()) {
-                // Just warning the user as if this is set it may break test cases
+                // Just warning the user cause if this is set it may break test cases
                 string test = getGlobalConfig(tool)->getConfigStr("display", "format", "<not_set>");
                 if (test != "<not_set>")
                     LOG_WARN("Tests will fail. Custom display string set to: ", test);
