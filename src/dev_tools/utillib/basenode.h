af9567c7d72c12720f13654c825aed9106132349
Thomas Jay Rush <jrush@quickblocks.io>
2024-04-12 10:36:04 -0400

    Cleaning

--- src/dev_tools/utillib/basenode.h
+++ src/dev_tools/utillib/basenode.h
@@ -1,7 +1,6 @@
 #pragma once
 /*
-
-*/
+ */
 #include <algorithm>
 #include "runtimeclass.h"
 
@@ -9,29 +8,8 @@ namespace qblocks {
 
 class CBaseNode {
   public:
-    uint64_t m_deleted;
-    uint64_t m_schema;
-    uint64_t m_showing;
-
-  public:
     CBaseNode(void);
     virtual ~CBaseNode(void);
-    bool isDeleted(void) const;
-    void setDeleted(bool val);
-
-  public:
-    static CRuntimeClass classCBaseNode;
-    virtual CRuntimeClass* getRuntimeClass(void) const;
-    virtual const CBaseNode* getObjectAt(const string_q& name, size_t i) const {
-        return NULL;
-    }
-    virtual const string_q getStringAt(const string_q& fieldName, size_t i) const {
-        return "";
-    }
-
-  protected:
-    void initialize(void);
-    void duplicate(const CBaseNode& bn);
 };
 
 template <class T>
@@ -45,6 +23,12 @@ T RandomValue(T a, T b) {
 template <class T>
 inline bool inRange(T val, T mn, T mx) {
     return (val >= mn && val <= mx);
+}
+
+inline CBaseNode::CBaseNode(void) {
+}
+
+inline CBaseNode::~CBaseNode(void) {
 }
 
 }  // namespace qblocks
