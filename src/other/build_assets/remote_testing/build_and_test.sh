9f1294a81e07f1c5e15b22915d4871f1ba8b27e9
tjayrush <jrush@quickblocks.io>
2024-04-16 13:26:09 -0400

    Fixing

--- src/other/build_assets/remote_testing/build_and_test.sh
+++ src/other/build_assets/remote_testing/build_and_test.sh
@@ -11,14 +11,6 @@ echo "Whoami:      `whoami`"
 echo "Server port: $SERVER_PORT"
 echo "Pwd:         $(pwd)"
 
-# warms up the cache, makes sure there's an installation
-chifra blocks 1>/dev/null 2>&1
-RESULT=$?
-if [ $RESULT -ne 0 ]; then
-   cd $BUILD_FOLDER
-   exit $RESULT
-fi
-
 # starts the API server and runs the tests
 export TB_TEST_API_SERVER=$SERVER_PORT
 export TB_REMOTE_TESTING=true
