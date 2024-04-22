ace33d6803dd12e133f823958c7013b415058eae
Thomas Jay Rush <jrush@quickblocks.io>
2024-04-21 22:22:47 -0400

    Remove examples submodule

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-echo "Building examples"
-pwd
 cd examples
-pwd
-go build -o ../bin/examples/example1 example1/main.go
-ls -l ../bin/examples/
+go build -o ../bin/examples/simple simple/main.go
