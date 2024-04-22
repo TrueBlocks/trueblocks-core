cf442e547b6107e91c3d76129bc199641975d818
Thomas Jay Rush <jrush@quickblocks.io>
2024-04-21 21:07:15 -0400

    Tries to use semantic versioning for SDK (#3601)
    * Tries to use semantic versioning for SDK
    
    * Tries to use semantic versioning for SDK

--- src/dev_tools/testRunner/go.mod
+++ src/dev_tools/testRunner/go.mod
@@ -4,3 +4,4 @@ module github.com/TrueBlocks/trueblocks-core/testRunner
 go 1.22
 
 replace github.com/TrueBlocks/trueblocks-core/sdk => ../../../sdk
+
--- src/other/build_assets/test-all.sh
+++ src/other/build_assets/test-all.sh
@@ -32,8 +32,8 @@ git submodule status tests
 cd build/dev_tools
 make -j 8
 cd ..
-#npm install -g prettier
-#make generate
+# npm install -g prettier
+# make generate
 make -j 8
 
 cd "$BUILD_FOLDER/"
