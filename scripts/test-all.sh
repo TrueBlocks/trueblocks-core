2743d761a6cac25f4143df7aaccb00f2937df61a
Thomas Jay Rush <jrush@quickblocks.io>
2024-04-22 22:34:18 -0400

    Starting to move other scripts

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -4,9 +4,9 @@
 # Assumes we start in the ./build folder
 cd ..
 REPO_ROOT=`pwd`
-BUILD_FOLDER="$REPO_ROOT/build/"
+BUILD_FOLDER=$REPO_ROOT/build/
 DEST_FOLDER="$HOME/Library/Application Support/TrueBlocks/config/mainnet"
-if [[ -d "$DEST_FOLDER" ]]
+if [[ -d $DEST_FOLDER ]]
 then
     echo ""
 else
@@ -14,51 +14,53 @@ else
 fi
 
 #------------------------------------------------
-# Function to check command results and quit if failed
-check_result() {
-    local result=$1
-    local message=${2:-"Build failed. Quitting."}
-    if [ $result -ne 0 ]; then
-        echo $message
-        cd $BUILD_FOLDER
-        exit $result
-    fi
-}
-
-#------------------------------------------------
-echo "Adding the tests submodule"
-cd "$REPO_ROOT"
-git submodule status tests
-
-#------------------------------------------------
 echo "Installing config files"
-cd "$REPO_ROOT/build/other/install"
+INSTALL=$REPO_ROOT/build/other/install
+cd "$INSTALL"
 make
 
 #------------------------------------------------
 echo "Running go tests..."
-cd "$REPO_ROOT/src/apps/chifra"
+CHIFRA=$REPO_ROOT/src/apps/chifra
+cd "$CHIFRA"
 go test --tags integration ./...
-check_result $? "Go tests failed. Quitting."
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+   cd $BUILD_FOLDER
+   exit $RESULT
+fi
+
+#------------------------------------------------
+echo "Adding the tests submodule"
+cd $REPO_ROOT
+git submodule status tests
 
 #------------------------------------------------
 echo "Running cmake"
 cd "$BUILD_FOLDER"
 cmake ../src
-check_result $? "CMake failed. Quitting."
 
 #------------------------------------------------
-echo "Running CMake"
+echo "Running cmake"
 # npm install -g prettier
 # make generate
 make -j 8
-check_result $?
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+   echo "Build failed. Quitting."
+   exit $RESULT
+fi
+
+#------------------------------------------------
+# The names.tab file is ours, so we can always replace it for testing
+cp -f "$REPO_ROOT/src/other/install/names/names.tab" "$DEST_FOLDER"
+touch "$DEST_FOLDER/names.tab"
 
 #------------------------------------------------
 echo "Running the tests with testRunner"
 TEST_MODE=true testRunner
-check_result ${PIPESTATUS[0]}
+RESULT=${PIPESTATUS[0]}
 
 #------------------------------------------------
 echo "Done..."
-exit 0
+exit $RESULT
