a8d6bc7ca9654e1dfa0ceee8cee3b504e37725cc
Thomas Jay Rush <jrush@quickblocks.io>
2024-04-26 07:35:38 -0400

    Use 'go work use .' in make-go-work.sh

--- scripts/make-go-work.sh
+++ scripts/make-go-work.sh
@@ -15,29 +15,16 @@ cd "$SCRIPT_DIR/.."
 #------------------------------------------------
 # Check if the go.work file already exists
 GO_WORK_FILE="go.work"
-if [ -f "$GO_WORK_FILE" ]; then
-    echo "Error: $GO_WORK_FILE already exists. Remove it before re-running this script."
-    exit 1
-fi
-
-#------------------------------------------------
-# Echo in header content to the go.work file
-echo "// Go Version" > $GO_WORK_FILE
-echo "go 1.22" >> $GO_WORK_FILE
-echo "" >> $GO_WORK_FILE
-echo "use (" >> $GO_WORK_FILE
 
 #------------------------------------------------
 # Find all go.mod files in the src directory and
 # add their directories to go.work.
 find . -type f -name 'go.mod' | while read -r modfile; do
     moddir=$(dirname "$modfile")
-    echo "	$moddir" >> $GO_WORK_FILE
+    go work use "$moddir"
 done
 
 #------------------------------------------------
-# Close the file, and run go work sync
-echo ")" >> $GO_WORK_FILE
 go work sync
 
 #------------------------------------------------
