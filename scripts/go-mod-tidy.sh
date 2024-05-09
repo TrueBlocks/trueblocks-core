b78f72cbbb2a106fbd78b9fc948d35bafaaaadcd
Thomas Jay Rush <jrush@quickblocks.io>
2024-05-08 22:51:10 -0400

    Updates go.mod in src/apps/chifra (#3671)
    * Updates go.mod in src/apps/chifra
    
    * Updates go.mod in examples/balanceChart
    
    * Updates go.mod in examples/findFirst
    
    * Updates go.mod in examples/simple
    
    * Updates go.mod in src/dev_tools/goMaker
    
    * Updates go.mod in ./sdk
    
    * Updates go.mod throughout
    
    * Fixes go-work-sync.sh to call go-mod-tidy.sh

---
+++ scripts/go-mod-tidy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#------------------------------------------------
+# Name:    go-mod-tidy.sh
+# Purpose: Finds all go.mod and tidys them
+
+#------------------------------------------------
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+#------------------------------------------------
+cd $SCRIPT_DIR/..
+
+#------------------------------------------------
+find . -type f -name 'go.mod' | while read -r modfile; do
+    moddir=$(dirname "$modfile")
+    echo "Tidying $moddir"
+    cd "$moddir" ; go mod tidy ; cd - >/dev/null
+done
