9d76e1edb89028e78cd5b5b85a397046d2c50c93
Thomas Jay Rush <jrush@quickblocks.io>
2024-06-09 20:12:28 -0400

    Fixes chifra names --custom tests

--- src/dev_tools/testRunner/main_test.go
+++ src/dev_tools/testRunner/main_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestMainFunction(t *testing.T) {
-	os.Setenv("TEST_MODE", "true:cmd")
+	os.Setenv("TEST_MODE", "true")
 	os.Setenv("TB_TEST_FILTER", "names")
 	err := os.Chdir("../../../build/")
 	if err != nil {
