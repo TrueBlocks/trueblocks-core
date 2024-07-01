1c5a79d480f9844eaa853f8753add5faf4d38c77
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-01 18:19:43 -0400

    Updates testing and adds some new test cases

--- src/dev_tools/testRunner/api_server.go
+++ src/dev_tools/testRunner/api_server.go
@@ -24,31 +24,3 @@ func startApiServer() error {
 	logger.Info(colors.Yellow + "Server started..." + colors.Off)
 	return nil
 }
-
-// func getFreePort() (int, error) {
-// 	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
-// 	if err != nil {
-// 		return 0, err
-// 	}
-
-// 	l, err := net.ListenTCP("tcp", addr)
-// 	if err != nil {
-// 		return 0, err
-// 	}
-// 	defer l.Close()
-// 	return l.Addr().(*net.TCPAddr).Port, nil
-// }
-
-// func startServer(port int) {
-// 	server := &http.Server{Addr: fmt.Sprintf(":%d", port), Handler: nil} // Example: No specific handler
-
-// 	listener, err := net.Listen("tcp", server.Addr)
-// 	if err != nil {
-// 		log.Fatalf("Failed to open port %d: %v", port, err)
-// 	}
-
-// 	fmt.Printf("Server starting on port %d\n", port)
-// 	if err := server.Serve(listener); err != nil {
-// 		log.Fatalf("Failed to start server: %v", err)
-// 	}
-// }
