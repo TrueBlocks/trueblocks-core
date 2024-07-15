b75502ef186bda63be3f3db24f3451b64aa2da0e
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-15 16:10:16 -0400

    Closes issue #3815

--- src/apps/chifra/pkg/output/render_ctx.go
+++ src/apps/chifra/pkg/output/render_ctx.go
@@ -1,12 +1,17 @@
 package output
 
-import "context"
+import (
+	"context"
+
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
+)
 
 type RenderCtx struct {
-	Ctx    context.Context    `json:"-"`
-	Cancel context.CancelFunc `json:"-"`
-	// TODO: Add a place for references to caller-level channels
-	// TODO: for progress, errors, and data
+	Ctx          context.Context    `json:"-"`
+	Cancel       context.CancelFunc `json:"-"`
+	ModelChan    chan types.Modeler `json:"-"`
+	ErrorChan    chan error         `json:"-"`
+	ProgressChan chan int           `json:"-"` // place holder
 }
 
 func NewRenderContext() *RenderCtx {
@@ -15,6 +20,15 @@ func NewRenderContext() *RenderCtx {
 		Ctx:    ctx,
 		Cancel: cancel,
 	}
+}
+
+func WithChannels() *RenderCtx {
+	rCtx := NewRenderContext()
+	// TODO: Should these be buffered channels? Issue #3821
+	rCtx.ModelChan = make(chan types.Modeler)
+	rCtx.ErrorChan = make(chan error)
+	rCtx.ProgressChan = make(chan int)
+	return rCtx
 }
 
 func (r *RenderCtx) WasCanceled() bool {
