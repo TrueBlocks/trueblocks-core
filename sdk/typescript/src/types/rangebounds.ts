d49b44842c6f9d2f89647c06d94b4ac72e223562
Thomas Jay Rush <jrush@quickblocks.io>
2024-08-22 09:55:55 -0400

    Adds blockRange type

---
+++ sdk/typescript/src/types/rangebounds.ts
@@ -0,0 +1,18 @@
+/* eslint object-curly-newline: ["error", "never"] */
+/* eslint max-len: ["error", 160] */
+/*
+ * Copyright 2016, 2024 The TrueBlocks Authors. All rights reserved.
+ * Use of this source code is governed by a license that can
+ * be found in the LICENSE file.
+ *
+ * This file was auto generated. DO NOT EDIT.
+ */
+
+import { datetime, timestamp } from '.';
+
+export type RangeBounds = {
+  firstDate?: datetime
+  firstTs: timestamp
+  lastDate?: datetime
+  lastTs: timestamp
+}
---
+++ src/apps/chifra/pkg/types/types_rangebounds.go
@@ -0,0 +1,52 @@
+// Copyright 2016, 2024 The TrueBlocks Authors. All rights reserved.
+// Use of this source code is governed by a license that can
+// be found in the LICENSE file.
+/*
+ * Parts of this file were auto generated. Edit only those parts of
+ * the code inside of 'EXISTING_CODE' tags.
+ */
+
+package types
+
+// EXISTING_CODE
+import (
+	"encoding/json"
+
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/base"
+)
+
+// EXISTING_CODE
+
+type RangeBounds struct {
+	FirstTs base.Timestamp `json:"firstTs"`
+	LastTs  base.Timestamp `json:"lastTs"`
+	// EXISTING_CODE
+	// EXISTING_CODE
+}
+
+func (s RangeBounds) String() string {
+	bytes, _ := json.Marshal(s)
+	return string(bytes)
+}
+
+func (s *RangeBounds) Model(chain, format string, verbose bool, extraOpts map[string]any) Model {
+	var model = map[string]any{}
+	var order = []string{}
+
+	// EXISTING_CODE
+	// EXISTING_CODE
+
+	return Model{
+		Data:  model,
+		Order: order,
+	}
+}
+
+// FinishUnmarshal is used by the cache. It may be unused depending on auto-code-gen
+func (s *RangeBounds) FinishUnmarshal() {
+	// EXISTING_CODE
+	// EXISTING_CODE
+}
+
+// EXISTING_CODE
+// EXISTING_CODE
