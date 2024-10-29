e080e4901d556fec6e1e607e712220b782684bdc
Thomas Jay Rush <jrush@quickblocks.io>
2024-10-28 23:22:45 -0400

    WizStep

--- src/apps/chifra/pkg/types/types_session.go
+++ src/apps/chifra/pkg/types/types_session.go
@@ -339,7 +339,7 @@ var stateOrder = []WizState{
 	Okay,
 }
 
-func (w *Wizard) Step(step Step) {
+func (w *Wizard) Step(step WizStep) {
 	switch step {
 	case Reset:
 		w.State = Welcome
@@ -391,24 +391,24 @@ var AllStates = []struct {
 	{Okay, "OKAY"},
 }
 
-type Step string
+type WizStep string
 
 const (
-	Reset    Step = "Reset"
-	Previous Step = "Previous"
-	Next     Step = "Next"
-	Finish   Step = "Finish"
+	Reset    WizStep = "Reset"
+	Previous WizStep = "Previous"
+	Next     WizStep = "Next"
+	Finish   WizStep = "Finish"
 )
 
 // String returns the string representation of the Step.
-func (s Step) String() string {
+func (s WizStep) String() string {
 	return string(s)
 }
 
 // AllSteps - all possible steps for the frontend codegen
 var AllSteps = []struct {
-	Value  Step   `json:"value"`
-	TSName string `json:"tsName"`
+	Value  WizStep `json:"value"`
+	TSName string  `json:"tsName"`
 }{
 	{Reset, "RESET"},
 	{Previous, "PREVIOUS"},
