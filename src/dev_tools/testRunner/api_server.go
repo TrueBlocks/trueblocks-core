e15566b63c4521770c74e6637e70f6806c972c09
Thomas Jay Rush <jrush@quickblocks.io>
2024-12-20 11:13:34 -0500

    Moves FindAvailablePort to the rpc package

--- src/dev_tools/testRunner/api_server.go
+++ src/dev_tools/testRunner/api_server.go
@@ -2,52 +2,35 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 	"strings"
 
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/colors"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/rpc"
 	sdk "github.com/TrueBlocks/trueblocks-sdk/v4"
 )
 
+func startApiServer() (apiUrl string, err error) {
+	apiUrl = getApiUrl()
+	ready := make(chan bool)
+	go sdk.NewDaemon(apiUrl).Start(ready)
+	<-ready
+	return
+}
+
+// apiPort is a global variable to store the port of the API server
+// of which there can be only one.
 var apiPort = ""
 
 func getApiUrl() string {
+	// use the same port if we've already started the server...
 	if apiPort != "" {
 		return "localhost:" + apiPort
 	}
 
 	apiPort = strings.ReplaceAll(os.Getenv("TB_TEST_API_SERVER"), ":", "")
 	if apiPort == "" {
-		apiPort = fmt.Sprintf("%d", findAvailablePort())
+		apiPort = fmt.Sprintf("%d", rpc.FindAvailablePort())
 	}
+
 	return "localhost:" + apiPort
-}
-
-func startApiServer() error {
-	ready := make(chan bool)
-	go sdk.NewDaemon(getApiUrl()).Start(ready)
-	<-ready
-	logger.Info(colors.Yellow + "Server started..." + colors.Off)
-	return nil
-}
-
-func findAvailablePort() int {
-	preferredPorts := []int{8080, 8088, 9090, 9099}
-	for _, port := range preferredPorts {
-		address := fmt.Sprintf(":%d", port)
-		listener, err := net.Listen("tcp", address)
-		if err == nil {
-			defer listener.Close()
-			return port
-		}
-	}
-	listener, err := net.Listen("tcp", ":0")
-	if err != nil {
-		return 0
-	}
-	defer listener.Close()
-	addr := listener.Addr().(*net.TCPAddr)
-	return addr.Port
 }
