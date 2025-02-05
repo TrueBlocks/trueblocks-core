2aa9cba3be61f7781d78913547955821826efc69
Thomas Jay Rush <jrush@quickblocks.io>
2025-02-05 00:01:03 -0500

    Removes chifra monitors --watch

--- src/apps/chifra/cmd/monitors.go
+++ src/apps/chifra/cmd/monitors.go
@@ -47,10 +47,7 @@ const longMonitors = `Purpose:
 const notesMonitors = `
 Notes:
   - An address must be either an ENS name or start with '0x' and be forty-two characters long.
-  - If no address is presented to the --clean command, all existing monitors will be cleaned.
-  - The --watch option requires two additional parameters to be specified: --watchlist and --commands.
-  - Addresses provided on the command line are ignored in --watch mode.
-  - Providing the value existing to the --watchlist monitors all existing monitor files (see --list).`
+  - If no address is presented to the --clean command, all existing monitors will be cleaned.`
 
 func init() {
 	var capabilities caps.Capability // capabilities for chifra monitors
@@ -67,12 +64,6 @@ func init() {
 	monitorsCmd.Flags().BoolVarP(&monitorsPkg.GetOptions().List, "list", "l", false, `list monitors in the cache (--verbose for more detail)`)
 	monitorsCmd.Flags().BoolVarP(&monitorsPkg.GetOptions().Count, "count", "c", false, `show the number of active monitors (included deleted but not removed monitors)`)
 	monitorsCmd.Flags().BoolVarP(&monitorsPkg.GetOptions().Staged, "staged", "S", false, `for --clean, --list, and --count options only, include staged monitors`)
-	monitorsCmd.Flags().BoolVarP(&monitorsPkg.GetOptions().Watch, "watch", "w", false, `continually scan for new blocks and extract data as per the command file`)
-	monitorsCmd.Flags().StringVarP(&monitorsPkg.GetOptions().Watchlist, "watchlist", "a", "", `available with --watch option only, a file containing the addresses to watch`)
-	monitorsCmd.Flags().StringVarP(&monitorsPkg.GetOptions().Commands, "commands", "d", "", `available with --watch option only, the file containing the list of commands to apply to each watched address`)
-	monitorsCmd.Flags().Uint64VarP(&monitorsPkg.GetOptions().BatchSize, "batch_size", "b", 8, `available with --watch option only, the number of monitors to process in each batch`)
-	monitorsCmd.Flags().Uint64VarP(&monitorsPkg.GetOptions().RunCount, "run_count", "u", 0, `available with --watch option only, run the monitor this many times, then quit`)
-	monitorsCmd.Flags().Float64VarP(&monitorsPkg.GetOptions().Sleep, "sleep", "s", 14, `available with --watch option only, the number of seconds to sleep between runs`)
 	globals.InitGlobals("monitors", monitorsCmd, &monitorsPkg.GetOptions().Globals, capabilities)
 
 	monitorsCmd.SetUsageTemplate(UsageWithNotes(notesMonitors))
