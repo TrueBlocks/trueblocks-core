955b2d7c941298834ec6204aa8197cb36f6ea37a
de666En <chernozub777@gmail.com>
2025-04-02 17:37:29 +0300

    link Update 0x7d655c57f71464b6f83811c55d84009cd9f5221c.sol
    Signed-off-by: de666En <chernozub777@gmail.com>

--- tests/gold/tools/grabABI/0x7d655c57f71464b6f83811c55d84009cd9f5221c.sol
+++ tests/gold/tools/grabABI/0x7d655c57f71464b6f83811c55d84009cd9f5221c.sol
@@ -293,7 +293,7 @@ library Address {
    * IMPORTANT: because control is transferred to `recipient`, care must be
    * taken to not create reentrancy vulnerabilities. Consider using
    * {ReentrancyGuard} or the
-   * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
+   * https://docs.soliditylang.org/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
    */
   function sendValue(address payable recipient, uint256 amount) internal {
     require(address(this).balance >= amount, "Address: insufficient balance");
