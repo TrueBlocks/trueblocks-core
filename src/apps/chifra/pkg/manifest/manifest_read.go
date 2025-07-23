4f93f5babb8ef833fca48aa5a39f15538b875a7e
Thomas Jay Rush <jrush@quickblocks.io>
2025-07-23 00:28:11 -0400

    Cleans up LoadManifest

--- src/apps/chifra/pkg/manifest/manifest_read.go
+++ src/apps/chifra/pkg/manifest/manifest_read.go
@@ -3,7 +3,6 @@ package manifest
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/base"
@@ -11,72 +10,91 @@ import (
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/file"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/usage"
 )
 
-// LoadManifest reads the manifest from a file or from the Unchained Index smart contract.
-//
-// It first checks if the manifest file exists. If it does, it reads the manifest from the file.
-// If the caller requests the contract or the cached manifest does not exist, it reads the
-// manifest from the contract. It then checks if the new manifest has more chunks than the existing
-// one. If it does (or if the file didn't exist), it saves the new manifest to the file. Finally, it
-// creates a map of chunks for easy lookup and sets the specification if it is not already set.
-func LoadManifest(chain string, publisher base.Address, source Source) (man *Manifest, err error) {
-	manifestFn := config.PathToManifestFile(chain)
-	exists := file.FileExists(manifestFn)
-	man = &Manifest{}
-
-	if exists {
-		// We will either return this or use it to compare with the downloaded manifest
-		// to see if we need to update the file.
-		man, err = readManifestFile(manifestFn)
-		if err != nil {
-			return nil, err
-		}
-		if man.Chain != chain {
-			msg := fmt.Sprintf("The remote manifest's chain (%s) does not match the cached manifest's chain (%s).", man.Chain, chain)
-			return man, errors.New(msg)
-		}
-	}
-
-	if source == TempContract || source == FromContract || !exists {
-		database := chain
-		cid, err := ReadUnchainedIndex(chain, publisher, database)
-		if err != nil {
-			return nil, err
-		} else if len(cid) == 0 {
-			return nil, fmt.Errorf("no record found in the Unchained Index for database %s from publisher %s", database, publisher.Hex())
-		}
-		gatewayURL := config.GetChain(chain).IpfsGateway
-		logger.InfoTable("Chain:", chain)
-		logger.InfoTable("Database:", database)
-		logger.InfoTable("Publisher:", publisher)
-		logger.InfoTable("Gateway:", gatewayURL)
-		logger.InfoTable("CID:", cid)
-
-		newManifest, err := downloadManifest(chain, gatewayURL, cid)
-		if err != nil {
-			return nil, err
-		}
-		if newManifest.Chain != chain {
-			msg := fmt.Sprintf("The remote manifest's chain (%s) does not match the cached manifest's chain (%s).", newManifest.Chain, chain)
-			return newManifest, errors.New(msg)
-		}
-		if source != TempContract {
-			err = newManifest.SaveManifest(chain, manifestFn)
-			if err != nil {
-				return nil, err
+// LoadManifest reads the manifest from a file or from the Unchained Index smart contract or local cache.
+func LoadManifest(chain string, publisher base.Address, source Source) (ret *Manifest, err error) {
+	// start with an empty manifest
+	ret = &Manifest{}
+	defer func() {
+		// always update the chunk map on return
+		if ret != nil {
+			ret.ChunkMap = make(map[string]*types.ChunkRecord)
+			if ret.Chunks != nil {
+				for i := range ret.Chunks {
+					ret.ChunkMap[ret.Chunks[i].Range] = &ret.Chunks[i]
+				}
 			}
 		}
+	}()
 
-		man = newManifest
+	manifestFn := config.PathToManifestFile(chain)
+	exists := file.FileExists(manifestFn)
+	if exists {
+		if ret, err = readManifestFile(manifestFn); err != nil {
+			return nil, err
+		} else if ret.Chain != chain {
+			return ret, fmt.Errorf("the remote manifest's chain (%s) does not match the cached manifest's chain (%s)", ret.Chain, chain)
+		} else {
+			// ret is now referring to the existing manifest
+		}
 	}
 
-	man.ChunkMap = make(map[string]*types.ChunkRecord)
-	for i := range man.Chunks {
-		man.ChunkMap[man.Chunks[i].Range] = &man.Chunks[i]
+	if source == LocalCache && exists {
+		// The caller only asked for the local cache, if it exists, return it
+		return ret, nil
 	}
 
-	return man, nil
+	// We need to download the manifest from the smart contract
+	database := chain
+	cid, err := ReadUnchainedIndex(chain, publisher, database)
+	if err != nil {
+		return nil, err
+	} else if len(cid) == 0 {
+		return nil, fmt.Errorf("no record found in the Unchained Index for database %s from publisher %s", database, publisher.Hex())
+	}
+	gatewayURL := config.GetChain(chain).IpfsGateway
+	logger.InfoTable("Chain:", chain)
+	logger.InfoTable("Database:", database)
+	logger.InfoTable("Publisher:", publisher)
+	logger.InfoTable("Gateway:", gatewayURL)
+	logger.InfoTable("CID:", cid)
+
+	remoteManifest, err := downloadManifest(chain, gatewayURL, cid)
+	if err != nil {
+		return nil, err
+	}
+	if remoteManifest.Chain != chain {
+		return remoteManifest, fmt.Errorf("the remote manifest's chain (%s) does not match the cached manifest's chain (%s)", ret.Chain, chain)
+	}
+
+	if source == TempContract {
+		// Caller does not want to save the manifest, so return without saving
+		ret = remoteManifest
+		return ret, nil
+	}
+
+	// If, for some reason, the downloaded manifest is older than the cached one...
+	if ret.Version > remoteManifest.Version {
+		// ...ask the user if they want to use the older manifest...
+		if usage.QueryUserNo("The remote manifest is older. Use it anyway? (y/N)?", "Using older manifest...") {
+			ret = remoteManifest
+			if err = ret.SaveManifest(chain, manifestFn); err != nil {
+				return ret, err
+			}
+		} else {
+			// We use the existing manifest without saving it
+		}
+	} else {
+		// The downloaded manifest is new or the same as the cached one. Use it and save it
+		ret = remoteManifest
+		if err = ret.SaveManifest(chain, manifestFn); err != nil {
+			return ret, err
+		}
+	}
+
+	return ret, nil
 }
 
 var specification = "QmUyyU8wKW57c3CuwphhMdZb2QA5bsjt9vVfTE6LcBKmE9"
